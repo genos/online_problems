61892af8a5390a91af92cd9ba560d9c9622448f0
genos <graham.enos@gmail.com>
2013-10-15 20:46:41 -0400

    new praxis

---
+++ prog_praxis/minimum_difference.hs
@@ -0,0 +1,20 @@
+import Data.Maybe (fromJust)
+
+mad :: [Integer] -> [Integer] -> Maybe Integer
+mad [] _ = Nothing
+mad _ [] = Nothing
+mad (x:xs) (y:ys) = Just $ m xs ys (abs $ x - y)
+    where
+        m [] _ d = d
+        m _ [] d = d
+        m (a:as) (b:bs) d | d' == 1   = 1
+                          | a < b     = m as (b:bs) d'
+                          | otherwise = m (a:as) bs d'
+                          where
+                              d' = min d (abs $ a - b)
+
+
+main :: IO ()
+main = mapM_ (print . uncurry mad) [([], [1..]),
+                                    ([1..], []),
+                                    ([19, 22, 24], [37, 28, 49, 88])]
