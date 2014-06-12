6a8962c8f5f791e1ab95f10932fd1a9cdd1b9160
genos <graham.enos@gmail.com>
2014-06-11 21:46:34 -0400

    new praxis

---
+++ prog_praxis/BalancedDelimiters.hs
@@ -0,0 +1,26 @@
+import Control.Arrow ((&&&))
+import Data.Maybe (fromJust)
+import qualified Data.Map as M
+import qualified Data.Set as S
+
+pairs :: M.Map Char Char
+pairs = M.fromList [(')', '('), (']', '['), ('}', '{')]
+
+open :: S.Set Char
+open = S.fromList . M.elems $ pairs
+
+shut :: S.Set Char
+shut = M.keysSet pairs
+
+ok :: String -> Bool
+ok = f []
+    where
+        f s []     = null s
+        f s (c:cs) | S.member c open          = f (c:s) cs
+                   | S.member c shut && g s c = f (tail s) cs
+                   | otherwise                = f s cs
+        g s c      = not (null s) && (head s == fromJust (M.lookup c pairs))
+
+main :: IO ()
+main = mapM_ (print . (id &&& ok)) . words $
+    "{} [] () a(b)c abc[d] a(b)c{d[e]} {] (] a(b]c abc[d} a(b)c{d[e}]"
