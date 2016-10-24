cc5ee583a099b510a05c6ad508f3be2923eafd18
genos <graham@quantifind.com>
2016-10-24 15:40:52 -0400

    newest submission

---
+++ exercism/rust/bob/src/lib.rs
@@ -0,0 +1,11 @@
+pub fn reply(s: &str) -> &str {
+    if s.is_empty() {
+        "Fine. Be that way!"
+    } else if s.ends_with("?") {
+        "Sure."
+    } else if s.chars().any(|c| c.is_alphabetic()) && s.to_uppercase() == s {
+        "Whoa, chill out!"
+    } else {
+        "Whatever."
+    }
+}
