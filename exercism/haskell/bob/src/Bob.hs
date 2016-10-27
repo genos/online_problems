1bfcc8b8fee4d4ed35c0b54cb87cd9d6a289c651
genos <graham@quantifind.com>
2016-10-27 11:22:20 -0400

    update to latest haskell problem

--- exercism/haskell/bob/src/Bob.hs
+++ exercism/haskell/bob/src/Bob.hs
@@ -1,4 +1,13 @@
 module Bob (responseFor) where
 
+import Data.Char (isAlpha, isSpace, toUpper)
+import Data.List (dropWhileEnd)
+
 responseFor :: String -> String
-responseFor = undefined
+responseFor = reply . trim
+  where
+    reply s | null s || all isSpace s              = "Fine. Be that way!"
+            | any isAlpha s && s == fmap toUpper s = "Whoa, chill out!"
+            | last s == '?'                        = "Sure."
+            | otherwise                            = "Whatever."
+    trim = dropWhileEnd isSpace . dropWhile isSpace
