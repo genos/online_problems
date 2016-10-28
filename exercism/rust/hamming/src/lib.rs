4fa5d6bd03d3e723fb20efdb2ea0c55a64446579
genos <graham@quantifind.com>
2016-10-28 11:42:32 -0400

    new rust

---
+++ exercism/rust/hamming/src/lib.rs
@@ -0,0 +1,17 @@
+#[derive(Debug)]
+pub enum HammingError {
+    FirstLonger,
+    SecondLonger,
+}
+
+pub fn hamming_distance(a: &str, b: &str) -> Result<u64, HammingError> {
+    if a.len() > b.len() {
+        Err(HammingError::FirstLonger)
+    } else if a.len() < b.len() {
+        Err(HammingError::SecondLonger)
+    } else {
+        Ok(a.chars()
+           .zip(b.chars())
+           .fold(0, |z, (x, y)| z + (if x == y { 0 } else { 1 })))
+    }
+}
