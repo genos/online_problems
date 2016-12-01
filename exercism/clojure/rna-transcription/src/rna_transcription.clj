3a61afd7dec711773682b757ad20817e4f04495c
genos <graham@quantifind.com>
2016-12-01 17:08:36 -0500

    better

--- exercism/clojure/rna-transcription/src/rna_transcription.clj
+++ exercism/clojure/rna-transcription/src/rna_transcription.clj
@@ -1,13 +1,6 @@
 (ns rna-transcription
   (:require [clojure.string :as str]))
 
-(defn- transcribe [c]
-  (cond
-    (= c \A) \U
-    (= c \C) \G
-    (= c \G) \C
-    (= c \T) \A
-    :else (throw (AssertionError. "invalid character"))))
-
 (defn to-rna [dna]
-  (str/join (map transcribe dna)))
+  (str/join
+    (map #(or ({\A \U \C \G \G \C \T \A} %) (assert false)) dna)))
