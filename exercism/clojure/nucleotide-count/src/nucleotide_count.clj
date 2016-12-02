83de07912b68c7d539a776ba584b6cfc22d9e51f
genos <graham@quantifind.com>
2016-12-02 12:08:18 -0500

    updated

--- exercism/clojure/nucleotide-count/src/nucleotide_count.clj
+++ exercism/clojure/nucleotide-count/src/nucleotide_count.clj
@@ -1,13 +1,12 @@
 (ns nucleotide-count)
 
 (def base {\A 0 \C 0 \G 0 \T 0})
+(def base-keys (set (keys base)))
 
 (defn nucleotide-counts [dna]
-  (let [freqs (frequencies dna)
-        result (merge base freqs)]
-    (assert (= (keys base) (keys result)))
-    result))
+  {:post [(= base-keys (set (keys %)))]}
+  (merge-with + base (frequencies dna)))
 
 (defn count [s dna]
-  (assert (contains? base s))
+  {:pre [(base-keys s)]}
   ((nucleotide-counts dna) s 0))
