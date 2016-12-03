bd48b9e338f9fa56fd8f86ea9e9aa6171ec60e7e
genos <graham@quantifind.com>
2016-12-03 14:53:48 -0500

    another clj

---
+++ exercism/clojure/grade-school/src/grade_school.clj
@@ -0,0 +1,10 @@
+(ns grade-school)
+
+(defn add [db s g]
+  (update db g #(vec (conj % s))))
+
+(defn grade [db g]
+  (get db g []))
+
+(defn sorted [db]
+  (into (sorted-map) (for [[g ss] db] [g (sort ss)])))
