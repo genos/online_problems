8dce37807bffcaf6c7b3130d22b5788f51faf1d8
genos <graham@quantifind.com>
2016-12-03 14:05:44 -0500

    better

--- exercism/clojure/phone-number/src/phone_number.clj
+++ exercism/clojure/phone-number/src/phone_number.clj
@@ -3,43 +3,40 @@
 
 (defrecord PhoneNumber [area exchange line])
 
-(defn- clean [s]
-  (str/replace s #"[^0-9]" ""))
-
 (defn- validate [s]
-  (let [c (clean s)
+  (let [c (str/replace s #"\D" "")
         n (count c)]
     (cond
-      (= 10 n) s
-      (and (= 11 n) (str/starts-with? s "1")) (subs s 1)
+      (= 10 n) c
+      (and (= 11 n) (str/starts-with? c "1")) (subs c 1)
       :else "0000000000")))
 
 (defn- parse [s]
   (->> s
        (re-find #"(\d{3})(\d{3})(\d{4})")
-       (rest)
+       rest
        (apply ->PhoneNumber)))
 
 (defn- to-phone-number [s]
-  (-> s clean validate parse))
+  (-> s validate parse))
 
-(defn- print-flat [p]
+(defn- flat [p]
   (let [a (:area p)
         e (:exchange p)
         l (:line p)]
     (str a e l)))
 
-(defn print-formatted [p]
+(defn- formatted [p]
   (let [a (:area p)
         e (:exchange p)
         l (:line p)]
     (format "(%s) %s-%s" a e l)))
 
 (defn number [s]
-  (-> s to-phone-number print-flat))
+  (-> s to-phone-number flat))
 
 (defn area-code [s]
   (-> s to-phone-number :area))
 
 (defn pretty-print [s] s
-  (-> s to-phone-number print-formatted))
+  (-> s to-phone-number formatted))
