d7441c9166a131367f1f09f21f8ed9929fd596bb
genos <graham@quantifind.com>
2016-12-03 14:17:43 -0500

    cleaner

--- exercism/clojure/phone-number/src/phone_number.clj
+++ exercism/clojure/phone-number/src/phone_number.clj
@@ -1,9 +1,7 @@
 (ns phone-number
   (:require [clojure.string :as str]))
 
-(defrecord PhoneNumber [area exchange line])
-
-(defn- validate [s]
+(defn number [s]
   (let [c (str/replace s #"\D" "")
         n (count c)]
     (cond
@@ -11,32 +9,8 @@
       (and (= 11 n) (str/starts-with? c "1")) (subs c 1)
       :else "0000000000")))
 
-(defn- parse [s]
-  (->> s
-       (re-find #"(\d{3})(\d{3})(\d{4})")
-       rest
-       (apply ->PhoneNumber)))
-
-(defn- to-phone-number [s]
-  (-> s validate parse))
-
-(defn- flat [p]
-  (let [a (:area p)
-        e (:exchange p)
-        l (:line p)]
-    (str a e l)))
-
-(defn- formatted [p]
-  (let [a (:area p)
-        e (:exchange p)
-        l (:line p)]
-    (format "(%s) %s-%s" a e l)))
-
-(defn number [s]
-  (-> s to-phone-number flat))
-
 (defn area-code [s]
-  (-> s to-phone-number :area))
+  (subs (number s) 0 3))
 
-(defn pretty-print [s] s
-  (-> s to-phone-number formatted))
+(defn pretty-print [s]
+  (str/replace (number s) #"(\d{3})(\d{3})(\d{4})" "($1) $2-$3"))
