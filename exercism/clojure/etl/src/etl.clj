6eeb98e8b181450d866fe53715c2cdf8dcd42b3f
genos <graham@quantifind.com>
2016-12-06 17:09:26 -0500

    better

--- exercism/clojure/etl/src/etl.clj
+++ exercism/clojure/etl/src/etl.clj
@@ -1,9 +1,8 @@
 (ns etl
   (:require [clojure.string :refer [lower-case]]))
 
-(defn invert [k vs]
-  (let [ms (map (fn [v] {(lower-case v) k}) vs)]
-    (into {} ms)))
-
 (defn transform [m]
-  (into {} (map (partial apply invert)) m))
+  (into {}
+        (for [[score words] m
+              word words]
+          [(lower-case word) score])))
