a405670cef35bd9dc7c2ee5d1fbff770badf55b4
genos <graham@quantifind.com>
2016-12-06 10:16:53 -0500

    day3 of aoc

---
+++ advent_of_code_2016/day3/src/Main.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+module Main where
+
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as BC
+import Data.FileEmbed (embedFile)
+import Data.List (transpose)
+import Data.List.Split (chunksOf)
+import Data.Maybe (fromJust)
+
+triple :: ByteString -> [Int]
+triple =
+  map (fst . fromJust . BC.readInt) . filter (/= "") . BC.splitWith (== ' ')
+
+input :: ByteString
+input = $(embedFile "input.txt")
+
+triples :: [[Int]]
+triples = map triple . BC.lines $ input
+
+validTriangle :: [Int]  -> Bool
+validTriangle xs = (a + b) > c && (a + c) > b && (b + c) > a
+  where [a, b, c] = take 3 xs
+
+countValid :: [[Int]] -> Int
+countValid = length . filter validTriangle
+
+part1 :: Int
+part1 = countValid triples
+
+part2 :: Int
+part2 = countValid . concatMap (chunksOf 3) . transpose $ triples
+
+main :: IO ()
+main = mapM_ print [part1, part2]
