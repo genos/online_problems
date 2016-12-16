19493117f77f1d7ac79f749aa5adb128e0eb1a07
genos <graham@quantifind.com>
2016-12-16 16:55:33 -0500

    one less extern crate, one less allocation

--- advent_of_code_2016/day16rs/src/main.rs
+++ advent_of_code_2016/day16rs/src/main.rs
@@ -1,23 +1,19 @@
-extern crate bit_vec;
 extern crate itertools;
-use bit_vec::BitVec;
 use itertools::Itertools;
 
-fn str_to_bv(s: &str) -> BitVec {
-    let mut b = BitVec::from_elem(s.len(), false);
-    for (i, c) in s.char_indices() {
-        b.set(i, c == '1');
-    }
-    b
+fn str_to_bv(s: &str) -> Vec<bool> {
+    s.chars().map(|c| c == '1').collect()
 }
 
-fn dragon(b: &mut BitVec) {
+fn bv_to_str(b: Vec<bool>) -> String {
+    b.iter().map(|x| if *x { '1' } else { '0' }).collect()
+}
+
+fn dragon(b: &mut Vec<bool>) {
     let n = b.len();
-    let mut c: BitVec = b.iter().rev().collect();
-    c.negate();
-    b.grow(n + 1, false);
+    b.resize(2 * n + 1, false);
     for i in 0..n {
-        b.set(n + 1 + i, c.get(i).unwrap());
+        b[n + 1 + i] = !b[n - i - 1];
     }
 }
 
@@ -30,18 +26,17 @@ fn dragon_test() {
                        ("11111", "11111000000"),
                        ("111100001010", "1111000010100101011110000")] {
         let mut x = str_to_bv(a);
-        let y = str_to_bv(b);
         dragon(&mut x);
-        assert_eq!(x, y);
+        assert_eq!(bv_to_str(x), b);
     }
 }
 
-fn check_step(b: &mut BitVec) {
+fn check_step(b: &mut Vec<bool>) {
     let n = b.len();
     let c = b.iter().tuples().map(|(x, y)| x == y).collect::<Vec<_>>();
     b.truncate(n / 2);
-    for i in 0..n / 2 {
-        b.set(i, c[i]);
+    for i in 0..b.len() {
+        b[i] = c[i];
     }
 }
 
@@ -52,20 +47,16 @@ fn check_step_test() {
                        ("10000011110010000111", "0111110101"),
                        ("0111110101", "01100")] {
         let mut x = str_to_bv(a);
-        let y = str_to_bv(b);
         check_step(&mut x);
-        assert_eq!(x, y);
+        assert_eq!(bv_to_str(x), b);
     }
 }
 
-fn check_sum(b: &mut BitVec, l: usize) {
-    // expand
+fn check_sum(b: &mut Vec<bool>, l: usize) {
     while b.len() < l {
         dragon(b);
     }
-    // cut
     b.truncate(l);
-    // shrink
     while b.len() & 1 == 0 {
         check_step(b);
     }
@@ -75,17 +66,17 @@ fn check_sum(b: &mut BitVec, l: usize) {
 fn check_sum_test() {
     let mut a = str_to_bv("110010110100");
     check_sum(&mut a, 12);
-    assert_eq!(a, str_to_bv("100"));
+    assert_eq!(bv_to_str(a), "100");
     let mut b = str_to_bv("10000");
     check_sum(&mut b, 20);
-    assert_eq!(b, str_to_bv("01100"));
+    assert_eq!(bv_to_str(b), "01100");
 }
 
 fn main() {
     let mut input1 = str_to_bv("10001001100000001");
     check_sum(&mut input1, 272);
-    println!("{:?}", input1);
+    println!("{}", bv_to_str(input1));
     let mut input2 = str_to_bv("10001001100000001");
     check_sum(&mut input2, 35651584);
-    println!("{:?}", input2);
+    println!("{}", bv_to_str(input2));
 }
