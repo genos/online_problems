a68c0c65ac757abb6fd4a69f4c77ecb0888f19ea
genos <graham@quantifind.com>
2016-12-21 11:24:20 -0500

    improvements to day12

--- advent_of_code_2016/day12/hask/src/Main.hs
+++ advent_of_code_2016/day12/hask/src/Main.hs
@@ -23,9 +23,9 @@ type Value = Int
 
 data Register = A | B | C | D deriving (Eq,Ord,Enum)
 
-data IorR = I Value | R Register deriving Eq
+data VorR = V Value | R Register deriving Eq
 
-data Command = Cpy IorR Register | Inc Register | Dec Register | Jnz IorR IorR
+data Command = Cpy VorR Register | Inc Register | Dec Register | Jnz VorR VorR
     deriving Eq
 
 type Registers = U.Vector Int
@@ -42,17 +42,13 @@ register =
   (char 'd' >> return D)
 
 signedInt :: Parser Int
-signedInt = do
-  sign <- option '+' $ char '-' <|> char '+'
-  let mul = if sign == '-' then (-1) else 1
-  i <- fromIntegral <$> L.integer
-  return $! mul * i
+signedInt = L.signed space $ fromInteger <$> L.integer
 
-iOrR :: Parser IorR
-iOrR = (I <$> signedInt) <|> (R <$> register)
+vOrR :: Parser VorR
+vOrR = (V <$> signedInt) <|> (R <$> register)
 
 cpy :: Parser Command
-cpy = Cpy <$> (string "cpy" *> space *> iOrR <* space) <*> register
+cpy = Cpy <$> (string "cpy" *> space *> vOrR <* space) <*> register
 
 inc :: Parser Command
 inc = Inc <$> (string "inc" *> space *> register)
@@ -61,23 +57,24 @@ dec :: Parser Command
 dec = Dec <$> (string "dec" *> space *> register)
 
 jnz :: Parser Command
-jnz = Jnz <$> (string "jnz" *> space *> iOrR <* space) <*> iOrR
+jnz = Jnz <$> (string "jnz" *> space *> vOrR <* space) <*> vOrR
 
 assembunny :: Parser Commands
 assembunny = V.fromList <$> (cpy <|> inc <|> dec <|> jnz) `sepEndBy` newline
-get :: IorR -> Registers -> Value
-get (I i) _  = i
-get (R r) rs = rs U.! fromEnum r
+
+load :: VorR -> Registers -> Value
+load (V v) _  = v
+load (R r) rs = rs U.! fromEnum r
 
 step :: IP -> Command -> Registers -> IP
 step i (Jnz a b) rs = i + fromIntegral s
   where
-    g = get a rs
-    s = if g == 0 then 1 else get b rs
+    g = load a rs
+    s = if g == 0 then 1 else load b rs
 step i _ _ = i + 1
 
 update :: Registers -> Command -> Registers
-update rs (Cpy a r) = rs U.// [(fromEnum r, get a rs)]
+update rs (Cpy a r) = rs U.// [(fromEnum r, load a rs)]
 update rs (Inc r)   = rs U.// [(i, 1 + rs U.! i)] where i = fromEnum r
 update rs (Dec r)   = rs U.// [(i, (-1) + rs U.! i)] where i = fromEnum r
 update rs (Jnz _ _) = rs
@@ -92,7 +89,7 @@ interpret rs i cs
     rs' = update rs x
 
 run :: Registers -> Text -> Maybe Value
-run rs t = get (R A) . interpret rs 0 <$> parseMaybe assembunny t
+run rs t = load (R A) . interpret rs 0 <$> parseMaybe assembunny t
 
 test1 :: Bool
 test1 = Just 42 == run (U.replicate 4 0) testInput
