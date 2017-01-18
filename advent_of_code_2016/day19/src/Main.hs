f8284cd0170ac696502913db5b2c0ec10b74fb87
genos <graham@quantifind.com>
2017-01-17 21:37:01 -0500

    clean

--- advent_of_code_2016/day19/src/Main.hs
+++ advent_of_code_2016/day19/src/Main.hs
@@ -6,17 +6,14 @@ import Protolude
 import Data.Sequence (Seq)
 import qualified Data.Sequence as S
 
-type Algebra f a = f a -> a
-
 build :: Int -> Seq Int
 build n = S.fromList [1 .. n]
 
 part1 :: Int -> Int
-part1 = f . build where
-  f :: Algebra Seq Int
-  f xs | S.null xs      = -1
-       | length xs <= 2 = S.index xs 0
-       | otherwise      = f $ S.drop 2 xs S.>< S.take 1 xs
+part1 = rec . build where
+  rec xs | S.null xs      = -1
+         | length xs <= 2 = S.index xs 0
+         | otherwise      = rec $ S.drop 2 xs S.>< S.take 1 xs
 
 testInput :: Int
 testInput = 5
@@ -28,15 +25,14 @@ testPart1 :: Bool
 testPart1 = 3 == part1 testInput
 
 part2 :: Int -> Int
-part2 = f . build where
-  f :: Algebra Seq Int
-  f xs | S.null xs      = -1
-       | length xs <= 2 = S.index xs 0
-       | otherwise      = f ws where
-                          ws = zs S.|> z
-                          (z S.:< zs) = S.viewl ys
-                          ys = S.take n xs S.>< S.drop (n + 1) xs
-                          n  = length xs `div` 2
+part2 = rec . build where
+  rec xs | S.null xs      = -1
+         | length xs <= 2 = S.index xs 0
+         | otherwise      = rec ws where
+                            ws = zs S.|> z
+                            (z S.:< zs) = S.viewl ys
+                            ys = S.take n xs S.>< S.drop (n + 1) xs
+                            n  = length xs `div` 2
 
 testPart2 :: Bool
 testPart2 = 2 == part2 testInput
