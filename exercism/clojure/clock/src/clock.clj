75236507e9fb3cbff5919bd9137ad0515b916a48
genos <graham@quantifind.com>
2017-09-28 16:35:05 -0400

    An even better answer

--- exercism/clojure/clock/src/clock.clj
+++ exercism/clojure/clock/src/clock.clj
@@ -1,13 +1,13 @@
 (ns clock)
 
-(defn clock [a b]
-  (let [q (Math/floorDiv b 60)
-        h (mod (+ a q) 24)
-        m (mod b 60)]
-    {:hours h :minutes m}))
+;; Per http://exercism.io/submissions/468867ee4cbd47ca83a4d850f3082e42,
+;; we represent clocks in terms of minutes
 
-(defn add-time [c n]
-  (clock (:hours c) (+ (:minutes c) n)))
+(defn- to-minutes [c] (mod c 1440))
+
+(defn clock [h m] (to-minutes (+ (* 60 h) m)))
+
+(defn add-time [c m] (to-minutes (+ c m)))
 
 (defn clock->string [c]
-  (format "%02d:%02d" (:hours c) (:minutes c)))
+  (format "%02d:%02d" (quot c 60) (mod c 60)))
