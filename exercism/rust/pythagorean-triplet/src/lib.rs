f1bdbd6330a49e055e810d3685618b23c81e1080
genos <graham@quantifind.com>
2017-10-03 14:23:29 -0400

    better: no overflow, all unsigned

--- exercism/rust/pythagorean-triplet/src/lib.rs
+++ exercism/rust/pythagorean-triplet/src/lib.rs
@@ -1,7 +1,7 @@
-pub fn find() -> Option<i32> {
-    let n: i32 = 1000;
+pub fn find() -> Option<u32> {
+    let n: u32 = 1000;
     for a in 1..n {
-        for b in a..n {
+        for b in 1..a - 1 {
             let c = n - a - b;
             if a * a + b * b == c * c {
                 return Some(a * b * c);
