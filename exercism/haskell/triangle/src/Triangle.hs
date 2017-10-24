fa4489d53bac1b4f732a3052e84a879b893b1e3f
genos <graham@quantifind.com>
2017-10-24 14:23:21 -0400

    haskell exercism

---
+++ exercism/haskell/triangle/src/Triangle.hs
@@ -0,0 +1,15 @@
+module Triangle (TriangleType(..), triangleType) where
+
+data TriangleType = Equilateral
+                  | Isosceles
+                  | Scalene
+                  | Illegal
+                  deriving (Eq, Show)
+
+triangleType :: (Eq a, Ord a, Num a) => a -> a -> a -> TriangleType
+triangleType a b c
+  | 0 `elem` [a, b, c]                         =  Illegal
+  | (a + b < c) || (a + c < b) || (b + c < a)  =  Illegal
+  | (a == b) && (b == c)                       =  Equilateral
+  | (a == b) || (a == c) || (b == c)           =  Isosceles
+  | otherwise                                  =  Scalene
