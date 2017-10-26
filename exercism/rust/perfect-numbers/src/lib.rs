ec4f52b7c8f9faa8e3fe2c5593a5a98aca010da7
genos <graham@quantifind.com>
2017-10-26 11:09:02 -0400

    tweak

--- exercism/rust/perfect-numbers/src/lib.rs
+++ exercism/rust/perfect-numbers/src/lib.rs
@@ -11,14 +11,14 @@ pub fn classify(num: u64) -> Result<Classification, &'static str> {
     if num == 0 {
         Err("Number must be positive")
     } else {
-        match (1..1 + num / 2)
+        Ok(match (1..1 + num / 2)
             .filter(|i| num % i == 0)
             .sum::<u64>()
             .cmp(&num)
         {
-            Ordering::Less => Ok(Classification::Deficient),
-            Ordering::Equal => Ok(Classification::Perfect),
-            Ordering::Greater => Ok(Classification::Abundant),
-        }
+            Ordering::Less => Classification::Deficient,
+            Ordering::Equal => Classification::Perfect,
+            Ordering::Greater => Classification::Abundant,
+        })
     }
 }
