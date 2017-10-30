7895a8b024b5513da950a879ed796c4c8458ef2c
genos <graham@quantifind.com>
2017-10-30 16:27:14 -0400

    different rust answer

--- exercism/rust/isogram/src/lib.rs
+++ exercism/rust/isogram/src/lib.rs
@@ -1,13 +1,7 @@
 use std::collections::HashSet;
 
 pub fn check(s: &str) -> bool {
-    let mut cs = HashSet::new();
-    for c in s.to_lowercase().chars().filter(|c| c.is_alphabetic()) {
-        if cs.contains(&c) {
-            return false;
-        } else {
-            cs.insert(c);
-        }
-    }
-    true
+    let xs: Vec<_> = s.to_lowercase().chars().filter(|c| c.is_alphabetic()).collect();
+    let ys: HashSet<_> = xs.iter().collect();
+    xs.len() == ys.len()
 }
