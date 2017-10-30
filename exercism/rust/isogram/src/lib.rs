bef39cb20294ef6ac18b018e9b0c49c6ec537caa
genos <graham@quantifind.com>
2017-10-30 16:28:00 -0400

    cargo fmt

--- exercism/rust/isogram/src/lib.rs
+++ exercism/rust/isogram/src/lib.rs
@@ -1,7 +1,10 @@
 use std::collections::HashSet;
 
 pub fn check(s: &str) -> bool {
-    let xs: Vec<_> = s.to_lowercase().chars().filter(|c| c.is_alphabetic()).collect();
+    let xs: Vec<_> = s.to_lowercase()
+        .chars()
+        .filter(|c| c.is_alphabetic())
+        .collect();
     let ys: HashSet<_> = xs.iter().collect();
     xs.len() == ys.len()
 }
