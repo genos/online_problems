3903feb4686496efafb7315ca9fae8db76bd01a2
genos <graham@quantifind.com>
2017-11-15 11:46:20 -0800

    generic ops

--- exercism/haskell/matrix/src/Matrix.hs
+++ exercism/haskell/matrix/src/Matrix.hs
@@ -20,10 +20,10 @@ import qualified Data.Vector   as V
 newtype Matrix a = M { _M :: Vector (Vector a) } deriving (Eq, Show)
 
 cols :: Matrix a -> Int
-cols = fromMaybe 0 . (V.!?0) . V.map V.length . _M
+cols = fromMaybe 0 . (V.!?0) . fmap length . _M
 
 column :: Int -> Matrix a -> Vector a
-column i = V.map (V.!i) . _M
+column i = fmap (V.!i) . _M
 
 flatten :: Matrix a -> Vector a
 flatten = V.concat . V.toList . _M
@@ -36,16 +36,16 @@ fromString = fromList . fmap (fmap read . words) . lines
 
 reshape :: (Int, Int) -> Matrix a -> Matrix a
 reshape (rs, cs) m =
-  M . V.map (\r -> V.slice r cs $ flatten m) $ V.enumFromStepN 0 cs rs
+  M . fmap (\r -> V.slice r cs $ flatten m) $ V.enumFromStepN 0 cs rs
 
 row :: Int -> Matrix a -> Vector a
 row i = (V.!i) . _M
 
 rows :: Matrix a -> Int
-rows = V.length . _M
+rows = length . _M
 
 shape :: Matrix a -> (Int, Int)
 shape = rows &&& cols
 
 transpose :: Matrix a -> Matrix a
-transpose m = M . V.map (`column`m) . V.enumFromN 0 $ cols m
+transpose m = M . fmap (`column`m) . V.enumFromN 0 $ cols m
