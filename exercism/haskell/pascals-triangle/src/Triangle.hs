9ab1880e6b1626ebe734a36421c0be2b9603e4ac
genos <graham@quantifind.com>
2017-11-15 16:13:39 -0800

    simpler

--- exercism/haskell/pascals-triangle/src/Triangle.hs
+++ exercism/haskell/pascals-triangle/src/Triangle.hs
@@ -1,12 +1,11 @@
 module Triangle (rows) where
 
 import           Data.Foldable (toList)
-import           Data.List     (unfoldr)
+import           Data.List     (iterate)
 import qualified Data.Sequence as S  -- `containers` package
 
 rows :: Int -> [[Integer]]
-rows n = take n $ [1] : unfoldr f (S.singleton 1)
+rows n = take n . fmap toList . iterate f . S.singleton $ 1
   where
-    f :: S.Seq Integer -> Maybe ([Integer], S.Seq Integer)
-    f xs =
-      let ys = S.zipWith (+) (0 S.<| xs) (xs S.|> 0) in Just (toList ys, ys)
+    f :: S.Seq Integer -> S.Seq Integer
+    f = S.zipWith (+) <$> (0 S.<|) <*> (S.|> 0)
