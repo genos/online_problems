58ee979ee32259d42a8713bf4259200b0ba1384e
genos <graham@quantifind.com>
2017-12-07 15:57:52 -0500

    shorter

--- exercism/java/two-fer/src/main/java/Twofer.java
+++ exercism/java/two-fer/src/main/java/Twofer.java
@@ -1,7 +1,5 @@
 public class Twofer {
   public String twofer(String name) {
-      String n = name;
-      if (n == null) { n = "you"; }
-      return "One for " + n + ", one for me.";
+    return String.format("One for %s, one for me.", name == null ? "you" : name);
   }
 }
