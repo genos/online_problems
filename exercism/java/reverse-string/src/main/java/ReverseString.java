bc1ca1b6841a002f962d681e456a19be86ae4792
genos <graham@quantifind.com>
2017-12-07 16:16:06 -0500

    moar java

--- exercism/java/reverse-string/src/main/java/ReverseString.java
+++ exercism/java/reverse-string/src/main/java/ReverseString.java
@@ -1,9 +1,13 @@
 class ReverseString {
   public String reverse(String input) {
     // return new StringBuilder(input).reverse().toString();
+    // return input
+    //     .chars()
+    //     .mapToObj((int i) -> Character.toString((char) i))
+    //     .reduce("", (String acc, String c) -> c + acc);
     return input
         .chars()
-        .mapToObj((int i) -> Character.toString((char) i))
-        .reduce("", (String acc, String c) -> c + acc);
+        .collect(StringBuilder::new, (sb, i) -> sb.insert(0, (char) i), StringBuilder::append)
+        .toString();
   }
 }
