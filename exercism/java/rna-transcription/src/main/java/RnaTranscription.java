efa2ed5a1d3183d867e2bb275d0a2648d4a63dde
genos <graham@quantifind.com>
2017-12-11 11:23:05 -0500

    I like this one more

--- exercism/java/rna-transcription/src/main/java/RnaTranscription.java
+++ exercism/java/rna-transcription/src/main/java/RnaTranscription.java
@@ -1,22 +1,20 @@
 class RnaTranscription {
   String transcribe(String dnaStrand) {
     StringBuilder s = new StringBuilder();
-    dnaStrand.chars().forEachOrdered(c -> s.append(lookup(c)));
-    return s.toString();
+    return dnaStrand
+        .chars()
+        .mapToObj((i) -> lookup(i))
+        .collect(StringBuilder::new, (b, c) -> b.append(c), StringBuilder::append)
+        .toString();
   }
 
   char lookup(int c) {
     switch ((char) c) {
-      case 'A':
-        return 'U';
-      case 'C':
-        return 'G';
-      case 'G':
-        return 'C';
-      case 'T':
-        return 'A';
-      default:
-        throw new IllegalArgumentException("Invalid input");
+      case 'A': return 'U';
+      case 'C': return 'G';
+      case 'G': return 'C';
+      case 'T': return 'A';
+      default: throw new IllegalArgumentException("Invalid input");
     }
   }
 }
