c10f8132ede6a120c03193e47c6a2633d59f13d3
genos <graham@quantifind.com>
2017-12-15 13:37:04 -0500

    tweak

--- exercism/ruby/hamming/hamming.rb
+++ exercism/ruby/hamming/hamming.rb
@@ -1,11 +1,11 @@
-module BookKeeping
-  VERSION = 3
-end
-
 # My submission for exercism.io's "Hamming" exercise.
 class Hamming
   def self.compute(x, y)
     raise(ArgumentError, 'Different lengths') unless x.length == y.length
     x.each_char.zip(y.each_char).count { |a, b| a != b }
   end
+end
+
+module BookKeeping
+  VERSION = 3
 end
