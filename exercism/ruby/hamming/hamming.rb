fae1f618520197cc536b6be2ce094dfb97d96030
genos <graham@quantifind.com>
2017-12-15 13:34:21 -0500

    better answer

--- exercism/ruby/hamming/hamming.rb
+++ exercism/ruby/hamming/hamming.rb
@@ -6,6 +6,6 @@ end
 class Hamming
   def self.compute(x, y)
     raise(ArgumentError, 'Different lengths') unless x.length == y.length
-    x.each_char.zip(y.each_char).map { |a, b| a == b ? 0 : 1 }.sum
+    x.each_char.zip(y.each_char).count { |a, b| a != b }
   end
 end
