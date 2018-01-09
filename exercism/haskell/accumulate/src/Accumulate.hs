14b3935037b696e9cd11ea84b6e94a6c1a4b82a1
genos <graham@quantifind.com>
2018-01-09 14:48:50 -0500

    unneeded ()

--- exercism/haskell/accumulate/src/Accumulate.hs
+++ exercism/haskell/accumulate/src/Accumulate.hs
@@ -2,4 +2,4 @@ module Accumulate (accumulate) where
 
 accumulate :: (a -> b) -> [a] -> [b]
 accumulate _ []     = []
-accumulate f (x:xs) = (f x) : accumulate f xs
+accumulate f (x:xs) = f x : accumulate f xs
