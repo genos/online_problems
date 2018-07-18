56aaa3c70ca5ddc7ec56e88a425d3fbfde066b26
genos <graham@quantifind.com>
2018-07-18 11:15:23 -0400

    new exercism track: common-lisp, with two solutions

---
+++ exercism/common-lisp/hamming/hamming.lisp
@@ -0,0 +1,12 @@
+(defpackage #:hamming
+  (:use #:cl)
+  (:export #:distance))
+
+(in-package #:hamming)
+
+(defun distance (dna1 dna2)
+  "Number of positional differences in two equal length dna strands."
+  (when (= (length dna1) (length dna2))
+    (loop for x across dna1
+          for y across dna2
+          counting (char/= dna1 dna2))))
