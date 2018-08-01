c59c7a6a5b606c23777557541bd2b6946662dfa7
genos <graham@quantifind.com>
2018-08-01 10:46:05 -0400

    threading

--- exercism/racket/gigasecond/gigasecond.rkt
+++ exercism/racket/gigasecond/gigasecond.rkt
@@ -2,9 +2,13 @@
 
 (provide add-gigasecond)
 
+(require threading)
 (require/typed racket/date
                [date->seconds (-> date Integer)])
 
 (: add-gigasecond (-> date date))
-(define (add-gigasecond datetime)
-  (seconds->date (+ 1000000000 (date->seconds datetime))))
+(define (add-gigasecond d)
+  (~> d
+      date->seconds
+      (+ 1000000000)
+      seconds->date))
