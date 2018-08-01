962c2afa45c51e5b33b4d99999fe7ddd3e775660
genos <graham@quantifind.com>
2018-08-01 14:17:50 -0400

    new exercism

---
+++ exercism/clojure/run-length-encoding/src/run_length_encoding.clj
@@ -0,0 +1,26 @@
+(ns run-length-encoding)
+
+(defn run-length-encode
+  "encodes a string with run-length-encoding"
+  [plain-text]
+  (->> plain-text
+       (partition-by identity)
+       (map (fn [xs]
+              (let [c (count xs)
+                    x (first xs)]
+                (if (= c 1)
+                  x
+                  (str c x)))))
+       (apply str)))
+
+(defn run-length-decode
+  "decodes a run-length-encoded string"
+  [cipher-text]
+  (->> cipher-text
+       (re-seq #"([0-9]*)([^0-9])")
+       (mapcat (fn [[_ c x]]
+                (let [i (if-not (empty? c)
+                          (Integer/parseInt c)
+                          1)]
+                  (replicate i x))))
+       (apply str)))
