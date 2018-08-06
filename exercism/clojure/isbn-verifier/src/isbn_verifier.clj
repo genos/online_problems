5777c2a785b17a69cd0fb522288883e32fd781d1
genos <graham@quantifind.com>
2018-08-06 10:24:06 -0400

    cleanup

--- exercism/clojure/isbn-verifier/src/isbn_verifier.clj
+++ exercism/clojure/isbn-verifier/src/isbn_verifier.clj
@@ -1,30 +1,23 @@
 (ns isbn-verifier)
 
-(defn clean [s]
-  (map first (re-seq #"[0-9X]" s)))
-
-(defn length-ok? [c]
-  (= (count c) 10))
-
 (defn x-only-last? [c]
-  (let [xs (keep-indexed #(if (= \X %2) %1) c)]
-    (or
-     (empty? xs)
-     (and (= 1 (count xs))
-          (= 9 (first xs))))))
+  (let [xs (keep-indexed #(if (= \X %2) %1) c)
+        n  (count xs)
+        x  (first xs)]
+    (or (empty? xs)
+        (and (= 1 n) (= 9 x)))))
 
-(def int-map
-  (apply hash-map (apply concat (map-indexed (fn [i x] [x i]) "0123456789X"))))
-
-(defn polynomial [c]
-  (apply + (map-indexed (fn [i x] (* (inc i) (int-map x))) (reverse c))))
+(defn modular-polynomial [c]
+  (let [int-map (zipmap "0123456789X" (range))]
+    (mod
+     (apply + (map #(mod (* %1 (int-map %2)) 11)
+                   (iterate inc 1)
+                   (reverse c)))
+     11)))
 
 (defn isbn? [s]
-  (->
-   s
-   clean
-   (as-> c
-         (and
-          (length-ok? c)
-          (x-only-last? c)
-          (zero? (mod (polynomial c) 11))))))
+  (let [c (map first (re-seq #"[0-9X]" s))]
+    (and
+     (= (count c) 10)
+     (x-only-last? c)
+     (zero? (modular-polynomial c)))))
