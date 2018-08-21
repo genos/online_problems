2052914d498169e20c656e4846ad40d9a768358d
genos <graham@quantifind.com>
2018-08-21 10:04:47 -0400

    fixed with suggestions from mentor

--- exercism/rust/bracket-push/src/lib.rs
+++ exercism/rust/bracket-push/src/lib.rs
@@ -1,8 +1,5 @@
 pub struct Brackets {
     ok: bool,
-    brack: i64,
-    brace: i64,
-    paren: i64,
     seen: Vec<char>,
 }
 
@@ -10,9 +7,6 @@ impl Default for Brackets {
     fn default() -> Self {
         Brackets {
             ok: true,
-            brack: 0,
-            brace: 0,
-            paren: 0,
             seen: Vec::new(),
         }
     }
@@ -23,39 +17,29 @@ impl<'a> From<&'a str> for Brackets {
         let mut b = Brackets::default();
         for c in input.chars() {
             match c {
-                '[' => {
-                    b.brack += 1;
-                    b.seen.push('[');
-                }
-                ']' => {
-                    b.brack -= 1;
-                    b.ok = b.seen.pop().map_or(false, |x| x == '[');
-                }
-                '{' => {
-                    b.brace += 1;
-                    b.seen.push('{');
-                }
-                '}' => {
-                    b.brace -= 1;
-                    b.ok = b.seen.pop().map_or(false, |x| x == '{');
-                }
-                '(' => {
-                    b.paren += 1;
-                    b.seen.push('(');
-                }
-                ')' => {
-                    b.paren -= 1;
-                    b.ok = b.seen.pop().map_or(false, |x| x == '(');
-                }
+                '[' | '{' | '(' => b.seen.push(c),
+                ']' | '}' | ')' => b.pop_and_pair(c),
                 _ => (),
             }
+            if !b.ok {
+                break;
+            }
         }
+        b.ok &= b.seen.is_empty();
         b
     }
 }
 
 impl Brackets {
     pub fn are_balanced(&self) -> bool {
-        self.ok && self.brack == 0 && self.brace == 0 && self.paren == 0
+        self.ok
+    }
+    fn pop_and_pair(&mut self, c: char) {
+        self.ok = self.seen.pop().map_or(false, |x| match c {
+            ']' => x == '[',
+            '}' => x == '{',
+            ')' => x == '(',
+            _ => panic!("Not a bracket pair character"),
+        });
     }
 }
