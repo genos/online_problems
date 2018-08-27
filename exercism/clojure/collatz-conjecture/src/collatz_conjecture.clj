047fe2721bdc2320b01ce9503a9237659172358a
genos <graham@quantifind.com>
2018-08-27 11:39:59 -0400

    mentor feedback

--- exercism/clojure/collatz-conjecture/src/collatz_conjecture.clj
+++ exercism/clojure/collatz-conjecture/src/collatz_conjecture.clj
@@ -1,9 +1,18 @@
 (ns collatz-conjecture)
 
+(defn- step [k]
+  (if
+    (even? k)
+    (quot k 2)
+    (inc (* 3 k))))
+
+(defn- sequence-length [k]
+  (count
+    (take-while
+      #(> % 1)
+      (iterate step k))))
+
 (defn collatz [n]
-  (if (< n 1) (throw (IllegalArgumentException. "n must be positive"))
-      (loop [k n
-             i 0]
-        (if (= k 1) i
-            (recur (if (even? k) (quot k 2) (inc (* 3 k)))
-                   (inc i))))))
+  (if (< n 1)
+    (throw (IllegalArgumentException. "n must be positive"))
+    (sequence-length n)))
