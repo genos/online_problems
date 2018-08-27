b333bca393595a106b4cf1008417bd232fe85408
genos <graham@quantifind.com>
2018-08-27 11:28:32 -0400

    better

--- exercism/rust/sublist/src/lib.rs
+++ exercism/rust/sublist/src/lib.rs
@@ -6,35 +6,37 @@ pub enum Comparison {
     Unequal,
 }
 
-pub fn sublist<T: PartialEq>(xs: &[T], ys: &[T]) -> Comparison {
-    if xs.len() >= ys.len() {
-        cmp_larger_short(xs, ys)
-    } else {
-        match cmp_larger_short(ys, xs) {
-            Comparison::Equal => Comparison::Equal,
+impl Comparison {
+    fn dual(self) -> Comparison {
+        match self {
             Comparison::Sublist => Comparison::Superlist,
             Comparison::Superlist => Comparison::Sublist,
-            Comparison::Unequal => Comparison::Unequal,
+            c => c,
         }
     }
 }
 
-// This works but is inefficient; I'd prefer a more intelligent search.
-fn cmp_larger_short<T: PartialEq>(larger: &[T], shorter: &[T]) -> Comparison {
+pub fn sublist<T: PartialEq>(xs: &[T], ys: &[T]) -> Comparison {
+    if xs.len() >= ys.len() {
+        cmp_larger_shorter(xs, ys)
+    } else {
+        cmp_larger_shorter(ys, xs).dual()
+    }
+}
+
+// This also works but is inefficient; I'd prefer a more intelligent search.
+fn cmp_larger_shorter<T: PartialEq>(larger: &[T], shorter: &[T]) -> Comparison {
     if larger.is_empty() {
         Comparison::Equal
     } else if shorter.is_empty() {
         Comparison::Superlist
-    } else {
-        for slice in larger.windows(shorter.len()) {
-            if slice == shorter {
-                if slice.len() == larger.len() {
-                    return Comparison::Equal;
-                } else {
-                    return Comparison::Superlist;
-                }
-            }
+    } else if larger.windows(shorter.len()).any(|slice| slice == shorter) {
+        if larger.len() == shorter.len() {
+            Comparison::Equal
+        } else {
+            Comparison::Superlist
         }
+    } else {
         Comparison::Unequal
     }
 }
