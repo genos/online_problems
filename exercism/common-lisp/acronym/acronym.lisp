fdb85cbe9c7f9a7563cdd164e36d6c367fe68b2f
genos <graham@quantifind.com>
2018-08-28 15:06:11 -0400

    move if

--- exercism/common-lisp/acronym/acronym.lisp
+++ exercism/common-lisp/acronym/acronym.lisp
@@ -5,15 +5,15 @@
 (in-package #:acronym)
 
 (defun acronym (s)
-  (if (string= s "")
-    ""
-    (string-upcase (coerce (initials s) 'string))))
+  (string-upcase (coerce (initials s) 'string)))
 
 ; With help from http://cl-cookbook.sourceforge.net/strings.html
 (defun initials (s)
-  (loop for i = 0 then (1+ j)
-        as j = (position-if
-                 #'(lambda (c) (or (char= c #\space) (char= c #\-)))
-                 s :start i)
-        collect (char s i)
-        while j))
+  (if (string= s "")
+    ""
+    (loop for i = 0 then (1+ j)
+          as j = (position-if
+                   #'(lambda (c) (or (char= c #\space) (char= c #\-)))
+                   s :start i)
+          collect (char s i)
+          while j)))
