a274bbd1d0dd1be7d71b257b518da62db07b82a5
genos <graham@quantifind.com>
2018-10-03 10:42:21 -0400

    following suggestions

--- exercism/common-lisp/collatz-conjecture/collatz-conjecture.lisp
+++ exercism/common-lisp/collatz-conjecture/collatz-conjecture.lisp
@@ -7,7 +7,6 @@
 (defun collatz (n)
   (when (and (integerp n) (plusp n))
     (loop
-      for k = n then (if (evenp k) (ash k -1) (+ k k k 1))
-      for steps = 0 then (1+ steps)
+      for k = n then (if (evenp k) (/ k 2) (1+ (* 3 k)))
       while (> k 1)
-      finally (return steps))))
+      counting k)))
