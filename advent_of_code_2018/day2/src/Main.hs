24f4ca58ec0ab8f14fa53ebbc890bd4909c913a8
genos <graham@quantifind.com>
2018-12-03 17:18:06 -0500

    spruce it up

--- advent_of_code_2018/day2/src/Main.hs
+++ advent_of_code_2018/day2/src/Main.hs
@@ -1,30 +1,26 @@
-{-# LANGUAGE BangPatterns #-}
 module Main where
 
+import           Control.Arrow   ((&&&))
 import           Control.Monad   ((<=<))
 import           Data.Bool       (bool)
-import           Data.Foldable   (find, foldl')
+import           Data.Foldable   (find)
 import           Data.List       (inits)
-import qualified Data.Map.Strict as M
+import           Data.Map.Strict (empty, insertWith)
+import           Data.Monoid     (Sum (..))
 import           Data.Text       (Text)
 import qualified Data.Text       as T
 import qualified Data.Text.IO    as T
 
 part1 :: [Text] -> Int
-part1 = uncurry (*) . foldl' f (0, 0)
+part1 = getSum . uncurry (*) . foldMap ((has 2 &&& has 3) . counts)
  where
-  f (!two, !three) t = (two + a, three + b)
-   where
-    cs = counts t
-    a  = bool 0 1 $ has 2 cs
-    b  = bool 0 1 $ has 3 cs
-    has n = not . M.null . M.filter (== n)
-    counts = T.foldl' (\m c -> M.insertWith (+) c (1 :: Word) m) M.empty
+  has n = Sum . bool 0 1 . elem n
+  counts = T.foldl' (\m c -> insertWith (+) c (1 :: Word) m) empty
 
 part2 :: [Text] -> Maybe Text
 part2 ts = search $ zip ts $ inits ts
  where
-  search  = find isProperLen <=< (find (any isProperLen) . fmap commons)
+  search = find isProperLen <=< (find (any isProperLen) . fmap commons)
   commons (x, xs) = fmap (commonChars x) xs
   commonChars a b =
     T.filter (/= ' ') $ T.zipWith (\x y -> if x == y then x else ' ') a b
