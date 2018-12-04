efacee8189e6846c28b0b35d9d136100acb0700a
genos <graham@quantifind.com>
2018-12-04 15:16:25 -0500

    day4, part1

---
+++ advent_of_code_2018/day4/src/Main.hs
@@ -0,0 +1,97 @@
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE NamedFieldPuns    #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections     #-}
+module Main where
+
+import           Control.Arrow        ((&&&))
+import           Data.Attoparsec.Text
+import           Data.Either          (fromRight)
+import           Data.Finite
+import           Data.Foldable        (foldl', maximumBy)
+import           Data.List            (sort)
+import           Data.List.Split      hiding (sepBy)
+import           Data.Map.Strict      (Map)
+import qualified Data.Map.Strict      as M
+import           Data.Maybe           (fromJust)
+import           Data.Ord             (comparing)
+import qualified Data.Text.IO         as T
+
+type Minute = Finite 60
+
+data DateTime = DT { _year   :: {-# UNPACK #-}!Word
+                   , _month  :: !(Finite 12)
+                   , _day    :: {-# UNPACK #-}!Word
+                   , _hour   :: !(Finite 24)
+                   , _minute :: Minute
+                   } deriving (Eq, Ord, Show)
+
+newtype Guard = G { _id :: Word } deriving (Eq, Ord, Show)
+
+data Action = BeginsShift | FallsAsleep | WakesUp deriving (Eq, Show)
+
+data Entry = E { _dateTime :: !DateTime
+               , _guard    :: Maybe Guard
+               , _action   :: !Action
+               } deriving (Eq, Show)
+
+instance Ord Entry where
+  (E dt1 _ _) <= (E dt2 _ _) = dt1 <= dt2
+
+dateTime :: Parser DateTime
+dateTime = do
+  _year   <- char '[' *> decimal
+  _month  <- char '-' *> decimal
+  _day    <- char '-' *> decimal
+  _hour   <- space *> decimal
+  _minute <- char ':' *> decimal <* char ']' <* space
+  pure DT {_year , _month , _day , _hour , _minute }
+
+guard :: Parser Guard
+guard = do
+  _id <- string "Guard #" *> decimal <* space
+  pure G {_id }
+
+action :: Parser Action
+action = choice $ zipWith (\a b -> string a >> pure b)
+                          ["begins shift", "falls asleep", "wakes up"]
+                          [BeginsShift, FallsAsleep, WakesUp]
+
+entry :: Parser Entry
+entry = do
+  _dateTime <- dateTime
+  _guard    <- option Nothing (Just <$> guard)
+  _action   <- action
+  pure E {_dateTime , _guard , _action }
+
+compile :: [Entry] -> Map Guard (Map Minute Word)
+compile =
+  M.fromListWith (M.unionWith (+))
+    . fmap tally
+    . tail
+    . split (keepDelimsL . whenElt $ (== BeginsShift) . _action)
+    . sort
+ where
+  tally = (fromJust . _guard . head) &&& (naps . tail)
+  naps  = foldl' (M.unionWith (+)) M.empty . fmap napped . chunksOf 2
+  napped [E df _ FallsAsleep, E dw _ WakesUp] =
+    M.fromListWith (+) . fmap (, 1) $ enumFromTo (_minute df) (_minute dw)
+  napped _ = M.empty
+
+sleepiest :: Map Guard (Map Minute Word) -> Guard
+sleepiest = fst . maximumBy (comparing snd) . M.toList . M.map sum
+
+part1 :: [Entry] -> Word
+part1 es = i * k
+ where
+  c = compile es
+  g = sleepiest c
+  k = fromIntegral $ fst $ maximumBy (comparing snd) $ M.toList (c M.! g)
+  i = _id g
+
+
+main :: IO ()
+main = do
+  entries <- (fromRight [] . parseOnly (entry `sepBy` char '\n'))
+    <$> T.readFile "input"
+  print . part1 $ entries
