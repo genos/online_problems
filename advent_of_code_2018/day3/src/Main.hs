e854eac7b7e2c54977f591fae64d5afa6bfab6a2
genos <graham@quantifind.com>
2018-12-04 12:21:01 -0500

    cleaner day3

--- advent_of_code_2018/day3/src/Main.hs
+++ advent_of_code_2018/day3/src/Main.hs
@@ -10,56 +10,49 @@ import           Data.Map.Strict      (Map)
 import qualified Data.Map.Strict      as Map
 import qualified Data.Text.IO         as T
 
-data Claim = Claim { elfId      :: {-# UNPACK #-}!Word
-                   , leftBorder :: {-# UNPACK #-}!Word
-                   , topBorder  :: {-# UNPACK #-}!Word
-                   , width      :: {-# UNPACK #-}!Word
-                   , height     :: {-# UNPACK #-}!Word
-                   } deriving Show
+data Rect = R { _left   :: {-# UNPACK #-}!Word
+              , _top    :: {-# UNPACK #-}!Word
+              , _width  :: {-# UNPACK #-}!Word
+              , _height :: {-# UNPACK #-}!Word
+              }
+
+data Claim = C { _id :: {-# UNPACK #-}!Word , _rect :: !Rect }
+
+rect :: Parser Rect
+rect = do
+  _left   <- decimal
+  _top    <- char ',' >> decimal
+  _width  <- char ':' >> space >> decimal
+  _height <- char 'x' >> decimal
+  pure R {_left , _top , _width , _height }
 
 claim :: Parser Claim
 claim = do
-  _          <- char '#'
-  elfId      <- decimal
-  _          <- space
-  _          <- char '@'
-  _          <- space
-  leftBorder <- decimal
-  _          <- char ','
-  topBorder  <- decimal
-  _          <- char ':'
-  _          <- space
-  width      <- decimal
-  _          <- char 'x'
-  height     <- decimal
-  pure Claim {elfId , leftBorder , topBorder , width , height }
-
-readClaims :: IO [Claim]
-readClaims =
-  (fromRight [] . parseOnly (claim `sepBy` char '\n')) <$> T.readFile "input"
+  _id   <- char '#' >> decimal
+  _rect <- space >> char '@' >> space >> rect
+  pure C {_id , _rect }
 
 covers :: Claim -> [(Word, Word)]
-covers (Claim _ l t w h) =
+covers (C _ (R l t w h)) =
   [ (x, y) | x <- [l .. l + w - 1], y <- [t .. t + h - 1] ]
 
 coverage :: [Claim] -> Map (Word, Word) Word
-coverage = foldl' cover Map.empty
+coverage = foldl' f Map.empty . fmap covers
  where
-  cover fabric =
-    foldl' (\m (x, y) -> Map.insertWith (+) (x, y) 1 m) fabric . covers
+  f = foldl' (\m xy -> Map.insertWith (+) xy 1 m)
 
 part1 :: [Claim] -> Word
-part1 = Map.foldl' (+) 0 . Map.map (bool 0 1 . (> 1)) . coverage
+part1 = sum . Map.map (bool 0 1 . (> 1)) . coverage
 
 part2 :: [Claim] -> Maybe Word
-part2 claims = elfId <$> find solo claims
-  where
-    solo = all p . covers
-    cs = coverage claims
-    p k = (== 1) $ Map.findWithDefault 1 k cs
+part2 cs = _id <$> find p cs
+ where
+  p = all (\k -> (== 1) $ Map.findWithDefault 1 k c) . covers
+  c = coverage cs
 
 main :: IO ()
 main = do
-  claims <- readClaims
-  print $ part1 claims
-  print $ part2 claims
+  cs <- (fromRight [] . parseOnly (claim `sepBy` char '\n'))
+    <$> T.readFile "input"
+  print $ part1 cs
+  print $ part2 cs
