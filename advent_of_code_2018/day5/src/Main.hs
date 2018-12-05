65cfd623918553c6e2d2313db63aeeff71ba5ec9
genos <graham@quantifind.com>
2018-12-05 15:44:50 -0500

    slight tweak to day 5

--- advent_of_code_2018/day5/src/Main.hs
+++ advent_of_code_2018/day5/src/Main.hs
@@ -2,18 +2,18 @@ module Main where
 
 import Data.Char (isLetter, toLower)
 
-react :: String -> Int
-react = length . foldr step ""
+react :: String -> String
+react = foldr step ""
  where
   step x (y : ys) | x /= y && toLower x == toLower y = ys
   step x ys       = x : ys
 
 part1 :: String -> Int
-part1 = react
+part1 = length . react
 
 part2 :: String -> Int
-part2 s = minimum $ fmap (react . remove) ['a' .. 'z']
-  where remove c = filter ((/= c) . toLower) s
+part2 s = minimum $ fmap (length . react . remove) ['a' .. 'z']
+  where remove c = filter ((/= c) . toLower) (react s) -- save recomputation
 
 main :: IO ()
 main = do
