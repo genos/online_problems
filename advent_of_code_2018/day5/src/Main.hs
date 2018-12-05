c5b902d49620073b0cdf2e9bf7df2bc7f20858dc
genos <graham@quantifind.com>
2018-12-05 14:57:10 -0500

    day5 (with help)

---
+++ advent_of_code_2018/day5/src/Main.hs
@@ -0,0 +1,23 @@
+module Main where
+
+import Data.Char     (isLetter, toLower)
+
+react :: String -> Int
+react = length . foldr step ""
+ where
+  step x (y : ys) | x /= y && toLower x == toLower y = ys
+  step x ys       = x : ys
+
+part1 :: String -> Int
+part1 = react
+
+part2 :: String -> Int
+part2 s = minimum $ fmap (react . remove) ['a' .. 'z']
+  where
+    remove c = filter ((/= c) . toLower) s
+
+main :: IO ()
+main = do
+  input <- filter isLetter <$> readFile "input"
+  print . part1 $ input
+  print . part2 $ input
