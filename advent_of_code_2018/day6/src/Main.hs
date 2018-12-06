a9d5654f960b78f0f0751d431f65262b2b2afb8a
genos <graham@quantifind.com>
2018-12-06 13:49:03 -0500

    tidy up

--- advent_of_code_2018/day6/src/Main.hs
+++ advent_of_code_2018/day6/src/Main.hs
@@ -3,9 +3,9 @@ module Main where
 import           Control.Arrow        ((&&&))
 import           Control.Lens         ((^.))
 import qualified Data.Attoparsec.Text as P
+import           Data.Foldable        (traverse_)
 import           Data.Ix              (range)
 import           Data.List            (sortOn)
-import           Data.Map.Strict      (Map)
 import qualified Data.Map.Strict      as M
 import qualified Data.Text.IO         as T
 import           Linear.V2
@@ -20,36 +20,33 @@ taxicab v w = sum . abs $ v - w
 
 bounds :: Int -> [V] -> (V, V)
 bounds n vs = (a - nv, b + nv)
-  where
-    xs = fmap (^. _x) vs
-    ys = fmap (^. _y) vs
-    a  = V2 (minimum xs) (minimum ys)
-    b  = V2 (maximum xs) (maximum ys)
-    nv = V2 n n
-
-grid :: Int -> [V] -> [V]
-grid n = range . bounds n
-
-closest :: [V] -> V -> [V]
-closest vs v = [ c0 | d0 /= d1 ]
  where
-  [(d0, c0), (d1, _c1)] = take 2 . sortOn fst $ fmap (taxicab v &&& id) vs
+  xs = fmap (^. _x) vs
+  ys = fmap (^. _y) vs
+  a  = V2 (minimum xs) (minimum ys)
+  b  = V2 (maximum xs) (maximum ys)
+  nv = V2 n n
 
-tally :: (Ord a) => [a] -> Map a Word
-tally = M.fromListWith (+) . fmap (id &&& const 1)
+near :: [V] -> V -> [V]
+near vs v = [ c | d0 /= d1 ]
+ where
+  [(d0, c), (d1, _c)] = take 2 . sortOn fst $ fmap (taxicab v &&& id) vs
 
-part1 :: [V] -> Word
-part1 vs = maximum . tally . concatMap (closest vs) $ grid 0 vs
-
-totalCabs :: [V] -> V -> Int
-totalCabs vs v = sum $ fmap (taxicab v) vs
+part1 :: [V] -> Int
+part1 vs = maximum . tally . concatMap (near vs) . filter finite $ range (l, u)
+ where
+  tally  = M.fromListWith (+) . fmap (id &&& const 1)
+  (l, u) = bounds 0 vs
+  finite (V2 x y) =
+    and $ zipWith (/=) [x, y, x, y] [l ^. _x, l ^. _y, u ^. _x, u ^. _y]
 
 part2 :: [V] -> Int
-part2 vs = length inRange
-  where
-    n = 100000
-    (l, u) = bounds n vs
-    inRange = filter ((< 10000) . totalCabs vs) $ grid (max 0 (n - taxicab l u)) vs
+part2 vs = length . filter ((< n) . cabsTotal) . range $ bounds n' vs
+ where
+  n      = 10000
+  (l, u) = bounds n vs
+  n'     = max 0 (n - taxicab l u)
+  cabsTotal v = sum $ fmap (taxicab v) vs
 
 main :: IO ()
 main = do
@@ -57,5 +54,4 @@ main = do
   let vecs = case P.parseOnly (vec `P.sepBy` P.char '\n') input of
         Left  err -> error err
         Right vs  -> vs
-  print $ part1 vecs
-  print $ part2 vecs
+  traverse_ (print . ($ vecs)) [part1, part2]
