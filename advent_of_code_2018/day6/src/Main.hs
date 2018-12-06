fc0e019130d354e5e2e86ad9233cba6b2b0ed19a
genos <graham@quantifind.com>
2018-12-06 13:19:01 -0500

    solved day 6

--- advent_of_code_2018/day6/src/Main.hs
+++ advent_of_code_2018/day6/src/Main.hs
@@ -3,43 +3,53 @@ module Main where
 import           Control.Arrow        ((&&&))
 import           Control.Lens         ((^.))
 import qualified Data.Attoparsec.Text as P
-import           Data.Ix
+import           Data.Ix              (range)
 import           Data.List            (sortOn)
+import           Data.Map.Strict      (Map)
 import qualified Data.Map.Strict      as M
 import qualified Data.Text.IO         as T
 import           Linear.V2
 
-type Vec = V2 Int
+type V = V2 Int
 
-vec :: P.Parser Vec
+vec :: P.Parser V
 vec = V2 <$> P.decimal <*> (P.char ',' *> P.space *> P.decimal)
 
-taxicab :: Vec -> Vec -> Int
+taxicab :: V -> V -> Int
 taxicab v w = sum . abs $ v - w
 
-bounds :: [Vec] -> (Int, Int, Int, Int)
-bounds vs = (minimum xs, maximum xs, minimum ys, maximum ys)
- where
-  xs = fmap (^. _x) vs
-  ys = fmap (^. _y) vs
+bounds :: Int -> [V] -> (V, V)
+bounds n vs = (a - nv, b + nv)
+  where
+    xs = fmap (^. _x) vs
+    ys = fmap (^. _y) vs
+    a  = V2 (minimum xs) (minimum ys)
+    b  = V2 (maximum xs) (maximum ys)
+    nv = V2 n n
 
-grid :: [Vec] -> [Vec]
-grid vs = range (V2 xMin yMin, V2 xMax yMax)
-  where (xMin, xMax, yMin, yMax) = bounds vs
+grid :: Int -> [V] -> [V]
+grid n = range . bounds n
 
-closest :: [Vec] -> Vec -> [Vec]
-closest vs v = [c0 | d0 /= d1]
+closest :: [V] -> V -> [V]
+closest vs v = [ c0 | d0 /= d1 ]
  where
-  cabs                  = sortOn fst $ fmap (taxicab v &&& id) vs
-  [(d0, c0), (d1, _c1)] = take 2 cabs
+  [(d0, c0), (d1, _c1)] = take 2 . sortOn fst $ fmap (taxicab v &&& id) vs
 
-area :: [Vec] -> Int
-area vs = maximum ms
- where
-  closests = concatMap (closest vs) $ grid vs
-  (xMin, xMax, yMin, yMax) = bounds vs
-  within (V2 x y) = x /= xMin && x /= xMax && y /= yMin && y /= yMax
-  ms = M.fromListWith (+) . fmap (id &&& const 1) $ filter within closests
+tally :: (Ord a) => [a] -> Map a Word
+tally = M.fromListWith (+) . fmap (id &&& const 1)
+
+part1 :: [V] -> Word
+part1 vs = maximum . tally . concatMap (closest vs) $ grid 0 vs
+
+totalCabs :: [V] -> V -> Int
+totalCabs vs v = sum $ fmap (taxicab v) vs
+
+part2 :: [V] -> Int
+part2 vs = length inRange
+  where
+    n = 100000
+    (l, u) = bounds n vs
+    inRange = filter ((< 10000) . totalCabs vs) $ grid (max 0 (n - taxicab l u)) vs
 
 main :: IO ()
 main = do
@@ -47,4 +57,5 @@ main = do
   let vecs = case P.parseOnly (vec `P.sepBy` P.char '\n') input of
         Left  err -> error err
         Right vs  -> vs
-  print $ area vecs
+  print $ part1 vecs
+  print $ part2 vecs
