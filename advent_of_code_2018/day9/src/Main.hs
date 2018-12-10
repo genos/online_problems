050dbe74dadbd69d53c16cb4d6a681cf6e973c42
genos <graham@quantifind.com>
2018-12-10 11:31:05 -0500

    tidying

--- advent_of_code_2018/day9/src/Main.hs
+++ advent_of_code_2018/day9/src/Main.hs
@@ -10,7 +10,7 @@ import           Data.Bifunctor                 (first)
 import           Data.Foldable                  (foldl')
 import           Data.Function                  ((&))
 import           Data.List.PointedList.Circular (PointedList (..))
-import qualified Data.List.PointedList.Circular as PL
+import qualified Data.List.PointedList.Circular as C
 import           Data.Maybe                     (fromJust)
 import qualified Data.Text.IO                   as T
 import           Data.Vector.Unboxed            (Vector)
@@ -18,11 +18,13 @@ import qualified Data.Vector.Unboxed            as V
 
 data Game = G { _scores :: !(Vector Int), _lastMarble :: {-# UNPACK #-}!Int }
 
+type Circle = PointedList Int
+
 numPlayers :: Game -> Int
 numPlayers = V.length . _scores
 
 make :: Int -> Int -> Game
-make np = G (V.replicate np 0)
+make np = G $ V.replicate np 0
 
 gameP :: P.Parser Game
 gameP =
@@ -30,17 +32,17 @@ gameP =
     <$> (P.decimal <* P.string " players; last marble is worth ")
     <*> (P.decimal <* P.string " points")
 
-move :: Int -> PointedList Int -> (Int, PointedList Int)
-move p c | p `mod` 23 /= 0 = (0, PL.insertLeft p $ PL.moveN 2 c)
-         | otherwise       = (p + PL._focus c', fromJust $ PL.deleteRight c')
-  where c' = PL.moveN (-7) c
+move :: Circle -> Int -> (Int, Circle)
+move c p | p `mod` 23 /= 0 = (0, C.insertLeft p $ C.moveN 2 c)
+         | otherwise       = (p + C._focus c', fromJust $ C.deleteRight c')
+  where c' = C.moveN (-7) c
 
 play :: Game -> Vector Int
 play g =
-  fst . foldl' go (_scores g, PL.singleton 0) $ zip moves pieces
+  fst . foldl' go (_scores g, C.singleton 0) $ zip moves pieces
  where
-  go (!scores, !circle) (!turn, !piece) =
-    first (\x -> scores & ix turn +~ x) $ move piece circle
+  go (!ss, !circle) (!turn, !piece) =
+    first (\points -> ss & ix turn +~ points) $ move circle piece
   moves  = [i `mod` numPlayers g | i <- [0 ..]]
   pieces = [1 .. _lastMarble g]
 
@@ -48,14 +50,11 @@ part1 :: Game -> Int
 part1 = V.maximum . play
 
 part1Tests :: Vector Bool
-part1Tests = V.zipWith3 (\np mp s -> s == part1 (make np mp))
-                        players
-                        lastMarble
-                        scores
+part1Tests = V.zipWith3 (\np mp s -> s == part1 (make np mp)) ps lm ss
  where
-  players    = V.fromList [10, 13, 17, 21, 30]
-  lastMarble = V.fromList [1618, 7999, 1104, 6111, 5807]
-  scores     = V.fromList [8317, 146373, 2764, 54718, 37305]
+  ps = V.fromList [10, 13, 17, 21, 30]
+  lm = V.fromList [1618, 7999, 1104, 6111, 5807]
+  ss = V.fromList [8317, 146373, 2764, 54718, 37305]
 
 part2 :: Game -> Int
 part2 g = V.maximum . play $ g { _lastMarble = 100 * _lastMarble g }
