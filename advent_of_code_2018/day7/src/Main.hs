c056aa8b00a67bf0253c6684dbe50ea800c0be49
genos <graham@quantifind.com>
2018-12-10 13:28:55 -0500

    done day7 part2, with help

--- advent_of_code_2018/day7/src/Main.hs
+++ advent_of_code_2018/day7/src/Main.hs
@@ -1,9 +1,14 @@
+{-# LANGUAGE BangPatterns      #-}
 {-# LANGUAGE OverloadedStrings #-}
+
 
 module Main where
 
 import qualified Data.Attoparsec.Text as P
+import           Data.Bifunctor       (second)
 import           Data.Char            (ord)
+import           Data.Foldable        (foldl')
+import           Data.List            (partition)
 import qualified Data.Map             as M
 import           Data.Map.Strict      (Map)
 import           Data.Set             (Set)
@@ -26,38 +31,44 @@ parseInput = do
     Left  l -> error l
     Right r -> pure . M.fromListWith S.union $ concat r
 
+clear :: Dependencies -> Char -> Dependencies
+clear deps v = M.map (S.delete v) $ M.delete v deps
+
+nextReady :: Dependencies -> Char
+nextReady = fst . M.findMin . M.filter S.null
+
 part1 :: Dependencies -> String  -- lexicographic-topological sort
 part1 = go ""
  where
-  go str deps | M.null deps  = reverse str
-              | otherwise    = go (v : str) deps'
-   where
-    v     = fst . M.findMin . M.filter S.null $ deps
-    deps' = M.map (S.delete v) $ M.delete v deps
+  go str deps | M.null deps = reverse str
+              | otherwise   = go (v : str) (clear deps v)
+    where v = nextReady deps
 
 part2 :: Dependencies -> Int
-part2 = go 0 . assign 0
+part2 deps = go deps (replicate 5 worker) 0
  where
-  assign :: Int -> Dependencies -> (Map Char Int, Dependencies)
-  assign n deps = (working, rest)
-    where
-      working = M.mapWithKey cost . M.take (5 - n) . M.filter S.null $ deps
-      w       = M.keysSet working
-      rest    = M.withoutKeys (M.map (S.\\ w) deps) w
-  cost :: Char -> Set Char -> Int
-  cost key = const $ ord key - 4
-  go :: Int -> (Map Char Int, Dependencies) -> Int
-  go time (workers, deps) | M.null deps = time + maximum workers
-                          | otherwise   = go time' (workers', deps')
+  worker  = (' ', -1)
+  go !ds !workers !time
+    | M.null ds = time + maximum (fmap snd workers)
+    | otherwise =
+      let elapsed            = min0 $ filter (> 0) (fmap snd workers)
+          workers'           = fmap (second $ subtract elapsed) workers
+          (done, working)    = partition ((<= 0) . snd) workers'
+          cleared            = foldl' clear ds $ fmap fst done
+          (workers'', deps') = assign (working, cleared) (length done)
+      in  go deps' workers'' (time + elapsed)
    where
-     elapsed           = if null workers then 0 else minimum workers
-     time'             = time + elapsed
-     working           = M.filter (> 0) $ M.map (subtract elapsed) workers
-     (starting, deps') = assign (length working) deps
-     workers'          = M.union working starting
+    min0 ts = if null ts then 0 else minimum ts
+    assign (w, d) n
+      | n <= 0                     = (w, d)
+      | M.null $ M.filter S.null d = assign (worker : w, d) n'
+      | otherwise                  = assign ((v, c) : w, M.delete v d) n'
+      where v = nextReady d
+            c = ord v - 4
+            n' = n - 1
 
 main :: IO ()
 main = do
   deps <- parseInput
-  putStrLn . part1 $ deps
-  print . part2 $ deps
+  putStrLn $ part1 deps
+  print $ part2 deps
