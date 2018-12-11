e51ca62ecc23a7ecf3057be4b388e575717572d1
genos <graham@quantifind.com>
2018-12-11 15:48:39 -0500

    part2?

--- advent_of_code_2018/day11/src/Main.hs
+++ advent_of_code_2018/day11/src/Main.hs
@@ -1,23 +1,26 @@
 module Main where
 
 import           Control.Arrow   ((&&&))
-import           Data.Foldable   (foldl', maximumBy)
+import           Data.Foldable   (maximumBy)
 import           Data.Ix         (range)
 import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
 import           Data.Ord        (comparing)
 import           Linear.V2       (V2 (..))
+import           Data.Vector     (Vector)
+import qualified Data.Vector     as V
 
-type Cell = V2 Int
+type Cell  = V2 Int
+type Table = Map Cell Int
 
 minCell :: Cell
 minCell = V2 1 1
 
-maxCoord :: Int
-maxCoord = 300
+maxSize :: Int
+maxSize = 300
 
 maxCell :: Cell
-maxCell = V2 maxCoord maxCoord
+maxCell = V2 maxSize maxSize
 
 grid :: [Cell]
 grid = range (minCell, maxCell)
@@ -28,6 +31,7 @@ power serial (V2 x y) = hundreds - 5
   rackID   = x + 10
   level    = (rackID * y + serial) * rackID
   hundreds = (level `div` 100) `mod` 10
+{-# INLINE power #-}
 
 testPower :: Bool
 testPower = and $ zipWith3 (\c s p -> power s c == p)
@@ -35,47 +39,61 @@ testPower = and $ zipWith3 (\c s p -> power s c == p)
                            [57, 39, 71]
                            [-5, 0, 4]
 
-powerTable :: Int -> Map Cell Int
+powerTable :: Int -> Table
 powerTable serial = M.fromList $ fmap (id &&& power serial) grid
 
-sum' :: Foldable f => f Int -> Int
-sum' = foldl' (+) 0
-
-neighbors :: Int -> Cell -> [Cell]
-neighbors size c@(V2 x y) = range (c, V2 x' y')
+neighbors :: Int -> Cell -> Vector Cell
+neighbors size c@(V2 x y) = V.fromList $ range (c, V2 x' y')
   where
-    x' = min maxCoord (x + size)
-    y' = min maxCoord (y + size)
+    x' = min maxSize (x + size)
+    y' = min maxSize (y + size)
 
 largest :: Int -> Int -> Cell
 largest serial size = maximumBy (comparing totalPower) grid
  where
   table        = powerTable serial
-  totalPower c = sum' [ table M.! n | n <- neighbors size c ]
+  totalPower c = sum $ fmap (table M.!) (neighbors size c)
 
 part1 :: String
 part1 = show x <> "," <> show y
   where (V2 x y) = largest 6878 2
 
--- largest' :: Int -> (Cell, Int)
--- largest' serial = maximumBy (comparing $ tp)
---   [ (cell, size) | cell <- grid, size <- range (0, maxCoord) ]
---     where
---       pm = powerTable serial
---       totalPower c
+bestInTable :: Table -> Cell
+bestInTable table = maximumBy (comparing (table M.!)) grid
 
--- part2 :: (Cell, Int)
--- part2 = largest' 6878
+powerTables :: Int -> Vector Table
+powerTables serial = V.unfoldrN maxSize f (1, p)
+  where
+    p = powerTable serial
+    f :: (Int, Table) -> Maybe (Table, (Int, Table))
+    f (size, table) = Just (table, (size + 1, M.fromList $ fmap g grid))
+      where
+        g :: Cell -> (Cell, Int)
+        g cell = (cell, sum $ fmap (p M.!) (neighbors (size - 1) cell))
 
--- testLargest' :: Bool
--- testLargest' = and $ zipWith3
---   (\serial cell size -> largest' serial == (cell, size))
---   [18, 42]
---   [V2 90 269, V2 232 251]
---   [16, 12]
+largest' :: Int -> (Cell, Int)
+largest' serial = (cell, size)
+  where
+    cell   = fst $ pairs V.! size
+    size   = V.maxIndex pairs
+    pairs  = V.zipWith (\t b -> (b, t M.! b)) tables bests
+    bests  = fmap bestInTable tables
+    tables = powerTables serial
+
+testLargest' :: Bool
+testLargest' = and $ zipWith3
+  (\serial cell size -> largest' serial == (cell, size))
+  [18, 42]
+  [V2 90 269, V2 232 251]
+  [16, 12]
+
+part2 :: String
+part2 = show x <> "," <> show y <> "," <> show size
+  where (V2 x y, size) = largest' 6878
 
 main :: IO ()
 main = do
   print testPower
   putStrLn part1
-  --print testLargest'
+  print testLargest'
+  putStrLn part2
