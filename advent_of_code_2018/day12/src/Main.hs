bc416ffdff795c3ec9cb0856f02673c05782a660
genos <graham@quantifind.com>
2018-12-12 18:24:26 -0500

    part2 too slow

--- advent_of_code_2018/day12/src/Main.hs
+++ advent_of_code_2018/day12/src/Main.hs
@@ -2,10 +2,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import           Control.Arrow        ((&&&))
 import qualified Data.Attoparsec.Text as P
+import           Data.Foldable        (traverse_)
 import           Data.IntSet          (IntSet)
 import qualified Data.IntSet          as I
-import           Data.Ix              (range)
 import           Data.Maybe           (catMaybes)
 import           Data.Set             (Set)
 import qualified Data.Set             as S
@@ -13,14 +14,15 @@ import           Data.Text            (Text)
 import qualified Data.Text            as T
 import qualified Data.Text.IO         as T
 
-type Rule  = IntSet  -- only keep those with alive outputs
-type State = IntSet  -- only keep alive positions
+type Rule = IntSet  -- only keep those with alive outputs
+type Pots = IntSet  -- only keep alive positions
 
 isPot :: Char -> Bool
 isPot = (||) <$> (== '#') <*> (== '.')
 
 readAlive :: Text -> IntSet
-readAlive = I.fromList . fmap fst . filter ((== '#') . snd) . zip [0 ..] . T.unpack
+readAlive =
+  I.fromList . fmap fst . filter ((== '#') . snd) . zip [0 ..] . T.unpack
 
 ruleP :: P.Parser (Maybe Rule)
 ruleP = do
@@ -28,29 +30,43 @@ ruleP = do
   right <- P.string " => " *> P.satisfy isPot
   pure $! if right == '.' then Nothing else Just $! readAlive left
 
-stateP :: P.Parser State
-stateP = do
-  _      <- P.string "initial state: "
-  plants <- P.takeWhile isPot
-  pure $! readAlive plants
+potsP :: P.Parser Pots
+potsP = readAlive <$> (P.string "initial state: " *> P.takeWhile isPot)
 
-inputP :: P.Parser (State, Set Rule)
+inputP :: P.Parser (Pots, Set Rule)
 inputP = do
-  state <- stateP <* P.skipSpace
+  pots  <- potsP <* P.skipSpace
   rules <- (ruleP `P.sepBy` P.char '\n') <* P.char '\n'
-  pure (state, S.fromList $! catMaybes rules)
+  pure (pots, S.fromList $! catMaybes rules)
 
-parseInput :: IO (State, Set Rule)
+parseInput :: IO (Pots, Set Rule)
 parseInput = do
   input <- T.readFile "input"
   either error pure $! P.parseOnly inputP input
 
-step :: Set Rule -> State -> State
-step rules state =
-  I.fromList . filter alive $ range (I.findMin state - 5, I.findMax state + 5)
-    where
-      alive i     = (neighbors i) `S.member` rules
-      neighbors i = _todo -- bad: I.fromList $ range (i - 2, i + 2)
+spread :: Int -> Int -> [Int]
+spread lo hi = [lo - 2 .. hi + 2]
+
+step :: Set Rule -> Pots -> Pots
+step rules pots =
+  I.fromList . filter alive . uncurry spread $ (I.findMin &&& I.findMax) pots
+ where
+  alive i = neighbors i `S.member` rules
+  neighbors i =
+    I.fromList . fmap ((+ 2) . subtract i) . filter (`I.member` pots) $ spread
+      i
+      i
+
+run :: Int -> Pots -> Set Rule -> Int
+run n pots rules = I.foldl' (+) 0 . (!! n) $ iterate (step rules) pots
+
+part1 :: Pots -> Set Rule -> Int
+part1 = run 20
+
+part2 :: Pots -> Set Rule -> Int
+part2 = run 50000000000
 
 main :: IO ()
-main = parseInput >>= print
+main = do
+  (pots, rules) <- parseInput
+  traverse_ (print . (\p -> p pots rules)) [part1, part2]
