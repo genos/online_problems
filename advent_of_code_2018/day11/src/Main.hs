279535cfab8e5508dff55736f9f0c2892eb57813
genos <graham@quantifind.com>
2018-12-11 23:07:10 -0500

    summed area tables (with help)

--- advent_of_code_2018/day11/src/Main.hs
+++ advent_of_code_2018/day11/src/Main.hs
@@ -1,99 +1,67 @@
 module Main where
 
-import           Control.Arrow   ((&&&))
-import           Data.Foldable   (maximumBy)
+import           Data.Foldable   (foldl', maximumBy)
 import           Data.Ix         (range)
 import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
 import           Data.Ord        (comparing)
-import           Data.Vector     (Vector)
-import qualified Data.Vector     as V
 import           Linear.V2       (V2 (..))
 
 type Cell  = V2 Int
 type Table = Map Cell Int
 
-minCell :: Cell
-minCell = V2 1 1
-
-maxSize :: Int
-maxSize = 300
-
-maxCell :: Cell
-maxCell = V2 maxSize maxSize
-
-grid :: [Cell]
-grid = range (minCell, maxCell)
-
-trimmedGrid :: Int -> [Cell]
-trimmedGrid size = range (minCell, maxCell - V2 size size)
-
-power :: Int -> Cell -> Int
-power serial (V2 x y) = hundreds - 5
- where
-  rackID   = x + 10
-  level    = (rackID * y + serial) * rackID
-  hundreds = (level `div` 100) `mod` 10
-{-# INLINE power #-}
-
-testPower :: Bool
-testPower = and $ zipWith3 (\c s p -> power s c == p)
-                           [V2 122 79, V2 217 196, V2 101 153]
-                           [57, 39, 71]
-                           [-5, 0, 4]
-
-powerTable :: Int -> Table
-powerTable serial = M.fromList $ fmap (id &&& power serial) grid
-
-neighbors :: Int -> Cell -> Vector Cell
-neighbors size c@(V2 x y) = V.fromList $ range (c, V2 x' y')
- where
-  x' = min maxSize (x + size)
-  y' = min maxSize (y + size)
-
-largest :: Int -> Int -> Cell
-largest serial size = maximumBy (comparing totalPower) (trimmedGrid size)
- where
-  table = powerTable serial
-  totalPower c = sum $ fmap (table M.!) (neighbors size c)
-
-part1 :: String
-part1 = show x <> "," <> show y where (V2 x y) = largest 6878 2
-
-bestInTable :: Table -> (Cell, Int)
-bestInTable = maximumBy (comparing snd) . M.assocs
-
-powerTables :: Int -> Vector Table
-powerTables serial = V.unfoldrN maxSize (Just . f) (1, p)
- where
-  p               = powerTable serial
-  f (size, table) = (table, (size + 1, M.fromList $ fmap (id &&& g) t))
-   where
-    t = trimmedGrid size
-    g = sum . fmap (p M.!) . (neighbors $ size - 1)
-
-largest' :: Int -> (Cell, Int)
-largest' serial = (cell, size)
- where
-  cell   = fst $ bests V.! size
-  size   = V.maxIndex bests
-  bests  = fmap bestInTable tables
-  tables = powerTables serial
-
-testLargest' :: Bool
-testLargest' = and $ zipWith3
-  (\serial cell size -> largest' serial == (cell, size))
-  [18, 42]
-  [V2 90 269, V2 232 251]
-  [16, 12]
-
-part2 :: String
-part2 = show x <> "," <> show y <> "," <> show size
-  where (V2 x y, size) = largest' 6878
-
 main :: IO ()
 main = do
-  print testPower
-  putStrLn part1
-  print testLargest'
-  putStrLn part2
+  let g = makeTable 6878
+  print $ part1 g
+  print $ part2 g
+
+part1 :: Table -> Cell
+part1 table = fst . keyAndMaxValue $ allSubCellPower 3 table
+
+part2 :: Table -> (Cell, Int)
+part2 table =
+  fst . maximumBy (comparing snd) . fmap (bestIn table) $ range (1, 300)
+
+makeTable :: Int -> Table
+makeTable serial = foldl'
+  add
+  M.empty
+  [ (c, power serial c) | c <- range (pure 1, pure 300) ]
+
+add :: Table -> (Cell, Int) -> Table
+add table (c@(V2 x y), p) = M.insert c (p + upper + left - upperLeft) table
+ where
+  upper     = M.findWithDefault 0 (V2 (x - 1) y) table
+  left      = M.findWithDefault 0 (V2 x (y - 1)) table
+  upperLeft = M.findWithDefault 0 (V2 (x - 1) (y - 1)) table
+
+power :: Int -> Cell -> Int
+power _      (V2 0 _) = 0
+power _      (V2 _ 0) = 0
+power serial (V2 x y) = ((interim `div` 100) `mod` 10) - 5
+ where
+  rackID  = x + 10
+  interim = (rackID * y + serial) * rackID
+
+subCellPower :: Int -> Cell -> Table -> Int
+subCellPower size (V2 x0 y0) t = f x y + f x' y' - f x y' - f x' y
+ where
+  f a b = M.findWithDefault 0 (V2 a b) t
+  x  = x0 - 1
+  x' = x + size
+  y  = y0 - 1
+  y' = y + size
+
+allSubCellPower :: Int -> Table -> Table
+allSubCellPower size table = M.fromList
+  [ (c, subCellPower size c table) | c <- range (pure 1, pure (301 - size)) ]
+
+keyAndMaxValue :: Ord b => M.Map a b -> (a, b)
+keyAndMaxValue m = M.foldrWithKey mergeKV (M.findMin m) m
+ where
+  mergeKV k v (bestK, bestV) = if v > bestV then (k, v) else (bestK, bestV)
+
+bestIn :: Table -> Int -> ((Cell, Int), Int)
+bestIn table size = ((V2 x y, size), p)
+  where (V2 x y, p) = keyAndMaxValue $ allSubCellPower size table
