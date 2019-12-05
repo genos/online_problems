e10cfcbb7d06c3b65b0d3f2966c24a6d8380843c
genos <graham@quantifind.com>
2019-12-04 22:04:22 -0500

    part2 done

--- advent_of_code_2019/day03/src/Main.hs
+++ advent_of_code_2019/day03/src/Main.hs
@@ -1,6 +1,10 @@
+{-# LANGUAGE BangPatterns #-}
 module Main where
 
-import           Data.Foldable              (foldl', traverse_)
+import           Data.Foldable              (foldl')
+import           Data.Functor               (($>))
+import           Data.Map.Strict            (Map)
+import qualified Data.Map.Strict            as M
 import           Data.Set                   (Set)
 import qualified Data.Set                   as S
 import           Data.Text                  (Text)
@@ -16,10 +20,7 @@ data Direction = U | D | L | R deriving Eq
 
 pDirection :: Parser Direction
 pDirection =
-  (char 'U' *> pure U)
-    <|> (char 'D' *> pure D)
-    <|> (char 'L' *> pure L)
-    <|> (char 'R' *> pure R)
+  (char 'U' $> U) <|> (char 'D' $> D) <|> (char 'L' $> L) <|> (char 'R' $> R)
 
 data Step = Step { _direction :: !Direction, _distance :: {-# UNPACK #-}!Int }
 
@@ -37,32 +38,51 @@ readSteps = do
   raw <- T.readFile "input"
   pure $ either (error . errorBundlePretty) id $ runParser pCircuits "input" raw
 
-type Coord = (Int, Int)
+type XY = (Int, Int)
 
-step :: Direction -> Coord -> Coord
+step :: Direction -> XY -> XY
 step U (x, y) = (x, y + 1)
 step D (x, y) = (x, y - 1)
 step L (x, y) = (x - 1, y)
 step R (x, y) = (x + 1, y)
 
-path :: (Coord, Set Coord) -> Step -> (Coord, Set Coord)
-path (c, cs) (Step dir dist) = (last ss, S.fromList (init ss) <> cs)
-  where ss = take (dist + 1) $ iterate (step dir) c
+walk :: XY -> Step -> [XY]
+walk xy (Step dir dist) = take (dist + 1) $ iterate (step dir) xy
 
-wires :: [Step] -> Set Coord
-wires = snd . foldl' path ((0, 0), mempty)
+wires :: [Step] -> Set XY
+wires = snd . foldl' go ((0, 0), mempty)
+ where
+  go (!xy, !xys) !s =
+    let ss = walk xy s in (last ss, S.fromList (init ss) <> xys)
 
-taxi :: Coord -> Int
-taxi (x, y) = abs x + abs y
-
-crossings :: ([Step], [Step]) -> Set Coord
+crossings :: ([Step], [Step]) -> Set XY
 crossings (a, b) = S.filter (/= (0, 0)) $ S.intersection (wires a) (wires b)
 
 part1 :: ([Step], [Step]) -> Int
-part1 = minimum . S.map taxi . crossings
+part1 = minimum . S.map taxi . crossings where taxi (x, y) = abs x + abs y
+
+build :: [Step] -> Map XY Int
+build = fin . foldl' go (0, (0, 0), mempty)
+ where
+  fin (_, _, !m) = m
+  go (!i, !xy, !m) !s = (i', xy', m')
+   where
+    ps        = zip (walk xy s) [i ..]
+    (xy', i') = last ps
+    m'        = m <> M.fromList ps
 
 part2 :: ([Step], [Step]) -> Int
-part2 = _todo
+part2 (a, b) = minimum . S.map add . S.filter (/= (0, 0)) $ S.intersection sa
+                                                                           sb
+ where
+  add xy = ma M.! xy + mb M.! xy
+  ma = build a
+  mb = build b
+  sa = M.keysSet ma
+  sb = M.keysSet mb
 
 main :: IO ()
-main = readSteps >>= \ab -> traverse_ (print . ($ ab)) [part1, part2]
+main = do
+  ab <- readSteps
+  print $ part1 ab
+  print $ part2 ab
