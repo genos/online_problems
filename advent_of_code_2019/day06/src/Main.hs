f7527a99884d0bc85a7636a9487192f09d2f79b0
genos <graham@quantifind.com>
2020-01-31 12:12:28 -0500

    faster with sets

--- advent_of_code_2019/day06/src/Main.hs
+++ advent_of_code_2019/day06/src/Main.hs
@@ -1,33 +1,35 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Algebra.Graph.AdjacencyMap
 import Algebra.Graph.AdjacencyMap.Algorithm
 import Data.Bifunctor (second)
 import Data.Foldable (traverse_)
-import Data.List (intersect)
+import qualified Data.Set as S
 import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 
 readGraph :: Text -> AdjacencyMap Text
 readGraph = overlays . fmap readEdge . T.lines
-  where readEdge = uncurry edge . second T.tail  . T.breakOn ")"
+  where
+    readEdge = uncurry edge . second T.tail . T.breakOn ")"
 
 part1 :: AdjacencyMap Text -> Int
 part1 g = sum (fmap numOrbits v) - length v
   where
-    numOrbits = length . (`dfs` g) . (:[])
+    numOrbits = length . (`dfs` g) . (: [])
     v = vertexList g
 
 part2 :: AdjacencyMap Text -> Int
-part2 g = len x + len y - 2
+part2 g = S.size you' + S.size san'
   where
     g' = transpose g
-    x = reachable "YOU" g'
-    y = reachable "SAN" g'
-    meet = head $ intersect x y
-    len = length . takeWhile (/= meet)
+    you = S.fromList $ reachable "YOU" g'
+    san = S.fromList $ reachable "SAN" g'
+    you' = you S.\\ san
+    san' = san S.\\ you
 
 main :: IO ()
 main = do
