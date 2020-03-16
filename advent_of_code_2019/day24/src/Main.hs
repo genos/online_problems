6cee233839f9f33f1c85ee1b5a81b15925705b8f
genos <graham@quantifind.com>
2020-03-16 12:16:11 -0400

    pretty printing, next grid step

--- advent_of_code_2019/day24/src/Main.hs
+++ advent_of_code_2019/day24/src/Main.hs
@@ -9,7 +9,7 @@ import Linear.V2 (V2 (..))
 
 data Square = Bug | Empty deriving (Eq, Show)
 
-type Grid = Map (V2 Word) Square
+type Grid = Map (V2 Int) Square
 
 toC :: Square -> Char
 toC = \case Bug -> '#'; Empty -> '.'
@@ -21,12 +21,30 @@ parse :: String -> Grid
 parse s = M.fromList $ zip coords squares
   where
     ls = lines s
-    n = fromIntegral $ length ls - 1
+    n = fromIntegral $ length ls - 1 -- assumes square grid…
     coords = [V2 x y | x <- [0 .. n], y <- [0 .. n]]
     squares = concatMap (mapMaybe fromC) ls
 
 readInput :: FilePath -> IO Grid
 readInput = (parse <$>) . readFile
 
+pretty :: Grid -> String
+pretty g = unlines ss
+  where
+    V2 x y = fst $ M.findMax g
+    coords = [[V2 x' y' | y' <- [0 .. y]] | x' <- [0 .. x]]
+    ss = fmap (fmap (toC . (g M.!))) coords
+
+next :: Grid -> Grid
+next g = M.mapWithKey update g
+  where
+    update v s
+      | s == Bug && n == 1 = Bug
+      | s == Empty && (n == 1 || n == 2) = Bug
+      | otherwise = Empty
+      where
+        adj = [v + w | w <- [V2 (-1) 0, V2 1 0, V2 0 (-1), V2 0 1]]
+        n = length . filter (== Bug) $ mapMaybe (g M.!?) adj
+
 main :: IO ()
-main = putStrLn "TODO"
+main = putStrLn . pretty =<< readInput "input_1"
