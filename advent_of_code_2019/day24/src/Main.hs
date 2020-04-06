066686351aa762dbd0abf89d34bb44f9ae74caf1
genos <graham@quantifind.com>
2020-04-06 12:41:41 -0400

    day24 part 1

--- advent_of_code_2019/day24/src/Main.hs
+++ advent_of_code_2019/day24/src/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE LambdaCase #-}
 
 module Main where
@@ -5,17 +6,18 @@ module Main where
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
 import Data.Maybe (mapMaybe)
+import qualified Data.Set as S
 import Linear.V2 (V2 (..))
 
-data Square = Bug | Empty deriving (Eq, Show)
+data Square = Empty | Bug deriving (Eq, Ord, Show)
 
 type Grid = Map (V2 Int) Square
 
-toC :: Square -> Char
-toC = \case Bug -> '#'; Empty -> '.'
-
 fromC :: Char -> Maybe Square
-fromC = \case '#' -> Just Bug; '.' -> Just Empty; _ -> Nothing
+fromC = \case '.' -> Just Empty; '#' -> Just Bug; _ -> Nothing
+
+toC :: Square -> Char
+toC = \case Empty -> '.'; Bug -> '#'
 
 parse :: String -> Grid
 parse s = M.fromList $ zip coords squares
@@ -39,12 +41,52 @@ next :: Grid -> Grid
 next g = M.mapWithKey update g
   where
     update v s
-      | s == Bug && n == 1 = Bug
       | s == Empty && (n == 1 || n == 2) = Bug
+      | s == Bug && n == 1 = Bug
       | otherwise = Empty
       where
         adj = [v + w | w <- [V2 (-1) 0, V2 1 0, V2 0 (-1), V2 0 1]]
         n = length . filter (== Bug) $ mapMaybe (g M.!?) adj
 
+testNext :: Bool
+testNext = and $ zipWith (==) (tail grids) (fmap next grids)
+  where
+    grids =
+      fmap
+        parse
+        [ "....#\n#..#.\n#..##\n..#..\n#....",
+          "#..#.\n####.\n###.#\n##.##\n.##..",
+          "#####\n....#\n....#\n...#.\n#.###",
+          "#....\n####.\n...##\n#.##.\n.##.#",
+          "####.\n....#\n##..#\n.....\n##..."
+        ]
+
+firstRepeat :: (Eq a, Ord a) => (a -> a) -> a -> a
+firstRepeat f = go S.empty
+  where
+    go !xs !x
+      | S.member x xs = x
+      | otherwise = go (S.insert x xs) (f x)
+
+testFirstRepeat :: Bool
+testFirstRepeat = h == firstRepeat next g
+  where
+    g = parse "....#\n#..#.\n#..##\n..#..\n#...."
+    h = parse ".....\n.....\n.....\n#....\n.#..."
+
+biodiversity :: Grid -> Int
+biodiversity = snd . M.foldl' f (1, 0)
+  where
+    f (!ctr, !acc) !s = let x = if s == Bug then ctr else 0 in (2 * ctr, acc + x)
+
+testBiodiversity :: Bool
+testBiodiversity = 2129920 == biodiversity (parse ".....\n.....\n.....\n#....\n.#...")
+
+part1 :: IO Int
+part1 = biodiversity . firstRepeat next <$> readInput "input_1"
+
 main :: IO ()
-main = putStrLn . pretty =<< readInput "input_1"
+main = do
+  putStrLn . pretty =<< readInput "input_1"
+  print $ testNext && testFirstRepeat && testBiodiversity
+  print =<< part1
