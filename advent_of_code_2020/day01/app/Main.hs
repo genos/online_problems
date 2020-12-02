640e422702ce73789f74e2f35414fbb0c71f0aaf
genos <genos@rigetti.com>
2020-12-02 15:23:04 -0500

    just use []

--- advent_of_code_2020/day01/app/Main.hs
+++ advent_of_code_2020/day01/app/Main.hs
@@ -2,24 +2,17 @@ module Main
   ( main
   ) where
 
-import qualified Data.List.NonEmpty as NE
+input :: IO [Int]
+input = sort . rights . fmap (readEither . toString) . lines <$> readFileText
+  "input.txt"
 
-input :: IO (NonEmpty Int)
-input =
-  fromList
-    .   sort
-    .   rights
-    .   fmap (readEither . toString)
-    .   lines
-    <$> readFileText "input.txt"
+solve :: (f Int -> Int) -> (f Int -> Int) -> [f Int] -> Maybe Int
+solve mul add = viaNonEmpty head . fmap mul . filter ((== 2020) . add)
 
-solve :: (f Int -> Int) -> (f Int -> Int) -> NonEmpty (f Int) -> Maybe Int
-solve mul add = viaNonEmpty head . fmap mul . NE.filter ((== 2020) . add)
-
-part1 :: NonEmpty Int -> Maybe Int
+part1 :: [Int] -> Maybe Int
 part1 xs = solve (uncurry (*)) (uncurry (+)) $ (,) <$> xs <*> xs
 
-part2 :: NonEmpty Int -> Maybe Int
+part2 :: [Int] -> Maybe Int
 part2 xs =
   solve (\(a, b, c) -> a * b * c) (\(a, b, c) -> a + b + c)
     $   (,,)
