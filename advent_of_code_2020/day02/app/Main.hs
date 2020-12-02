0bb72b1121db02798970b000afc995ee2ac2ae8e
genos <genos@rigetti.com>
2020-12-02 13:48:06 -0500

    Day 2, plus a tweak to day01.cabal

---
+++ advent_of_code_2020/day02/app/Main.hs
@@ -0,0 +1,46 @@
+module Main
+  ( main
+  ) where
+import Data.Attoparsec.Text hiding (count)
+import Data.Char            (isAlpha)
+import Data.Text            (count, index)
+
+data Rule = Rule
+  { _lower    :: Int
+  , _upper    :: Int
+  , _char     :: Char
+  , _password :: Text
+  }
+  deriving stock (Eq, Ord, Show)
+
+rule :: Parser Rule
+rule =
+  Rule
+    <$> decimal
+    <*> (char '-' *> decimal)
+    <*> (skipSpace *> letter)
+    <*> (char ':' *> skipSpace *> takeWhile1 isAlpha)
+
+input :: IO [Rule]
+input =
+  fromRight []
+    .   parseOnly ((rule `sepBy1'` char '\n') <* skipSpace <* endOfInput)
+    <$> readFileText "input.txt"
+
+part1 :: [Rule] -> Int
+part1 = length . filter policy
+  where policy (Rule l u c p) = let n = count (one c) p in l <= n && n <= u
+
+part2 :: [Rule] -> Int
+part2 = length . filter policy where
+  policy (Rule l u c p) = (x == c && y /= c) || (x /= c && y == c)
+   where
+    x = p `index` (l - 1)
+    y = p `index` (u - 1)
+
+
+main :: IO ()
+main = do
+  rules <- input
+  print $ part1 rules
+  print $ part2 rules
