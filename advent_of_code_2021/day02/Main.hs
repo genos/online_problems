3f0da1bb54e48534577ce247a858fef49eb95acb
Graham Enos <genos@rigetti.com>
2021-12-02 09:43:59 -0500

    Parse it all first

--- advent_of_code_2021/day02/Main.hs
+++ advent_of_code_2021/day02/Main.hs
@@ -9,37 +9,38 @@ import           Data.Text            (Text)
 import qualified Data.Text.IO         as T
 import           Linear.V2
 
-newLines :: Parser a -> Text -> [a]
-newLines p = fromRight [] . parseOnly (p `sepBy1'` "\n")
+data Inst = Down Int | Up Int | Forward Int
 
-part1 :: Text -> Int
-part1 = product . sum . newLines inst
+inst :: Text -> [Inst]
+inst = fromRight [] . parseOnly (p `sepBy1'` "\n")
  where
-  inst = choice
-    [ V2 0 <$> ("down " *> decimal)
-    , negate . V2 0 <$> ("up " *> decimal)
-    , flip V2 0 <$> ("forward " *> decimal)
+  p = choice
+    [ Down <$> ("down " *> decimal)
+    , Up <$> ("up " *> decimal)
+    , Forward <$> ("forward " *> decimal)
     ]
 
-data Pos = Pos
-  { horizontal :: {-# UNPACK #-} !Int
-  , depth      :: {-# UNPACK #-} !Int
-  , aim        :: {-# UNPACK #-} !Int
+part1 :: [Inst] -> Int
+part1 = product . sum . fmap v2
+ where
+  v2 (Down    x) = V2 0 x
+  v2 (Up      x) = V2 0 (-x)
+  v2 (Forward x) = V2 x 0
+
+data Sub = Sub
+  { horizontal :: Int
+  , depth      :: Int
+  , aim        :: Int
   }
 
-part2 :: Text -> Int
-part2 = ((*) <$> horizontal <*> depth) . foldl' (&) (Pos 0 0 0) . newLines inst
+part2 :: [Inst] -> Int
+part2 = ((*) <$> horizontal <*> depth) . foldl' (&) (Sub 0 0 0) . fmap go
  where
-  inst = choice
-    [ d <$> ("down " *> decimal)
-    , u <$> ("up " *> decimal)
-    , f <$> ("forward " *> decimal)
-    ]
-  d x p = p { aim = aim p + x }
-  u x p = p { aim = aim p - x }
-  f x p = p { horizontal = x + horizontal p, depth = depth p + x * aim p }
+  go (Down    x) s = s { aim = aim s + x }
+  go (Up      x) s = s { aim = aim s - x }
+  go (Forward x) s = s { horizontal = x + horizontal s, depth = depth s + x * aim s }
 
 main :: IO ()
 main = do
-  input <- T.readFile "input.txt"
+  input <- inst <$> T.readFile "input.txt"
   traverse_ (print . ($ input)) [part1, part2]
