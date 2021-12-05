28c5ecb3d643ebfbce0d2b2cdb49793cc8254120
Graham Enos <genos@rigetti.com>
2021-12-04 21:58:40 -0500

    Part 2

--- advent_of_code_2021/day04/Main.hs
+++ advent_of_code_2021/day04/Main.hs
@@ -2,41 +2,48 @@
 module Main where
 
 import           Data.Attoparsec.Text
+import           Data.Bool            (bool)
 import           Data.Either          (fromRight)
-import           Data.Foldable        (find)
+import           Data.Foldable        (find, traverse_)
 import           Data.Matrix          (Matrix)
 import qualified Data.Matrix          as M
 import           Data.Text            (Text)
 import qualified Data.Text.IO         as T
 
-data BingoCard = BC (Matrix Bool) (Matrix Int) deriving Show
+data BingoCard = BC (Matrix Bool) (Matrix Int)
 type Game = ([Int], [BingoCard])
+
+len :: Int
+len = 5
 
 gameP :: Text -> Game
 gameP = fromRight (error "PARSE") . parseOnly ((,) <$> (nums <* "\n") <*> many1' card)
  where
   nums = decimal `sepBy1'` ","
-  card = toBC . M.fromLists <$> count 5 (count 5 (skipSpace *> decimal <* skipSpace))
-  toBC = BC (M.matrix 5 5 $ const False)
+  card = bc . M.fromLists <$> count len (count len (skipSpace *> decimal <* skipSpace))
+  bc   = BC (M.matrix len len $ const False)
+
+indices :: [Int]
+indices = [1 .. len]
+
+pairs :: [(Int, Int)]
+pairs = [ (i, j) | i <- indices, j <- indices ]
 
 won :: BingoCard -> Bool
 won (BC marks _) = rows || cols || diags
  where
-  rows  = or [ and (M.getRow i marks) | i <- [1 .. 5] ]
-  cols  = or [ and (M.getCol i marks) | i <- [1 .. 5] ]
+  rows  = or [ and (i `M.getRow` marks) | i <- indices ]
+  cols  = or [ and (i `M.getCol` marks) | i <- indices ]
   diags = and (M.getDiag marks) || and (M.getDiag $ M.transpose marks)
 
 mark :: Int -> BingoCard -> BingoCard
-mark n (BC marks card) = BC marks' card
+mark n (BC marks card) = BC m' card
  where
-  marks' =
-    case find (\ij -> (n ==) $ card M.! ij) [ (i, j) | i <- [1 .. 5], j <- [1 .. 5] ] of
-      Nothing     -> marks
-      Just (i, j) -> M.setElem True (i, j) marks
+  m' = maybe marks (flip (M.setElem True) marks) $ find ((n ==) . (card M.!)) pairs
 
 score :: Int -> BingoCard -> Int
 score n (BC mask card) = n * sum unmarked
-  where unmarked = zipWith (\m c -> if m then 0 else c) (M.toList mask) (M.toList card)
+  where unmarked = zipWith (\m c -> bool c 0 m) (M.toList mask) (M.toList card)
 
 part1 :: Game -> Maybe Int
 part1 (_ , []) = Nothing
@@ -50,9 +57,12 @@ part1 (n : ns, bcs) =
 part2 :: Game -> Maybe Int
 part2 (_ , []) = Nothing
 part2 ([], _ ) = Nothing
-part2 (n : ns, bcs) = _todo
+part2 (n : ns, [bc]) | won bc'   = Just $ score n bc'
+                     | otherwise = part2 (ns, [bc'])
+  where bc' = mark n bc
+part2 (n : ns, bcs) = part2 (ns, filter (not . won) $ mark n <$> bcs)
 
 main :: IO ()
 main = do
-  test <- gameP <$> T.readFile "input.txt"
-  print $ part1 test
+  input <- gameP <$> T.readFile "input.txt"
+  traverse_ (print . ($ input)) [part1, part2]
