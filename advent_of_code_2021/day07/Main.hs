3039d64a2a1e779212c7ba0a1740ac9ad55eef8f
Graham Enos <genos@rigetti.com>
2021-12-07 10:52:56 -0500

    part 1

--- advent_of_code_2021/day07/Main.hs
+++ advent_of_code_2021/day07/Main.hs
@@ -1,4 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import           Data.Attoparsec.Text
+import           Data.Text            (Text)
+import qualified Data.Text.IO         as T
+import           Data.Vector.Unboxed  (Vector)
+import qualified Data.Vector.Unboxed  as V
+
+readCrabs :: Text -> Vector Int
+readCrabs = either (error "Parse") V.fromList <$> parseOnly (decimal `sepBy1'` ",")
+
+fuelRequired :: Vector Int -> Int -> Int
+fuelRequired crabs i = V.sum $ V.map (abs . (i -)) crabs
+
+part1 :: Vector Int -> Int
+part1 crabs = V.minimum . V.map (fuelRequired crabs) $ V.enumFromTo (V.minimum crabs) (V.maximum crabs)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  crabs <- readCrabs <$> T.readFile "input.txt"
+  print $ part1 crabs
