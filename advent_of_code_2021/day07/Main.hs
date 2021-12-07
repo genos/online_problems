c882390c65d8724db481a3e473df438112e9c784
Graham Enos <genos@rigetti.com>
2021-12-07 10:59:31 -0500

    Part 2

--- advent_of_code_2021/day07/Main.hs
+++ advent_of_code_2021/day07/Main.hs
@@ -2,6 +2,7 @@
 module Main where
 
 import           Data.Attoparsec.Text
+import           Data.Foldable        (traverse_)
 import           Data.Text            (Text)
 import qualified Data.Text.IO         as T
 import           Data.Vector.Unboxed  (Vector)
@@ -10,13 +11,17 @@ import qualified Data.Vector.Unboxed  as V
 readCrabs :: Text -> Vector Int
 readCrabs = either (error "Parse") V.fromList <$> parseOnly (decimal `sepBy1'` ",")
 
-fuelRequired :: Vector Int -> Int -> Int
-fuelRequired crabs i = V.sum $ V.map (abs . (i -)) crabs
+solve :: Vector Int -> (Vector Int -> Int -> Int) -> Int
+solve crabs fuel =
+  V.minimum . V.map (fuel crabs) $ V.enumFromTo (V.minimum crabs) (V.maximum crabs)
 
-part1 :: Vector Int -> Int
-part1 crabs = V.minimum . V.map (fuelRequired crabs) $ V.enumFromTo (V.minimum crabs) (V.maximum crabs)
+fuelPart1 :: Vector Int -> Int -> Int
+fuelPart1 crabs i = V.sum $ V.map (abs . (i -)) crabs
+
+fuelPart2 :: Vector Int -> Int -> Int
+fuelPart2 crabs i = V.sum $ V.map (V.sum . V.enumFromN 0 . (succ . abs . (i -))) crabs
 
 main :: IO ()
 main = do
   crabs <- readCrabs <$> T.readFile "input.txt"
-  print $ part1 crabs
+  traverse_ (print . solve crabs) [fuelPart1, fuelPart2]
