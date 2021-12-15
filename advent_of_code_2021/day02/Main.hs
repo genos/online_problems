31d9245f8256d1af9435782355d94d475ccf3288
Graham Enos <genos@rigetti.com>
2021-12-15 14:11:52 -0500

    less noise

--- advent_of_code_2021/day02/Main.hs
+++ advent_of_code_2021/day02/Main.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE DerivingVia           #-}
-{-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE LambdaCase            #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings     #-}
