317d9eaef8cd85b4867f60b5c0a349f0515d17f7
Graham Enos <genos@rigetti.com>
2021-12-22 14:34:11 -0500

    part1 only

--- advent_of_code_2021/day22/Main.hs
+++ advent_of_code_2021/day22/Main.hs
@@ -4,7 +4,7 @@ module Main where
 import           Control.Applicative  ((<|>))
 import           Data.Attoparsec.Text
 import           Data.Either          (fromRight)
-import           Data.Foldable        (foldl', traverse_)
+import           Data.Foldable        (foldl')
 import           Data.Function        ((&))
 import           Data.Set             (Set)
 import qualified Data.Set             as S
@@ -19,34 +19,31 @@ turn
   -> (Int, Int)  -- x bounds
   -> (Int, Int)  -- y bounds
   -> (Int, Int)  -- z bounds
-  -> Bool  -- whether to clamp
   -> Step
-turn op (xLo, xHi) (yLo, yHi) (zLo, zHi) clamp core = op core $ S.unions
+turn op (xLo, xHi) (yLo, yHi) (zLo, zHi) core = op core $ S.unions
   [ S.singleton (x, y, z)
   | x <- [f xLo .. f xHi]
   , y <- [f yLo .. f yHi]
   , z <- [f zLo .. f zHi]
   ]
-  where f = if clamp then max (-50) . min 50 else id
+  where f = max (-50) . min 50
 
-on, off :: (Int, Int) -> (Int, Int) -> (Int, Int) -> Bool -> Step
+on, off :: (Int, Int) -> (Int, Int) -> (Int, Int) -> Step
 on = turn S.union
 off = turn S.difference
 
-readSteps :: Bool -> Text -> [Step]
-readSteps clamp = fromRight (error "Bad parse") . parseOnly (step `sepBy1'` "\n")
+readSteps :: Text -> [Step]
+readSteps = fromRight (error "Bad parse") . parseOnly (step `sepBy1'` "\n")
  where
   step = do
     f  <- on <$ "on" <|> off <$ "off"
     xs <- (,) <$> (" x=" *> signed decimal) <*> (".." *> signed decimal)
     ys <- (,) <$> (",y=" *> signed decimal) <*> (".." *> signed decimal)
     zs <- (,) <$> (",z=" *> signed decimal) <*> (".." *> signed decimal)
-    pure $ f xs ys zs clamp
+    pure $ f xs ys zs
 
 run :: [Step] -> Int
 run = S.size . foldl' (&) S.empty
 
 main :: IO ()
-main = do
-  input <- T.readFile "input.txt"
-  traverse_ (print . (\b -> run $ readSteps b input)) [True, False]
+main = print . run . readSteps =<< T.readFile "input.txt"
