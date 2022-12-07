9c1eda5996a4aa882a73477490be218e9f7709ff
Graham Enos <genos@rigetti.com>
2022-12-07 14:29:27 -0500

    simplify

--- advent_of_code_2022/day07/Main.hs
+++ advent_of_code_2022/day07/Main.hs
@@ -3,35 +3,22 @@
 module Main where
 
 import Data.Attoparsec.Text
-import Data.Foldable (find, foldl')
+import Data.Foldable (foldl', toList, traverse_)
 import Data.Functor ((<$))
 import Data.List (break, sort)
-import Data.Monoid (Sum (..))
 import Data.Text (Text)
 import qualified Data.Text.IO as T
-import Data.Tree (Tree (..), flatten)
 
 data Entry
     = File {_size :: {-# UNPACK #-} !Int, _fname :: {-# UNPACK #-} !Text}
     | Directory {_entries :: [Entry], _dname :: {-# UNPACK #-} !Text}
-    deriving (Show)
 
 -- http://learnyouahaskell.com/zippers
-data Crumb = Crumb {_cname :: {-# UNPACK #-} !Text, _lefts :: [Entry], _rights :: [Entry]} deriving (Show)
+data Crumb = Crumb {_cname :: {-# UNPACK #-} !Text, _lefts :: [Entry], _rights :: [Entry]}
 type Zipper = (Entry, [Crumb])
 
 newRoot :: Zipper
 newRoot = (Directory [] "/", [])
-
-toTree :: Zipper -> Tree Int
-toTree = annotate . toRoot
-  where
-    annotate (File s _) = Node 0 []
-    annotate (Directory es _) = let s = sum (size <$> es) in Node s (fmap annotate es)
-    toRoot (e, []) = e
-    toRoot z = toRoot $ up z
-    size (File s _) = s
-    size (Directory es _) = sum (size <$> es)
 
 up :: Zipper -> Zipper
 up (_, []) = error "up at root"
@@ -49,32 +36,33 @@ insert :: Zipper -> Entry -> Zipper
 insert (File _ _, _) _ = error "insert on File focus"
 insert (Directory es n, cs) e = (Directory (e : es) n, cs)
 
-parseRoot :: Parser Zipper
-parseRoot = newRoot <$ (string "$ cd /" <* endOfLine)
-
-parseZipper :: Zipper -> Parser Zipper
-parseZipper z = do
-    z' <- choice [contents z, step z] <* endOfLine
-    choice [z' <$ endOfInput, parseZipper z']
+sizes :: Zipper -> [Int]
+sizes = reverse . sort . annotate . toRoot
   where
-    name = takeTill isEndOfLine
-    step z = choice [up z <$ string "$ cd ..", (`dn` z) <$> (string "$ cd " *> name)]
+    annotate (File s _) = []
+    annotate d@(Directory es _) = size d : concatMap annotate es
+    toRoot z@(e, cs) = if null cs then e else toRoot $ up z
+    size (File s _) = s
+    size (Directory es _) = sum (size <$> es)
+
+readSizes :: Text -> [Int]
+readSizes = either (error "Bad parse") sizes . parseOnly (parseZipper =<< newRoot <$ (string "$ cd /" <* endOfLine))
+  where
+    parseZipper z = (choice [contents z, step z] <* endOfLine) >>= \z' -> choice [z' <$ endOfInput, parseZipper z']
     contents z = foldl' insert z <$> ((string "$ ls" *> endOfLine) *> (entry `sepBy'` endOfLine))
+    step z = choice [up z <$ string "$ cd ..", (`dn` z) <$> (string "$ cd " *> name)]
     entry = choice [Directory [] <$> (string "dir " *> name), File <$> decimal <*> (skipSpace *> name)]
+    name = takeTill isEndOfLine
 
-readZipper :: Text -> Zipper
-readZipper = either (error "Bad parse") id . parseOnly (parseZipper =<< parseRoot)
+part1 :: [Int] -> Int
+part1 = sum . filter (<= 100000) . tail
 
-part1 :: Tree Int -> Int
-part1 = getSum . foldMap (\i -> if i > 100000 then 0 else Sum i)
-
-part2 :: Tree Int -> Maybe Int
-part2 t = let (root:rest) = flatten t
-              free = 70000000 - root
-           in find (\n -> free + n >= 30000000) $ sort rest
+part2 :: [Int] -> Int
+part2 (root : rest) = minimum $ filter ((>= 30000000) . (free +)) rest
+  where
+    free = 70000000 - root
 
 main :: IO ()
 main = do
-    tree <- toTree . readZipper <$> T.readFile "input.txt"
-    print $ part1 tree
-    print $ part2 tree
+    tree <- readSizes <$> T.readFile "input.txt"
+    traverse_ (print . ($ tree)) [part1, part2]
