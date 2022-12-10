e32b47a84245ed6f961d5fa025f6f4c14b50cee3
Graham Enos <genos@rigetti.com>
2022-12-10 16:36:47 -0500

    generalize

--- advent_of_code_2022/day10/Main.hs
+++ advent_of_code_2022/day10/Main.hs
@@ -4,8 +4,8 @@ module Main where
 
 import Data.Attoparsec.Text
 import Data.Bool (bool)
-import Data.Foldable (foldl')
-import Data.Text (Text)
+import Data.Foldable (foldl', traverse_)
+import Data.Text (Text, pack)
 import qualified Data.Text.IO as T
 
 readAndInterpret :: Text -> [Int -> Int]
@@ -13,15 +13,15 @@ readAndInterpret = either (error "Bad parse") concat . parseOnly (f `sepBy1'` en
   where
     f = choice [[id] <$ string "noop", (\n -> [id, (+ n)]) <$> (string "addx " *> signed decimal)]
 
-part1 :: [Int -> Int] -> Int
-part1 = snd . foldl' f (1, 0) . zip [1 ..]
+solve :: (x -> Text) -> (x -> (Int -> Int) -> x) -> x -> [Int -> Int] -> Text
+solve g f z = g . foldl' f z
+
+part1 :: [Int -> Int] -> Text
+part1 = solve (pack . show . \(_, _, s) -> s) f (1, 1, 0)
   where
-    f (x, s) (i, g) = (x', s')
-      where
-        x' = g x
-        s' = s + i * x * bool 0 1 (i `elem` [20, 60, 100, 140, 180, 220])
+    f (i, x, s) g = (succ i, g x, s + i * x * bool 0 1 (i `elem` [20, 60, 100, 140, 180, 220]))
 
 main :: IO ()
 main = do
     input <- readAndInterpret <$> T.readFile "input.txt"
-    print $ part1 input
+    traverse_ (T.putStrLn . ($ input)) [part1]
