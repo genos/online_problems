68b5a86ecd0ea0a6f93a3f562a6365aec7b0aa94
Graham Enos <genos@rigetti.com>
2022-12-10 15:35:13 -0500

    tighten up

--- advent_of_code_2022/day10/Main.hs
+++ advent_of_code_2022/day10/Main.hs
@@ -1,32 +1,27 @@
-{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
 import Data.Attoparsec.Text
-import Data.Foldable (foldl', traverse_)
+import Data.Bool (bool)
+import Data.Foldable (foldl')
 import Data.Text (Text)
 import qualified Data.Text.IO as T
 
-data Instruction = NOOP | ADDX {-# UNPACK #-} !Int
-
-readInstructions :: Text -> [Instruction]
-readInstructions = either (error "Bad parse") id . parseOnly (i `sepBy1'` endOfLine)
+readAndInterpret :: Text -> [Int -> Int]
+readAndInterpret = either (error "Bad parse") concat . parseOnly (f `sepBy1'` endOfLine)
   where
-    i = choice [NOOP <$ string "noop", ADDX <$> (string "addx " *> signed decimal)]
+    f = choice [[id] <$ string "noop", (\n -> [id, (+ n)]) <$> (string "addx " *> signed decimal)]
 
-interpret :: Instruction -> [Int -> Int]
-interpret = \case NOOP -> [id]; (ADDX i) -> [id, (+ i)]
-
-part1 :: [Instruction] -> Int
-part1 =  snd . foldl' f (1, 0) . zip [1 ..] . concatMap interpret
+part1 :: [Int -> Int] -> Int
+part1 = snd . foldl' f (1, 0) . zip [1 ..]
   where
     f (x, s) (i, g) = (x', s')
       where
         x' = g x
-        s' = if i `elem` [20, 60, 100, 140, 180, 220] then s + (i * x) else s
+        s' = s + i * x * bool 0 1 (i `elem` [20, 60, 100, 140, 180, 220])
 
 main :: IO ()
 main = do
-    input <- readInstructions <$> T.readFile "input.txt"
-    traverse_ (print . ($ input)) [part1]
+    input <- readAndInterpret <$> T.readFile "input.txt"
+    print $ part1 input
