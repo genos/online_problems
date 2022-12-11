f2f5aeb4ef59a2d9c33c21d9c290febf5d01f439
Graham Enos <genos@rigetti.com>
2022-12-11 12:14:53 -0500

    wip; part 2 still wrong

--- advent_of_code_2022/day10/Main.hs
+++ advent_of_code_2022/day10/Main.hs
@@ -5,23 +5,35 @@ module Main where
 import Data.Attoparsec.Text
 import Data.Bool (bool)
 import Data.Foldable (foldl', traverse_)
-import Data.Text (Text, pack)
+import Data.Function ((&))
+import Data.List (scanl')
+import Data.Text (Text)
+import qualified Data.Text as T
 import qualified Data.Text.IO as T
 
-readAndInterpret :: Text -> [Int -> Int]
-readAndInterpret = either (error "Bad parse") concat . parseOnly (f `sepBy1'` endOfLine)
+readInterpretApply :: Text -> [Int]
+readInterpretApply = either (error "Bad parse") (scanl' (&) 1 . concat) . parseOnly (f `sepBy1'` endOfLine)
   where
     f = choice [[id] <$ string "noop", (\n -> [id, (+ n)]) <$> (string "addx " *> signed decimal)]
 
-solve :: (x -> Text) -> (x -> (Int -> Int) -> x) -> x -> [Int -> Int] -> Text
-solve g f z = g . foldl' f z
+solve :: ((a, b) -> Int -> (a, b)) -> (a, b) -> (b -> Text) -> [Int] -> Text
+solve f z g = g . snd . foldl' f z
 
-part1 :: [Int -> Int] -> Text
-part1 = solve (pack . show . \(_, _, s) -> s) f (1, 1, 0)
+part1 :: [Int] -> Text
+part1 = solve f (1, 0) (T.pack . show)
   where
-    f (i, x, s) g = (succ i, g x, s + i * x * bool 0 1 (i `elem` [20, 60, 100, 140, 180, 220]))
+    f (i, strength) n = (succ i, strength')
+      where
+        strength' = strength + i * n * bool 0 1 (i `elem` [20, 60, 100, 140, 180, 220])
+
+part2 :: [Int] -> Text
+part2 = solve f (0, T.empty) (T.unlines . T.chunksOf 40 . T.take 240 . T.reverse)
+  where
+    f (i, screen) sprite = (succ i, T.cons c screen)
+      where
+        c = bool '.' '#' (abs (i - sprite) <= 1)
 
 main :: IO ()
 main = do
-    input <- readAndInterpret <$> T.readFile "input.txt"
-    traverse_ (T.putStrLn . ($ input)) [part1]
+    sprites <- readInterpretApply <$> T.readFile "test.txt"
+    traverse_ (T.putStrLn . ($ sprites)) [part1, part2]
