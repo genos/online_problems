bdad3fee50bc695e29845584f5cb51be745ddd9b
Graham Enos <genos@rigetti.com>
2022-12-12 11:38:10 -0500

    tighten up

--- advent_of_code_2022/day11/Main.hs
+++ advent_of_code_2022/day11/Main.hs
@@ -13,13 +13,11 @@ import Data.Text (Text)
 import qualified Data.Text.IO as T
 import Data.Vector (Vector)
 import qualified Data.Vector as V
-import Data.Word (Word64)
 
-data Monkey = M
-    { _number :: {-# UNPACK #-} !Int
-    , _worries :: Seq Word64
-    , _operation :: Word64 -> Word64
-    , _divBy :: {-# UNPACK #-}!Word64
+data Monkey = Monkey
+    { _worries :: Seq Int
+    , _operation :: Int -> Int
+    , _divBy :: {-# UNPACK #-}!Int
     , _true :: {-# UNPACK #-} !Int
     , _false :: {-# UNPACK #-} !Int
     , _inspected :: {-# UNPACK #-}!Int
@@ -30,8 +28,8 @@ $(makeLenses ''Monkey)
 readMonkeys :: Text -> Vector Monkey
 readMonkeys = either (error "Bad Parse") V.fromList . parseOnly (m `sepBy1'` endOfLine)
   where
-    m = M <$> n <*> w <*> o <*> d <*> t <*> f <*> pure 0
-    n = string "Monkey " *> decimal <* char ':' <* endOfLine
+    m = Monkey <$> (h *> w) <*> o <*> d <*> t <*> f <*> pure 0
+    h = string "Monkey " *> skipWhile (/= '\n') *> endOfLine
     w = fromList <$> (skipSpace *> string "Starting items: " *> (decimal `sepBy'` string ", ") <* endOfLine)
     o = skipSpace *> parseOp <* endOfLine
     d = skipSpace *> string "Test: divisible by " *> decimal <* endOfLine
@@ -39,32 +37,29 @@ readMonkeys = either (error "Bad Parse") V.fromList . parseOnly (m `sepBy1'` end
     f = skipSpace *> string "If false: throw to monkey " *> decimal <* endOfLine
     parseOp = do
         _ <- string "Operation: new = old "
-        g <- choice [(+) <$ string "+ ", (*) <$ string "* "]
-        choice [self g, other g]
-    self g = (\x -> g x x) <$ string "old"
-    other g = g <$> decimal
+        b <- choice [(+) <$ string "+ ", (*) <$ string "* "]
+        choice [self b, other b]
+    self b = (\x -> b x x) <$ string "old"
+    other = (<$> decimal)
 
-turn :: (Word64 -> Word64) -> Vector Monkey -> Int -> Vector Monkey
+turn :: (Int -> Int) -> Vector Monkey -> Int -> Vector Monkey
 turn g ms n = ms'
   where
-    m = ms ^?! ix n
+    m = ms `V.unsafeIndex` n
     m' = m & worries .~ mempty & inspected +~ (m ^. worries & length)
     ms' = foldl' h ms (_worries m) & ix n .~ m'
     h mz w =
         let w' = g $ _operation m w
-            n1 = bool (_false m) (_true m) $ 0 == (w' `mod` _divBy m)
-            m1 = (mz V.! n1) & worries %~ (|> w')
-         in mz & ix n1 .~ m1
+            n' = bool (_false m) (_true m) $ 0 == (w' `mod` _divBy m)
+        in mz & (ix n' . worries) %~ (|> w')
 
-gameRound :: (Word64 -> Word64) -> Vector Monkey -> Vector Monkey
-gameRound g ms = V.foldl' (turn g) ms . V.enumFromTo 0 . pred $ V.length ms
-
-solve :: Word64 -> (Word64 -> Word64) -> Vector Monkey -> Int
-solve n g = product . take 2 . reverse . sort . V.toList . fmap _inspected . go 0
+solve :: Int -> (Int -> Int) -> Vector Monkey -> Int
+solve n g = product . take 2 . reverse . sort . (^.. traverse . inspected) . outer 0
   where
-    go k ms
+    outer k ms
         | k == n = ms
-        | otherwise = go (succ k) (gameRound g ms)
+        | otherwise = outer (succ k) (inner ms)
+    inner ms = V.foldl' (turn g) ms . V.enumFromN 0 $ V.length ms
 
 main :: IO ()
 main = do
