e7d8afe02a542142cd74e4fe37c576243822d4ca
Graham Enos <genos@rigetti.com>
2022-12-13 10:29:04 -0500

    victory

--- advent_of_code_2022/day13/Main.hs
+++ advent_of_code_2022/day13/Main.hs
@@ -2,7 +2,7 @@ module Main where
 
 import Data.Attoparsec.Text
 import Data.Foldable (traverse_)
-import Data.List (findIndices)
+import Data.List (findIndices, sort)
 import Data.Text (Text)
 import qualified Data.Text.IO as T
 
@@ -24,11 +24,13 @@ part1 :: [(Packet, Packet)] -> Int
 part1 = sum . fmap fst . filter (uncurry (<) . snd) . zip [1 ..]
 
 part2 :: [(Packet, Packet)] -> Int
-part2 = product . fmap succ . findIndices isDivider . concatMap (\(a, b) -> [a, b])
+part2 = product . fmap succ . findIndices isDivider . sort . ([two, six] ++) . concatMap (\(a, b) -> [a, b])
   where
-    isDivider = (`elem` [List [List [One 2]], List [List [One 6]]])
+    two = List [List [One 2]]
+    six = List [List [One 6]]
+    isDivider = (`elem` [two, six])
 
 main :: IO ()
 main = do
-    input <- readPairs <$> T.readFile "test.txt"
+    input <- readPairs <$> T.readFile "input.txt"
     traverse_ (print . ($ input)) [part1, part2]
