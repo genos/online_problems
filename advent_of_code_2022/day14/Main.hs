774ae0f039ecbf195ad7399a4e11afab598860da
Graham Enos <genos@rigetti.com>
2022-12-14 11:02:22 -0500

    stage set for part 2

--- advent_of_code_2022/day14/Main.hs
+++ advent_of_code_2022/day14/Main.hs
@@ -12,50 +12,39 @@ import Data.Text (Text)
 import qualified Data.Text.IO as T
 import Linear.V2
 
-data Space = Rock | Sand deriving Eq
-
-instance Show Space where
-    show Rock = "#"
-    show Sand = "o"
-
+data Space = Rock | Sand deriving (Eq)
 type Coordinate = V2 Int
 type Cave = Map Coordinate Space
 
 readCave :: Text -> Cave
 readCave = either (error "Bad parse") (M.fromList . concat) . parseOnly (path `sepBy1'` endOfLine)
   where
+    path = fmap (,Rock) . concat . (zipWith expand <*> tail) <$> stops
     coord = V2 <$> decimal <*> (char ',' *> decimal)
     stops = coord `sepBy1'` " -> "
-    path = fmap (,Rock) . concat . (zipWith expand <*> tail) <$> stops
-    expand a b =
-        let d = b - a
-            s = signum d
-            n = maximum $ abs d
-         in take (succ n) $ iterate (+ s) a
+    expand a b = let d = b - a in take (succ . maximum $ abs d) $ iterate (+ signum d) a
 
-sand :: Coordinate -> Cave -> Either Cave Cave
-sand xy cave = go xy cave
+top :: Coordinate
+top = V2 500 0
+
+solve :: (Cave -> Cave) -> Cave -> Int
+solve sand = length . M.filter (== Sand) . sand
+
+sand1 :: Cave -> Cave
+sand1 cave = either sand1 id (go top cave)
   where
     keys = M.keys cave
     off_x = minimum1Of (folded . _x) keys
     off_y = maximum1Of (folded . _y) keys
     go v@(V2 x y) c
-        | x < off_x || y > off_y = Left c  -- fell off
+        | x < off_x || y > off_y = Right c -- fell off, all done
         | otherwise =
             let next = findOf folded (`M.notMember` c) [v + V2 0 1, v + V2 (-1) 1, v + V2 1 1]
              in if isNothing next
-                    then Right (M.insert v Sand c) -- at rest
-                    else go (fromJust next) c  -- keep going
-
-allSand :: Coordinate -> Cave -> Cave
-allSand xy cave = case sand xy cave of
-                    Left c' -> c'
-                    Right c' -> allSand xy c'
-
-part1 :: Cave -> Int
-part1 = length . M.filter (== Sand) . allSand (V2 500 0)
+                    then Left (M.insert v Sand c) -- at rest, next sand
+                    else go (fromJust next) c -- keep going with this one
 
 main :: IO ()
 main = do
     input <- readCave <$> T.readFile "input.txt"
-    print $ part1 input
+    print $ solve sand1 input
