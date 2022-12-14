b650b7b2866caccccd79fdaa3432c8da7c0771f8
Graham Enos <genos@rigetti.com>
2022-12-14 11:26:13 -0500

    better part 1, broken part 2

--- advent_of_code_2022/day14/Main.hs
+++ advent_of_code_2022/day14/Main.hs
@@ -6,9 +6,9 @@ module Main where
 import Control.Lens
 import Data.Attoparsec.Text hiding (take)
 import Data.Foldable (traverse_)
-import qualified Data.Map as M
 import Data.Map.Strict (Map)
-import Data.Maybe (fromJust, isNothing)
+import qualified Data.Map.Strict as M
+import Data.Set (Set)
 import Data.Text (Text)
 import qualified Data.Text.IO as T
 import Linear.V2
@@ -28,24 +28,35 @@ readCave = either (error "Bad parse") (M.fromList . concat) . parseOnly (path `s
 top :: Coordinate
 top = V2 500 0
 
+candidates :: Coordinate -> [Coordinate]
+candidates v = (v +) <$> [V2 0 1, V2 (-1) 1, V2 1 1]
+
 solve :: (Cave -> Cave) -> Cave -> Int
 solve sand = length . M.filter (== Sand) . sand
 
 sand1 :: Cave -> Cave
-sand1 cave = either sand1 id (go top cave)
+sand1 cave = go cave top
   where
     keys = M.keys cave
-    off_x = minimum1Of (folded . _x) keys
-    off_y = maximum1Of (folded . _y) keys
-    go v@(V2 x y) c
-        | x < off_x || y > off_y = Right c -- fell off, all done
+    offX = minimum1Of (folded . _x) keys
+    offY = maximum1Of (folded . _y) keys
+    go c v@(V2 x y)
+        | x < offX || y > offY = c
         | otherwise =
-            let next = findOf folded (`M.notMember` c) [v + V2 0 1, v + V2 (-1) 1, v + V2 1 1]
-             in if isNothing next
-                    then Left (M.insert v Sand c) -- at rest, next sand
-                    else go (fromJust next) c -- keep going with this one
+            let next = findOf folded (`M.notMember` c) (candidates v)
+             in maybe (sand1 $ M.insert v Sand c) (go c) next
+
+sand2 :: Cave -> Cave
+sand2 cave = go cave top
+  where
+    maxY = (2 +) $ maximum1Of (folded . _y) (M.keys cave)
+    go c v
+        | M.member top c = c
+        | otherwise =
+            let next = findOf folded (`M.notMember` c) . filter ((<= maxY) . (^. _y)) $ candidates v
+             in maybe (sand2 $ M.insert v Sand c) (go c) next
 
 main :: IO ()
 main = do
-    input <- readCave <$> T.readFile "input.txt"
-    traverse_ (print . (`solve` input)) [sand1]
+    input <- readCave <$> T.readFile "test.txt"
+    traverse_ (print . (`solve` input)) [sand1, sand2]
