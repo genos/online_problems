b155076e656123b265d4458aebd026de25414fc2
Graham Enos <genos@rigetti.com>
2023-06-16 10:53:31 -0400

    cleaner

--- advent_of_code_2022/day22/Main.hs
+++ advent_of_code_2022/day22/Main.hs
@@ -15,12 +15,25 @@ import Data.Text (Text)
 import Data.Text.IO (readFile)
 import Linear.V2
 
-type Direction = V2 Int -- assumed to be ± unit vector
+data Direction = N | S | E | W deriving (Enum)
 type Coord = V2 Int
 data RL = L | R
 data Move = Forward Int | Turn RL
 data Square = Tile | Wall deriving (Eq)
 type Board = Map Coord Square
+
+d2C :: Direction -> Coord
+d2C = \case N -> V2 0 1; S -> V2 0 (-1); E -> V2 1 0; W -> V2 (-1) 0
+
+turn :: RL -> Direction -> Direction
+turn L N = W
+turn R N = E
+turn L S = E
+turn R S = W
+turn L E = N
+turn R E = S
+turn L W = S
+turn R W = N
 
 readBoardAndMoves :: Text -> (Board, [Move])
 readBoardAndMoves =
@@ -34,33 +47,26 @@ readBoardAndMoves =
     entries ls = [(V2 x y, fromJust s) | (x, l) <- zip [0 ..] ls, (y, s) <- zip [0 ..] l, isJust s]
 
 step :: Board -> (Coord, Direction) -> Move -> (Coord, Direction)
-step _ (c, d) (Turn t) = let turn = case t of L -> perp; R -> negate . perp in (c, turn d)
+step _ (c, d) (Turn t) = (c, turn t d)
 step b (c, d) (Forward n) = go n c
   where
     go k xy
         | k <= 0 = (xy, d)
         | otherwise =
-            let xy' = case b M.!? (xy + d) of
-                    Just Tile -> xy + d
+            let d' = d2C d
+                xy' = case b M.!? (xy + d') of
+                    Just Tile -> xy + d'
                     Just Wall -> xy
                     Nothing ->
-                        let xy'' = last . takeWhile (`M.member` b) $ iterate (subtract d) xy
+                        let xy'' = last . takeWhile (`M.member` b) $ iterate (subtract d') xy
                          in if b M.! xy'' == Tile then xy'' else xy
              in go (pred k) xy'
 
-facing :: Direction -> Int
-facing = \case
-    V2 0 1 -> 0
-    V2 1 0 -> 1
-    V2 0 (-1) -> 2
-    V2 (-1) 0 -> 3
-    c -> error ("Bad direction: " <> show c)
-
 part1 :: Board -> [Move] -> Int
 part1 b ms =
-    let start = (,V2 0 1) . fst . M.findMin $ M.filter (== Tile) b
+    let start = (,N) . fst . M.findMin $ M.filter (== Tile) b
         (V2 x y, d) = foldl' (step b) start ms
-     in 1000 * succ x + 4 * succ y + facing d
+     in 1000 * succ x + 4 * succ y + succ (fromEnum d)
 
 main :: IO ()
 main = do
