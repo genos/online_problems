ba5c173bc0fc74a55b1672dd597c6a3b3f102086
Graham Enos <graham@strangeworks.com>
2023-12-02 18:09:19 -0500

    part2

--- advent_of_code_2023/day02/Main.hs
+++ advent_of_code_2023/day02/Main.hs
@@ -11,14 +11,6 @@ import Data.Text (Text)
 import Data.Text.IO qualified as T
 import Data.Tuple (swap)
 
-test :: Text
-test =
-    "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\n\
-    \Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\n\
-    \Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\n\
-    \Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\n\
-    \Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
-
 data Color = Red | Green | Blue deriving (Eq, Ord, Show)
 data Game = Game {num :: Int, subsets :: [Map Color Int]} deriving (Show)
 
@@ -36,8 +28,13 @@ part1 = sum . fmap num . filter possible
     bag = M.fromList [(Red, 12), (Green, 13), (Blue, 14)]
     possible = all ((== bag) . M.unionWith max bag) . subsets
 
+part2 :: [Game] -> Int
+part2 = sum . fmap power
+  where
+    power = M.foldl' (*) 1 . M.unionsWith max . subsets
+
 main :: IO ()
 main = do
-    print . part1 $ readGames test
-    input <- T.readFile "input.txt"
-    print . part1 $ readGames input
+    input <- readGames <$> T.readFile "input.txt"
+    print $ part1 input
+    print $ part2 input
