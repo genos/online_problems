b8d5a7593f05e5c729b7a5635863d5eb429983e8
Graham Enos <graham@strangeworks.com>
2023-12-02 20:57:03 -0500

    tidier

--- advent_of_code_2023/day02/Main.hs
+++ advent_of_code_2023/day02/Main.hs
@@ -5,34 +5,37 @@ module Main where
 import Control.Applicative
 import Data.Attoparsec.Text
 import Data.Foldable (traverse_)
-import Data.Functor (($>))
-import Data.Map.Strict (Map)
-import Data.Map.Strict qualified as M
 import Data.Text (Text)
 import Data.Text.IO qualified as T
-import Data.Tuple (swap)
 
-data Color = Red | Green | Blue deriving (Eq, Ord)
-data Game = Game {num :: Int, subsets :: [Map Color Int]}
+data Subset = S {red :: Word, green :: Word, blue :: Word} deriving (Eq)
+instance Semigroup Subset where
+    (S r1 g1 b1) <> (S r2 g2 b2) = S (r1 `max` r2) (g1 `max` g2) (b1 `max` b2)
+instance Monoid Subset where
+    mempty = S 0 0 0
+
+type Game = (Word, [Subset])
 
 readGames :: Text -> [Game]
 readGames = either (error "Bad parse") id . parseOnly ((game `sepBy1'` "\n") <* endOfInput)
   where
-    color = ("red" $> Red) <|> ("green" $> Green) <|> ("blue" $> Blue)
-    entry = fmap swap . (,) <$> decimal <*> (" " *> color)
-    subset = M.fromList <$> (entry `sepBy1'` ", ")
-    game = Game <$> ("Game " *> decimal <* ": ") <*> (subset `sepBy1'` "; ")
+    r = (\n -> S n 0 0) <$> decimal <* " red"
+    g = (\n -> S 0 n 0) <$> decimal <* " green"
+    b = S 0 0 <$> decimal <* " blue"
+    entry = r <|> g <|> b
+    subset = mconcat <$> entry `sepBy1'` ", "
+    game = (,) <$> ("Game " *> decimal <* ": ") <*> (subset `sepBy1'` "; ")
 
-part1 :: [Game] -> Int
-part1 = sum . fmap num . filter possible
+part1 :: [Game] -> Word
+part1 = sum . fmap fst . filter possible
   where
-    bag = M.fromList [(Red, 12), (Green, 13), (Blue, 14)]
-    possible = all ((== bag) . M.unionWith max bag) . subsets
+    bag = S 12 13 14
+    possible = (== bag) . mconcat . (bag :) . snd
 
-part2 :: [Game] -> Int
+part2 :: [Game] -> Word
 part2 = sum . fmap power
   where
-    power = product . M.unionsWith max . subsets
+    power = (\(S r g b) -> r * g * b) . mconcat . snd
 
 main :: IO ()
 main = do
