585a7e4564e5c191cdeb40adb6e3689087a6eb27
Graham Enos <graham@strangeworks.com>
2023-12-06 11:10:29 -0500

    tidier.

--- advent_of_code_2023/day06/Main.hs
+++ advent_of_code_2023/day06/Main.hs
@@ -11,32 +11,21 @@ import Data.Text.IO qualified as T
 test :: Text
 test = "Time:      7  15   30\nDistance:  9  40  200"
 
-type Race = (Word, Word)
-
-solve :: (Text -> [Race]) -> Text -> Int
-solve reader = product . fmap (\(t, d) -> length $ filter (beats d t) [0 .. t]) . reader
+solve :: Parser [Word] -> Text -> Int
+solve parser = product . fmap (\(t, d) -> length $ filter (beats d t) [0 .. t]) . reader
   where
-    beats distance time t = (time - t) * t > distance
+    beats d t x = (t - x) * x > d
+    reader = either (error "Bad parse") id . parseOnly (zip <$> time <*> ("\n" *> distance) <* endOfInput)
+    time = "Time:" *> skipSpace *> parser
+    distance = "Distance:" *> skipSpace *> parser
 
 part1 :: Text -> Int
-part1 = solve read1
-  where
-    read1 = either (error "Bad parse") id . parseOnly (zip <$> time <*> ("\n" *> distance) <* endOfInput)
-    time = "Time:" *> skipSpace *> nums
-    distance = "Distance:" *> skipSpace *> nums
-    nums = decimal `sepBy1'` skipSpace
+part1 = solve (decimal `sepBy1'` skipSpace)
 
 part2 :: Text -> Int
-part2 = solve read2
-  where
-    read2 = either (error "Bad parse") id . parseOnly ((\t d -> [(t, d)]) <$> time <*> ("\n" *> distance) <* endOfInput)
-    time = "Time:" *> skipSpace *> num
-    distance = "Distance:" *> skipSpace *> num
-    num :: Parser Word
-    num = read . T.unpack . T.filter (/= ' ') <$> takeTill isEndOfLine
+part2 = solve ((: []) . read . filter (/= ' ') . T.unpack <$> takeTill isEndOfLine)
 
 main :: IO ()
 main = do
-    traverse_ (print . ($ test)) [part1, part2]
     input <- T.readFile "input.txt"
     traverse_ (print . ($ input)) [part1, part2]
