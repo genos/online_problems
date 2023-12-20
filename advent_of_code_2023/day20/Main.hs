b2a1870832bacfa7b792f1add72796863dbe971f
Graham Enos <graham@strangeworks.com>
2023-12-20 12:07:20 -0500

    Types

--- advent_of_code_2023/day20/Main.hs
+++ advent_of_code_2023/day20/Main.hs
@@ -2,7 +2,14 @@
 
 module Main where
 
+import Data.Map.Strict (Map)
+import Data.Text (Text)
 import Data.Text.IO qualified as T
+
+data Pulse = Low | High
+data OnOff = On | Off
+data Module = Broadcast | FlipFlop OnOff | Conjunction (Map Text Pulse)
+type Circuit = Map Text (Module, [Text])
 
 main :: IO ()
 main = T.putStrLn "Day 20"
