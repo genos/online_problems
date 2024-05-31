3a64bfda326b7eaedb7fdd9a774d3f8c09d2d74d
Graham Enos <graham@strangeworks.com>
2024-05-31 14:51:19 -0400

    acronym.ml

---
+++ exercism/ocaml/acronym/acronym.ml
@@ -0,0 +1,10 @@
+open Base
+
+let acronym s =
+  s
+  |> String.filter_map ~f:(function
+       | ' ' | '-' -> Some ' '
+       | c -> Option.some_if (Char.is_alpha c) c)
+  |> String.split ~on:' '
+  |> List.map ~f:(Fn.compose String.uppercase (Fn.flip String.prefix 1))
+  |> String.concat
