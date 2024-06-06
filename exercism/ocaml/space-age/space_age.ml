1aacee645f0ba7d1b95f1c07978eca96ca7141e4
Graham Enos <graham@strangeworks.com>
2024-06-06 14:18:15 -0400

    space_age.ml

--- exercism/ocaml/space-age/space_age.ml
+++ exercism/ocaml/space-age/space_age.ml
@@ -9,15 +9,14 @@ type planet =
   | Uranus
 
 let age_on p s =
-  let m =
-    match p with
-    | Mercury -> 0.2408467
-    | Venus -> 0.61519726
-    | Earth -> 1.0
-    | Mars -> 1.8808158
-    | Jupiter -> 11.862615
-    | Saturn -> 29.447498
-    | Uranus -> 84.016846
-    | Neptune -> 164.79132
-  in
-  float_of_int s /. (m *. 31557600.0)
+  float_of_int s /. 31557600.0
+  /.
+  match p with
+  | Mercury -> 0.2408467
+  | Venus -> 0.61519726
+  | Earth -> 1.0
+  | Mars -> 1.8808158
+  | Jupiter -> 11.862615
+  | Saturn -> 29.447498
+  | Uranus -> 84.016846
+  | Neptune -> 164.79132
