c8094946e47d36bb706933e0e6fa7978cf1cab90
Graham Enos <graham@strangeworks.com>
2024-06-21 15:50:10 -0400

    atbash_cipher.ml

--- exercism/ocaml/atbash-cipher/atbash_cipher.ml
+++ exercism/ocaml/atbash-cipher/atbash_cipher.ml
@@ -1,16 +1,10 @@
 open Base
 
 let normalize = String.filter_map ~f:(fun c -> Char.(Option.some_if (is_alphanum c) (lowercase c)))
+let atbash c = Char.(if is_alpha c then to_int 'a' + to_int 'z' - to_int c |> of_int_exn else c)
 
-let table =
-  let a = "abcdefghijklmnopqrstuvwxyz" in
-  Map.of_alist_exn (module Char) @@ List.zip_exn (String.to_list a) String.(to_list @@ rev a)
-
-let atbash c = Map.find table c |> Option.value ~default:c
-
-let encode ?block_size plain =
-  normalize plain |> String.map ~f:atbash |> String.to_list
-  |> List.chunks_of ~length:(Option.value block_size ~default:5)
+let encode ?(block_size = 5) plain =
+  normalize plain |> String.map ~f:atbash |> String.to_list |> List.chunks_of ~length:block_size
   |> List.map ~f:String.of_list |> String.concat ~sep:" "
 
 let decode cipher = normalize cipher |> String.map ~f:atbash
