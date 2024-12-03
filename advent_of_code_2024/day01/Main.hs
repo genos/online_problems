40540604ce35a5ac1e80003b38aa65cf14432270
Graham <genos@users.noreply.github.com>
2024-12-02 23:07:03 -0500

    Advent of code 2024 (#105)
    * 2024/01 haskell
    
    * day01 part 2
    
    * tidy
    
    * day2
    
    * day2 done
    
    * day2 squeeze
    
    * squeeze

---
+++ advent_of_code_2024/day01/Main.hs
@@ -0,0 +1,27 @@
+module Main where
+
+import Data.Attoparsec.Text
+import Data.Foldable (traverse_)
+import Data.IntMap qualified as I
+import Data.List (sort)
+import Data.Text (Text)
+import Data.Text.IO qualified as T
+
+parse_ :: Text -> ([Int], [Int])
+parse_ = either (error "Bad pasre") unzip . parseOnly (line <* endOfInput)
+  where
+    line = ((,) <$> decimal <*> (skipSpace *> decimal)) `sepBy1'` char '\n'
+
+part1 :: ([Int], [Int]) -> Int
+part1 (xs, ys) = sum . fmap abs $ zipWith (-) (sort xs) (sort ys)
+
+part2 :: ([Int], [Int]) -> Int
+part2 (xs, ys) = sum $ fmap get xs
+  where
+    zs = I.fromListWith (+) $ fmap (,1) ys
+    get x = x * I.findWithDefault 0 x zs
+
+main :: IO ()
+main = do
+    input <- parse_ <$> T.readFile "input.txt"
+    traverse_ (print . ($ input)) [part1, part2]
---
+++ advent_of_code_2024/day02/Main.hs
@@ -0,0 +1,29 @@
+module Main where
+
+import Data.Attoparsec.Text
+import Data.Foldable (traverse_)
+import Data.List (subsequences)
+import Data.Text (Text)
+import Data.Text.IO qualified as T
+
+parse_ :: Text -> [[Int]]
+parse_ = either (error "Bad parse") id . parseOnly ((line `sepBy1'` char '\n') <* endOfInput)
+  where
+    line = decimal `sepBy1'` char ' '
+
+isSafe :: [Int] -> Bool
+isSafe xs = (\(up, down, close) -> (up || down) && close) . foldl' f (True, True, True) . zip xs $ drop 1 xs
+  where
+    f (up, down, close) (x, y) = (up && x < y, down && x > y, close && abs (x - y) < 4)
+
+dropAtMost1 :: [Int] -> [[Int]]
+dropAtMost1 xs = filter (\zs -> length zs >= pred (length xs)) (subsequences xs)
+
+part1, part2 :: [Int] -> Bool
+part1 = isSafe
+part2 = any isSafe . dropAtMost1
+
+main :: IO ()
+main = do
+    input <- parse_ <$> T.readFile "input.txt"
+    traverse_ (print . (\p -> length $ filter p input)) [part1, part2]
