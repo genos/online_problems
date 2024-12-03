9cf1bd66f8d8a7e558c7f499c1ae6f086d2ad1b4
Graham Enos <graham@strangeworks.com>
2024-12-02 22:47:46 -0500

    day2 done

--- advent_of_code_2024/day02/Main.hs
+++ advent_of_code_2024/day02/Main.hs
@@ -4,6 +4,7 @@ module Main where
 
 import Data.Attoparsec.Text
 import Data.Foldable (traverse_)
+import Data.List (subsequences)
 import Data.Text (Text)
 import Data.Text.IO qualified as T
 
@@ -20,11 +21,16 @@ isSafe xs = (monotonic xs && close xs) || (monotonic xs' && close xs')
   where
     xs' = reverse xs
     monotonic zs = and . zipWith (<) zs $ tail zs
-    close zs = all (< 4) . fmap abs . zipWith (-) zs $ tail zs
+    close zs = all ((< 4) . abs) . zipWith (-) zs $ tail zs
+
+atMost1Drop :: [Int] -> [[Int]]
+atMost1Drop xs = filter (\zs -> length zs >= n') (subsequences xs)
+  where
+    n' = pred $ length xs
 
 part1, part2 :: [[Int]] -> Int
 part1 = length . filter isSafe
-part2 = undefined
+part2 = length . filter (any isSafe . atMost1Drop)
 
 main :: IO ()
 main = do
