485ab3b3544a95591248208e97709aa7b1876adb
Graham Enos <graham@strangeworks.com>
2024-12-02 21:50:55 -0500

    day01 part 2

--- advent_of_code_2024/day01/Main.hs
+++ advent_of_code_2024/day01/Main.hs
@@ -3,6 +3,8 @@
 module Main where
 
 import Data.Attoparsec.Text
+import Data.Foldable (traverse_)
+import Data.IntMap qualified as I
 import Data.List (sort)
 import Data.Text (Text)
 import Data.Text.IO qualified as T
@@ -18,5 +20,13 @@ readL = either (error "Bad pasre") unzip . parseOnly ((pair `sepBy1` "\n") <* sk
 part1 :: ([Int], [Int]) -> Int
 part1 (xs, ys) = sum . fmap abs $ zipWith (-) (sort xs) (sort ys)
 
+part2 :: ([Int], [Int]) -> Int
+part2 (xs, ys) = sum $ fmap get xs
+  where
+    zs = I.fromListWith (+) $ fmap (,1) ys
+    get x = x * I.findWithDefault 0 x zs
+
 main :: IO ()
-main = print . part1 . readL =<< T.readFile "input.txt"
+main = do
+    input <- readL <$> T.readFile "input.txt"
+    traverse_ (print . ($ input)) [part1, part2]
