197e2740a28835418c6e12b6db2a6f779449bc77
Graham Enos <graham@strangeworks.com>
2024-12-04 10:41:54 -0500

    solved

--- advent_of_code_2024/day04/Main.hs
+++ advent_of_code_2024/day04/Main.hs
@@ -5,19 +5,31 @@ import Data.Map.Strict (Map)
 import Data.Map.Strict qualified as M
 import Linear.V2 (V2 (..))
 
-parse :: String -> Map (V2 Int) Char
+type Coord = V2 Int
+type Dir = V2 Int
+type Board = Map Coord Char
+
+parse :: String -> Board
 parse input = M.fromList [(V2 i j, c) | (i, l) <- zip [0 ..] $ lines input, (j, c) <- zip [0 ..] l]
 
-countXMAS :: Map (V2 Int) Char -> V2 Int -> Int
-countXMAS board direction = M.size $ M.filterWithKey (\xy _ -> and $ zipWith is (line xy) "XMAS") board
+part1 :: Board -> Int
+part1 b = sum $ [countXMAS (V2 i j) | i <- [-1 .. 1], j <- [-1 .. 1], (i, j) /= (0, 0)]
   where
-    is xy c = board M.!? xy == Just c
-    line xy = take 4 $ iterate (+ direction) xy
+    countXMAS dir = M.size $ M.filterWithKey (\xy _ -> and $ zipWith is (line xy dir) "XMAS") b
+    line xy dir = take 4 $ iterate (+ dir) xy
+    is xy c = b M.!? xy == Just c
 
-part1 :: Map (V2 Int) Char -> Int
-part1 board = sum $ [countXMAS board (V2 i j) | i <- [-1 .. 1], j <- [-1 .. 1], (i, j) /= (0, 0)]
+part2 :: Board -> Int
+part2 b = sum $ fmap countX_MAS [(V2 1 0, V2 0 1), (V2 0 1, V2 1 0), (V2 (-1) 0, V2 0 (-1)), (V2 0 (-1), V2 (-1) 0)]
+  where
+    countX_MAS (d1, d2) =
+        M.size $
+            M.filterWithKey
+                (\xy _ -> and [xy `is` 'M', (xy + 2 * d1) `is` 'M', (xy + d1 + d2) `is` 'A', (xy + 2 * d2) `is` 'S', (xy + 2 * (d1 + d2)) `is` 'S'])
+                b
+    is xy c = b M.!? xy == Just c
 
 main :: IO ()
 main = do
-  input <- parse <$> readFile "input.txt"
-  traverse_ (print . ($ input)) [part1]
+    input <- parse <$> readFile "input.txt"
+    traverse_ (print . ($ input)) [part1, part2]
