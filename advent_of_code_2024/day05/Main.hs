7eb3827f66b53d7c31d69c09e2de7e78a7cb1a86
Graham <genos@users.noreply.github.com>
2024-12-05 11:00:43 -0500

    Day05 (#108)
    * day05
    
    * part2
    
    * cleanup
    
    * tighten
    
    * squeeze

---
+++ advent_of_code_2024/day05/Main.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Data.Attoparsec.Text
+import Data.Bifunctor (second)
+import Data.Foldable (traverse_)
+import Data.IntMap.Strict (IntMap)
+import Data.IntMap.Strict qualified as IM
+import Data.IntSet (IntSet)
+import Data.IntSet qualified as IS
+import Data.List (sortBy)
+import Data.Text (Text)
+import Data.Text.IO qualified as T
+
+parse_ :: Text -> (IntMap IntSet, [[Int]])
+parse_ = either (error "Bad parse") id . parseOnly ((,) <$> rules <*> ("\n\n" *> pages))
+  where
+    rules = IM.fromListWith (<>) . fmap (second IS.singleton) <$> pairs
+    pages = (decimal `sepBy1'` ",") `sepBy1'` endOfLine
+    pairs = ((,) <$> decimal <*> ("|" *> decimal)) `sepBy1'` "\n"
+
+middle :: [Int] -> Int
+middle xs = xs !! (length xs `div` 2)
+
+inOrder :: IntMap IntSet -> [Int] -> Bool
+inOrder rs = fst . foldl' f (True, mempty)
+  where
+    f (ok, seen) x = (ok && IS.null (IS.intersection (IM.findWithDefault mempty x rs) seen), IS.insert x seen)
+
+part1 :: IntMap IntSet -> [[Int]] -> Int
+part1 rs = sum . fmap middle . filter (inOrder rs)
+
+part2 :: IntMap IntSet -> [[Int]] -> Int
+part2 rs = sum . fmap (middle . sortBy (⋈)) . filter (not . inOrder rs)
+  where
+    x ⋈ y = if IS.member y (IM.findWithDefault mempty x rs) then GT else LT
+
+main :: IO ()
+main = do
+    input <- parse_ <$> T.readFile "input.txt"
+    traverse_ (print . ($ input) . uncurry) [part1, part2]
