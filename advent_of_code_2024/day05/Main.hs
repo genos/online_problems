74708945538a4b21b291651a2a913099e4b13158
Graham Enos <graham@strangeworks.com>
2024-12-05 10:36:41 -0500

    part2

--- advent_of_code_2024/day05/Main.hs
+++ advent_of_code_2024/day05/Main.hs
@@ -7,33 +7,38 @@ import Data.IntMap.Strict (IntMap)
 import Data.IntMap.Strict qualified as IM
 import Data.IntSet (IntSet)
 import Data.IntSet qualified as IS
+import Data.List (sortBy)
 import Data.Text (Text)
 import Data.Text.IO qualified as T
-import Data.Vector.Unboxed (Vector)
-import Data.Vector.Unboxed qualified as U
 
-type Vec = Vector Int
-type Rules = IntMap IntSet
-
-parse_ :: Text -> (Rules, [Vec])
+parse_ :: Text -> (IntMap IntSet, [[Int]])
 parse_ = either (error "Bad parse") id . parseOnly ((,) <$> rules <*> (endOfLine *> endOfLine *> pages))
   where
     rules = IM.fromListWith (<>) . fmap (second IS.singleton) <$> pairs
-    pages = fmap U.fromList <$> (decimal `sepBy1'` char ',') `sepBy1'` endOfLine
+    pages = (decimal `sepBy1'` char ',') `sepBy1'` endOfLine
     pairs = ((,) <$> decimal <*> (char '|' *> decimal)) `sepBy1'` endOfLine
 
-middle :: Vec -> Int
-middle xs = U.unsafeIndex xs (U.length xs `div` 2)
+middle :: [Int] -> Int
+middle xs = xs !! (length xs `div` 2)
 
-inOrder :: Rules -> Vec -> Bool
-inOrder r = fst . U.foldl' f (True, IS.empty)
+inOrder :: IntMap IntSet -> [Int] -> Bool
+inOrder rs = fst . foldl' f (True, mempty)
   where
-    f (ok, seen) x = (ok && IS.null (IS.intersection (IM.findWithDefault IS.empty x r) seen), IS.insert x seen)
+    f (ok, seen) x = (ok && IS.null (IS.intersection (IM.findWithDefault mempty x rs) seen), IS.insert x seen)
 
-part1 :: Rules -> [Vec] -> Int
-part1 r = sum . fmap middle . filter (inOrder r)
+part1 :: IntMap IntSet -> [[Int]] -> Int
+part1 rs = sum . fmap middle . filter (inOrder rs)
+
+part2 :: IntMap IntSet -> [[Int]] -> Int
+part2 rs = sum . fmap (middle . sortBy order) . filter (not . inOrder rs)
+  where
+    order x y = case (after x y, after y x) of
+        (True, False) -> GT
+        (False, True) -> LT
+        _ -> EQ
+    after x y = IS.member y (IM.findWithDefault mempty x rs)
 
 main :: IO ()
 main = do
     input <- parse_ <$> T.readFile "input.txt"
-    traverse_ (print . ($ input)) [uncurry part1]
+    traverse_ (print . ($ input) . uncurry) [part1, part2]
