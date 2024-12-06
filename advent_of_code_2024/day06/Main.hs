339c15a32a2150074afa2e6ae8bff3c56c17f370
Graham Enos <graham@strangeworks.com>
2024-12-06 11:33:43 -0500

    tighten

--- advent_of_code_2024/day06/Main.hs
+++ advent_of_code_2024/day06/Main.hs
@@ -30,15 +30,15 @@ turnRight = \case U -> R; R -> D; D -> L; L -> U
 toStep :: Dir -> V2 Int
 toStep = \case U -> V2 1 0; D -> V2 (-1) 0; R -> V2 0 1; L -> V2 0 (-1)
 
-next :: (Ord x) => (Coord -> Dir -> x) -> Map -> Guard x -> Guard x
+next :: (Ord a) => (Coord -> Dir -> a) -> Map -> Guard a -> Guard a
 next f m Guard{pos, dir, visited} = Guard pos' dir' visited'
   where
     is xy c = m M.!? xy == Just c
-    forward = pos + toStep dir
-    blocked = forward `is` '#'
-    done = not $ M.member forward m
+    ahead = pos + toStep dir
+    blocked = ahead `is` '#'
+    done = not $ M.member ahead m
     dir' = if blocked then turnRight dir else dir
-    pos' = if blocked || done then pos else forward
+    pos' = if blocked || done then pos else ahead
     visited' = if blocked || done then visited else S.insert (f pos' dir') visited
 
 goTilSame :: (Eq a) => (a -> a) -> a -> a
