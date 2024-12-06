a33148164746d26168804bc6393d092647468460
Graham Enos <graham@strangeworks.com>
2024-12-06 11:32:24 -0500

    generalizing

--- advent_of_code_2024/day06/Main.hs
+++ advent_of_code_2024/day06/Main.hs
@@ -7,22 +7,22 @@ import Linear.V2 (V2 (..))
 
 type Coord = V2 Int
 type Map = M.Map Coord Char
-data Dir = U | D | L | R deriving (Eq, Show)
-data Guard = Guard {pos :: Coord, dir :: Dir, visited :: S.Set Coord} deriving (Eq, Show)
+data Dir = U | D | L | R deriving (Eq)
+data Guard a = Guard {pos :: Coord, dir :: Dir, visited :: S.Set a} deriving (Eq)
 
-parse :: String -> (Map, Guard)
+parse :: String -> Map
 parse input =
-    start $
-        M.fromList
-            [ (V2 j i, c)
-            | (j, l) <- zip [0 ..] $ lines $ reverse input
-            , (i, c) <- zip [0 ..] $ reverse l
-            ]
+    M.fromList
+        [ (V2 j i, c)
+        | (j, l) <- zip [0 ..] $ lines $ reverse input
+        , (i, c) <- zip [0 ..] $ reverse l
+        ]
+
+start :: (Coord -> Dir -> a) -> Map -> (Map, Guard a)
+start f m = (m', Guard p U (S.singleton $ f p U))
   where
-    start m = (m', Guard p U (S.singleton p))
-      where
-        (p, _) = M.findMin $ M.filter (== '^') m
-        m' = M.update (const $ Just '.') p m
+    (p, _) = M.findMin $ M.filter (== '^') m
+    m' = M.update (const $ Just '.') p m
 
 turnRight :: Dir -> Dir
 turnRight = \case U -> R; R -> D; D -> L; L -> U
@@ -30,8 +30,8 @@ turnRight = \case U -> R; R -> D; D -> L; L -> U
 toStep :: Dir -> V2 Int
 toStep = \case U -> V2 1 0; D -> V2 (-1) 0; R -> V2 0 1; L -> V2 0 (-1)
 
-next :: Map -> Guard -> Guard
-next m Guard{pos, dir, visited} = Guard pos' dir' visited'
+next :: (Ord x) => (Coord -> Dir -> x) -> Map -> Guard x -> Guard x
+next f m Guard{pos, dir, visited} = Guard pos' dir' visited'
   where
     is xy c = m M.!? xy == Just c
     forward = pos + toStep dir
@@ -39,15 +39,15 @@ next m Guard{pos, dir, visited} = Guard pos' dir' visited'
     done = not $ M.member forward m
     dir' = if blocked then turnRight dir else dir
     pos' = if blocked || done then pos else forward
-    visited' = if blocked || done then visited else S.insert pos' visited
+    visited' = if blocked || done then visited else S.insert (f pos' dir') visited
 
 goTilSame :: (Eq a) => (a -> a) -> a -> a
 goTilSame f x = let y = f x in if y == x then x else goTilSame f y
 
-part1 :: Map -> Guard -> Int
-part1 m = S.size . visited . goTilSame (next m)
+part1 :: Map -> Int
+part1 m = let (m', g) = start const m in S.size . visited $ goTilSame (next const m') g
 
 main :: IO ()
 main = do
-    (m, g) <- parse <$> readFile "input.txt"
-    traverse_ (print . ($ (m, g)) . uncurry) [part1]
+    m <- parse <$> readFile "input.txt"
+    traverse_ (print . ($ m)) [part1]
