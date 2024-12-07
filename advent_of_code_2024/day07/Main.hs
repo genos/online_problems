2e2ad3c4c67aa57408440e512eda1e83f054f6e3
Graham Enos <graham@strangeworks.com>
2024-12-07 14:31:52 -0500

    squeeze

--- advent_of_code_2024/day07/Main.hs
+++ advent_of_code_2024/day07/Main.hs
@@ -32,9 +32,8 @@ part1 :: [Equation] -> Word64
 part1 = solve [(+), (*)]
 
 part2 :: [Equation] -> Word64
-part2 = solve [(+), (*), conc]
+part2 = solve [(+), (*), \x y -> x * tens 0 y + y]
   where
-    conc x y = x * tens 0 y + y
     tens :: Op
     tens t 0 = 10 ^ t
     tens t n = tens (succ t) (n `div` 10)
