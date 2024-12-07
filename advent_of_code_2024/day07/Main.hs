206170f3fd8c92bd9795c5bf3c93f992a6d39bd3
Graham Enos <graham@strangeworks.com>
2024-12-07 14:30:00 -0500

    my own tens

--- advent_of_code_2024/day07/Main.hs
+++ advent_of_code_2024/day07/Main.hs
@@ -34,17 +34,11 @@ part1 = solve [(+), (*)]
 part2 :: [Equation] -> Word64
 part2 = solve [(+), (*), conc]
   where
-    conc x y = x * 10 ^ nDigits y + y
+    conc x y = x * 10 ^ tens 0 y + y
     -- copy-pasta from number-length package
-    nDigits :: Word64 -> Word64
-    nDigits n
-        | n < 10 = 1
-        | n < 100 = 2
-        | n < 1000 = 3
-        | n < 10000 = 4
-        | n >= 10000000000000000 = 16 + nDigits (n `div` 10000000000000000)
-        | n >= 100000000 = 8 + nDigits (n `div` 100000000)
-        | otherwise = 4 + nDigits (n `div` 10000)
+    tens :: Op
+    tens t 0 = 10 ^ t
+    tens t n = tens (succ t) (n `div` 10)
 
 main :: IO ()
 main = do
