a172e19eab0355c6eb0726961950cd7563a87a90
Graham Enos <graham@strangeworks.com>
2024-12-09 19:00:18 -0500

    squeeze

--- advent_of_code_2024/day09/src/main.rs
+++ advent_of_code_2024/day09/src/main.rs
@@ -3,16 +3,13 @@ use eyre::{Result, WrapErr};
 use std::fs;
 
 fn parse(input: &str) -> Vec<Option<usize>> {
-    let mut i = 0;
-    let mut is_file = true;
-    let mut output = vec![];
+    let (mut i, mut is_file, mut output) = (0, true, vec![]);
     for c in input.chars() {
         if c.is_numeric() {
             let d = c.to_digit(10).expect("is_numeric() should handle this.") as usize;
             let to_add = if is_file {
-                let x = Some(i);
                 i += 1;
-                x
+                Some(i - 1)
             } else {
                 None
             };
@@ -35,11 +32,10 @@ fn part1(mut blocks: Vec<Option<usize>>) -> usize {
         if left < right {
             blocks.swap(left, right);
         }
-        left += 1;
-        right -= 1;
+        (left, right) = (left + 1, right - 1);
     }
     blocks
-        .into_iter()
+        .iter()
         .enumerate()
         .filter_map(|(i, b)| b.map(|n| i * n))
         .sum()
