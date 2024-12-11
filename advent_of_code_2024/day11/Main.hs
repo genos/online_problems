9cfd0f96b78adfc636963b88f02673aba155b654
Graham Enos <graham@strangeworks.com>
2024-12-11 11:00:15 -0500

    part1, naïve

--- advent_of_code_2024/day11/Main.hs
+++ advent_of_code_2024/day11/Main.hs
@@ -6,13 +6,33 @@ import Data.Word (Word64)
 parse :: String -> [Word64]
 parse = fmap read . words
 
+digits :: Word64 -> [Word64]
+digits = go []
+  where
+    go ds n =
+        let (q, r) = n `divMod` 10
+            ds' = r : ds
+         in if q == 0 then ds' else go ds' q
+
+undigits :: [Word64] -> Word64
+undigits = foldl' (\acc d -> 10 * acc + d) 0
+
+blink :: Word64 -> [Word64]
+blink 0 = [1]
+blink n | even d = [undigits l, undigits r]
+  where
+    ds = digits n
+    d = length ds
+    (l, r) = splitAt (d `div` 2) ds
+blink n = [2024 * n]
+
 part1 :: [Word64] -> Int
-part1 = undefined
+part1 = length . (!! 25) . iterate (concatMap blink)
 
 part2 :: [Word64] -> Int
-part2 = undefined
+part2 = length . (!! 75) . iterate (concatMap blink)
 
 main :: IO ()
 main = do
-    m <- parse <$> readFile "test.txt"
+    m <- parse <$> readFile "input.txt"
     traverse_ (print . ($ m)) [part1, part2]
