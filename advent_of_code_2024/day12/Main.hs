d82dffe685b6987ed0b6be530c845e4ea877eb7d
Graham Enos <graham@strangeworks.com>
2024-12-12 15:17:22 -0500

    part2 wip

--- advent_of_code_2024/day12/Main.hs
+++ advent_of_code_2024/day12/Main.hs
@@ -7,7 +7,14 @@ import Linear.V2 (V2 (..))
 
 type Coord = V2 Int
 type Map = M.Map Coord Char
-data Region = Region {_plant :: Char, _plots :: S.Set Coord} deriving (Eq, Ord, Show)
+data Region = Region
+    { _plant :: {-# UNPACK #-} !Char
+    , _plots :: S.Set Coord
+    , _area :: {-# UNPACK #-} !Int
+    , _perimeter :: {-# UNPACK #-} !Int
+    , _sides :: {-# UNPACK #-} !Int
+    }
+    deriving (Eq, Ord, Show)
 type Garden = S.Set Region
 
 parse :: String -> Map
@@ -18,35 +25,36 @@ parse input =
         , (i, c) <- zip [0 ..] l
         ]
 
-nbd :: Coord -> [Coord]
-nbd c = [c + u | u <- [V2 (-1) 0, V2 1 0, V2 0 (-1), V2 0 1]]
-
-area, perimeter, price :: Region -> Int
-area = S.size . _plots
-perimeter r = S.foldl' (\acc c -> acc + f c) 0 ps
-  where
-    f c = 4 - sum [1 | n <- nbd c, n `S.member` ps]
-    ps = _plots r
-price = (*) <$> area <*> perimeter
-
 plant :: Map -> Coord -> Region
-plant m c = go [c] S.empty
+plant m c = go (S.singleton c) S.empty 0 0 0
   where
-    p = m M.! c
-    go [] cs = Region p cs
-    go (x : xs) cs = go ([y | y <- nbd x, not $ y `S.member` cs, m M.!? y == Just p] <> xs) (S.insert x cs)
+    pl = m M.! c
+    go xs cs a p s
+        | S.null xs = Region pl cs a p s
+        | otherwise = go (ys `S.union` ss) (S.insert x cs) (a + 1) (p + S.size ds) s'
+      where
+        (x, ys) = S.deleteFindMin xs
+        nbd = S.fromList [x + u | u <- [V2 (-1) 0, V2 1 0, V2 0 (-1), V2 0 1]]
+        (same, diff) = S.partition ((== Just pl) . (m M.!?)) nbd
+        ss = same S.\\ cs
+        ds = diff S.\\ cs
+        s' = s + S.size ds - S.size ss
 
 grow :: Map -> Garden
 grow m = go (M.keysSet m) S.empty
   where
     go cs g
         | S.null cs = g
-        | otherwise = let (c, cs') = S.deleteFindMin cs; r = plant m c in go cs' (S.insert r g)
+        | otherwise = let (c, cs') = S.deleteFindMin cs; r = plant m c in go (cs' S.\\ _plots r) (S.insert r g)
 
-part1 :: Garden -> Int
-part1 = S.foldl' (\p r -> p + price r) 0
+solve :: (Region -> Int) -> Garden -> Int
+solve f = S.foldl' (\p r -> p + _area r * f r) 0
+
+part1, part2 :: Garden -> Int
+part1 = solve _perimeter
+part2 = solve _sides
 
 main :: IO ()
 main = do
-    g <- grow . parse <$> readFile "input.txt"
-    traverse_ (print . ($ g)) [part1]
+    g <- grow . parse <$> readFile "test.txt"
+    traverse_ (print . ($ g)) [part1, part2]
