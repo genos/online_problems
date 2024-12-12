04be307c55895aba43809a3ca994347634d6cac9
Graham Enos <graham@strangeworks.com>
2024-12-12 18:58:30 -0500

    fin

--- advent_of_code_2024/day12/Main.hs
+++ advent_of_code_2024/day12/Main.hs
@@ -7,54 +7,65 @@ import Linear.V2 (V2 (..))
 
 type Coord = V2 Int
 type Map = M.Map Coord Char
-data Region = Region
-    { _plant :: {-# UNPACK #-} !Char
-    , _plots :: S.Set Coord
-    , _area :: {-# UNPACK #-} !Int
-    , _perimeter :: {-# UNPACK #-} !Int
-    , _sides :: {-# UNPACK #-} !Int
-    }
-    deriving (Eq, Ord, Show)
+data Region = R {_plant :: Char, _plots :: S.Set Coord} deriving (Eq, Ord, Show)
 type Garden = S.Set Region
 
-parse :: String -> Map
-parse input =
-    M.fromList
-        [ (V2 i j, c)
-        | (j, l) <- zip [0 ..] $ reverse $ lines input
-        , (i, c) <- zip [0 ..] l
-        ]
-
-plant :: Map -> Coord -> Region
-plant m c = go (S.singleton c) S.empty 0 0 0
+parse :: String -> Garden
+parse input = grow (M.keysSet m) S.empty
   where
-    pl = m M.! c
-    go xs cs a p s
-        | S.null xs = Region pl cs a p s
-        | otherwise = go (ys `S.union` ss) (S.insert x cs) (a + 1) (p + S.size ds) s'
-      where
-        (x, ys) = S.deleteFindMin xs
-        nbd = S.fromList [x + u | u <- [V2 (-1) 0, V2 1 0, V2 0 (-1), V2 0 1]]
-        (same, diff) = S.partition ((== Just pl) . (m M.!?)) nbd
-        ss = same S.\\ cs
-        ds = diff S.\\ cs
-        s' = s + S.size ds - S.size ss
-
-grow :: Map -> Garden
-grow m = go (M.keysSet m) S.empty
-  where
-    go cs g
+    m = M.fromList [(V2 i j, c) | (j, l) <- zip [0 ..] $ reverse $ lines input, (i, c) <- zip [0 ..] l]
+    grow cs g
         | S.null cs = g
-        | otherwise = let (c, cs') = S.deleteFindMin cs; r = plant m c in go (cs' S.\\ _plots r) (S.insert r g)
+        | otherwise = grow cs'' (S.insert r g)
+      where
+        (c, cs') = S.deleteFindMin cs
+        r = plant c
+        cs'' = cs' S.\\ _plots r
+    plant c = go (S.singleton c) S.empty
+      where
+        pl = m M.! c
+        go todo done
+            | S.null todo = R pl done
+            | otherwise = go (xs `S.union` ss) (S.insert x done)
+          where
+            (x, xs) = S.deleteFindMin todo
+            same = S.filter ((== Just pl) . (m M.!?)) . S.fromList $ nbd x
+            ss = same S.\\ done
+
+nbd :: Coord -> [Coord]
+nbd x = [x + u | u <- [V2 (-1) 0, V2 1 0, V2 0 (-1), V2 0 1]]
+
+area, perimeter, sides :: Region -> Int
+area = S.size . _plots
+perimeter (R _ ps) = S.foldl' (\acc c -> acc + f c) 0 ps
+  where
+    f c = 4 - sum [1 | n <- nbd c, n `S.member` ps]
+sides (R _ ps) = S.foldl' (\acc c -> acc + sum (corners <$> xNbd c)) 0 ps
+  where
+    corners (x, y, z) = b2i convex + b2i concave
+      where
+        ins@(x', y', _) = (x `S.member` ps, y `S.member` ps, z `S.member` ps)
+        convex = ins == (True, True, False)
+        concave = (x', y') == (False, False)
+    xNbd c =
+        [ (c + x, c + y, c + z)
+        | (x, y, z) <-
+            [ (V2 (-1) 0, V2 0 1, V2 (-1) 1)
+            , (V2 1 0, V2 0 1, V2 1 1)
+            , (V2 1 0, V2 0 (-1), V2 1 (-1))
+            , (V2 (-1) 0, V2 0 (-1), V2 (-1) (-1))
+            ]
+        ]
+    b2i b = if b then 1 else 0
 
 solve :: (Region -> Int) -> Garden -> Int
-solve f = S.foldl' (\p r -> p + _area r * f r) 0
+solve f = S.foldl' (\p r -> p + area r * f r) 0
 
 part1, part2 :: Garden -> Int
-part1 = solve _perimeter
-part2 = solve _sides
+part1 = solve perimeter
+part2 = solve sides
 
 main :: IO ()
 main = do
-    g <- grow . parse <$> readFile "test.txt"
+    g <- parse <$> readFile "input.txt"
     traverse_ (print . ($ g)) [part1, part2]
