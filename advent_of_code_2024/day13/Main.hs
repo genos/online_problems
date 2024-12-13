1585a0d9b7abe65ecdb770b9f5843f7e7e659a5b
Graham <genos@users.noreply.github.com>
2024-12-13 14:01:46 -0500

    Day13 (#115)
    * day13
    
    * part1
    
    * tidy
    
    * part2
    
    * cleaner with quotRem

---
+++ advent_of_code_2024/day13/Main.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Control.Applicative ((<|>))
+import Data.Attoparsec.Text
+import Data.Foldable (traverse_)
+import Data.Text (Text)
+import Data.Text.IO qualified as T
+
+data Button = B {_x :: {-# UNPACK #-} !Int, _y :: {-# UNPACK #-} !Int}
+data Machine = M
+    { _a :: !Button
+    , _b :: !Button
+    , _prizeX :: {-# UNPACK #-} !Int
+    , _prizeY :: {-# UNPACK #-} !Int
+    }
+
+parse_ :: Text -> [Machine]
+parse_ = either (error "Bad parse") id . parseOnly (m `sepBy1'` "\n\n")
+  where
+    m = M <$> b <*> ("\n" *> b) <*> ("\n" *> ("Prize: X=" *> decimal)) <*> (", Y=" *> decimal)
+    b = B <$> ("Button " *> ("A" <|> "B") *> ": X+" *> decimal) <*> (", Y+" *> decimal)
+
+{-
+part1 :: [Machine] -> Int
+part1 = sum . fmap minimum . filter (not . null) . fmap f
+  where
+    f (M a b x y) =
+        [ 3 * p + q
+        | p <- [0 .. 100]
+        , q <- [0 .. 100]
+        , x == p * _x a + q * _x b
+        , y == p * _y a + q * _y b
+        ]
+-}
+
+tokens :: Machine -> Int
+tokens (M (B ax ay) (B bx by) x y) = if ok then 3 * a + b else 0
+  where
+    -- https://en.wikipedia.org/wiki/Cramer%27s_rule#Explicit_formulas_for_small_systems
+    ok = ra == 0 && rb == 0
+    (a, ra) = (x * by - bx * y) `quotRem` d
+    (b, rb) = (ax * y - x * ay) `quotRem` d
+    d = ax * by - bx * ay
+
+part1, part2 :: [Machine] -> Int
+part1 = sum . fmap tokens
+part2 = sum . fmap (tokens . \m@(M _ _ x y) -> m{_prizeX = x + big, _prizeY = y + big})
+  where
+    big = 10 ^ (13 :: Int)
+
+main :: IO ()
+main = do
+    input <- parse_ <$> T.readFile "input.txt"
+    traverse_ (print . ($ input)) [part1, part2]
