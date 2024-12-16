9c9a67f377e6f93196862a79b1679d8fc78b9896
Graham Enos <graham@strangeworks.com>
2024-12-16 15:40:37 -0500

    parsing

--- advent_of_code_2024/day15/Main.hs
+++ advent_of_code_2024/day15/Main.hs
@@ -2,8 +2,41 @@
 
 module Main where
 
+import Data.List (elemIndex)
+import Data.Map.Strict qualified as M
+import Data.Maybe (fromJust)
+import Data.Monoid (First (..))
+import Data.Text (Text)
+import Data.Text qualified as T
 import Data.Text.IO qualified as T
+import Linear.V2
+
+type Coord = V2 Int
+data Spot = Box | Open | Closed deriving (Show)
+type Map = M.Map Coord Spot
+data Instruction = U | D | L | R deriving (Show)
+
+makeC2X :: String -> [a] -> (Char -> a)
+makeC2X s as = let m = M.fromList $ zip s as in (m M.!)
+
+c2s :: Char -> Spot
+c2s = makeC2X "O.@#" [Box, Open, Open, Closed]
+
+c2i :: Char -> Instruction
+c2i = makeC2X "^v<>" [U, D, L, R]
+
+parse :: Text -> (Map, Coord, [Instruction])
+parse t = (m, fromJust $ getFirst g, is)
+  where
+    (m, g) = foldl' f (M.empty, mempty) . zip [0 ..] $ T.lines left
+    is = concatMap (T.foldr' ((:) . c2i) []) $ T.lines right
+    (left, right) = T.breakOn "\n\n" t
+    f (m', g') (j, l) =
+        let l' = T.unpack l
+            cs = [(V2 i j, c2s c) | (i, c) <- zip [0 ..] l']
+            g'' = First ((`V2` j) <$> elemIndex '@' l')
+         in (M.union m' $ M.fromList cs, g' <> g'')
 
 main :: IO ()
-main = 
-  T.putStrLn "TODO"
+main =
+    T.putStrLn "TODO"
