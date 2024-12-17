68554ee902c4b60e38cace43574f7a2e42bdfd6e
Graham <genos@users.noreply.github.com>
2024-12-17 16:25:52 -0500

    Day17 (#118)
    * day 17 part 1
    
    * tighten part 1
    
    * part 2 brute force
    
    * squeeze
    
    * rusty
    
    * shiftL!
    
    * pythonZ3 ftw

---
+++ advent_of_code_2024/day17/Main.hs
@@ -0,0 +1,73 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module Main where
+
+import Control.Lens
+import Data.Attoparsec.Text
+import Data.Bits (shiftL, xor)
+import Data.Text (Text)
+import Data.Text qualified as T
+import Data.Text.IO qualified as T
+import Data.Vector (Vector)
+import Data.Vector qualified as V
+
+data Computer = C {_regA :: Int, _regB :: Int, _regC :: Int, _ip :: Int, _ops :: Vector Int}
+makeLenses ''Computer
+
+parse_ :: Text -> Computer
+parse_ = either (error "Bad parse") id . parseOnly c
+  where
+    c = C <$> rA <*> rB <*> rC <*> pure 0 <*> ws
+    rA = "Register A: " *> decimal
+    rB = "\nRegister B: " *> decimal
+    rC = "\nRegister C: " *> decimal
+    ws = V.fromList <$> ("\n\nProgram: " *> (decimal `sepBy1'` ","))
+
+combo :: Computer -> Int -> Int
+combo c =
+    \case
+        n | 0 <= n && n <= 3 -> n
+        4 -> c ^. regA
+        5 -> c ^. regB
+        6 -> c ^. regC
+        n -> error $ "Bad combo value: " <> show n
+
+run :: Computer -> [Int]
+run = go []
+  where
+    go output computer@(C a b c i ws)
+        | i >= V.length ws - 1 = reverse output
+        | otherwise =
+            let
+                code = ws V.! i
+                word = ws V.! (i + 1)
+                com = combo computer word
+                i' = if a /= 0 && code == 3 then word else i + 2
+                output' = if code == 5 then (com `mod` 8) : output else output
+                computer' = case code of
+                    0 -> computer & regA .~ a `shiftL` com
+                    1 -> computer & regB .~ xor b word
+                    2 -> computer & regB .~ com `mod` 8
+                    3 -> computer & ip .~ i'
+                    4 -> computer & regB .~ xor b c
+                    5 -> computer
+                    6 -> computer & regB .~ a `shiftL` com
+                    7 -> computer & regC .~ a `shiftL` com
+                    _ -> error $ "Bad code: " <> show code
+             in
+                go output' (computer' & ip .~ i')
+
+part1 :: Computer -> Text
+part1 = T.init . T.tail . T.pack . show . run
+
+part2 :: Computer -> Maybe Int
+part2 c = V.find quine $ V.enumFromTo 0 maxBound
+  where
+    quine a = run (c & regA .~ a) == (c ^. ops & V.toList)
+
+main :: IO ()
+main = do
+    input <- parse_ <$> T.readFile "input.txt"
+    T.putStrLn $ part1 input
+    print $ part2 input
---
+++ advent_of_code_2024/day17/src/main.rs
@@ -0,0 +1,97 @@
+use eyre::Result;
+use rayon::prelude::*;
+use std::fs;
+
+#[derive(Clone)]
+struct Computer {
+    a: usize,
+    b: usize,
+    c: usize,
+    ip: usize,
+    ops: Vec<usize>,
+}
+
+peg::parser! {
+    grammar parser() for str {
+        pub rule computer() -> Computer =
+            "Register A: " a:num()
+            "\nRegister B: " b:num()
+            "\nRegister C: " c:num()
+            "\n\nProgram: " ops:(num() ++",") {
+                Computer { a, b, c, ip: 0, ops }
+            }
+        rule num() -> usize = n:$(['0'..='9']['0'..='9']*) {? n.parse().or(Err("num")) }
+    }
+}
+
+impl Computer {
+    #[allow(clippy::inline_always)]
+    #[inline(always)]
+    fn combo(&self, w: usize) -> usize {
+        match w {
+            4 => self.a,
+            5 => self.b,
+            6 => self.c,
+            _ => w, // pass through invalid states, too
+        }
+    }
+    fn run(&mut self) -> Vec<usize> {
+        let mut output = vec![];
+        while self.ip < self.ops.len() {
+            // safety checked above
+            let code = unsafe { *self.ops.get_unchecked(self.ip) };
+            let word = unsafe { *self.ops.get_unchecked(self.ip + 1) };
+            match code {
+                0 => self.a >>= self.combo(word),
+                1 => self.b ^= word,
+                2 => self.b = self.combo(word) % 8,
+                3 => {
+                    if self.a != 0 {
+                        self.ip = word;
+                        continue;
+                    }
+                }
+                4 => self.b ^= self.c,
+                5 => {
+                    #[allow(clippy::cast_possible_truncation)]
+                    output.push(self.combo(word) % 8);
+                }
+                6 => self.b = self.a >> self.combo(word),
+                7 => self.c = self.a >> self.combo(word),
+                _ => {} // invalid, pass through
+            }
+            self.ip += 2;
+        }
+        output
+    }
+    fn part1(&mut self) {
+        let mut sep = "";
+        for x in &self.run() {
+            print!("{sep}{x}");
+            sep = ",";
+        }
+        println!();
+    }
+    fn set_a(&mut self, a: usize) -> &mut Self {
+        self.a = a;
+        self
+    }
+    fn part2(&self) {
+        println!(
+            "{:?}",
+            (0..usize::MAX)
+                .into_par_iter()
+                .by_exponential_blocks()
+                .find_first(|&i| self.clone().set_a(i).run() == self.ops)
+        );
+    }
+}
+
+fn main() -> Result<()> {
+    let input = fs::read_to_string("input.txt")?;
+    let mut c = parser::computer(&input)?;
+    c.part1();
+    c.ip = 0;
+    c.part2();
+    Ok(())
+}
