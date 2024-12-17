2b44e1e4b7721e23de604d1d907bfe2c2c1f9f56
Graham Enos <graham@strangeworks.com>
2024-12-17 15:06:25 -0500

    squeeze

--- advent_of_code_2024/day17/Main.hs
+++ advent_of_code_2024/day17/Main.hs
@@ -6,68 +6,65 @@ module Main where
 import Control.Lens
 import Data.Attoparsec.Text
 import Data.Bits (xor)
-import Data.Foldable (toList)
-import Data.Sequence qualified as S
 import Data.Text (Text)
 import Data.Text qualified as T
 import Data.Text.IO qualified as T
 import Data.Vector (Vector)
 import Data.Vector qualified as V
 
-data Registers = R {_regA :: Int, _regB :: Int, _regC :: Int} deriving (Eq)
-data Computer = C {_registers :: Registers, _ip :: Int, _ops :: Vector Int, _output :: S.Seq Int} deriving (Eq)
-makeLenses ''Registers
+data Computer = C {_regA :: Int, _regB :: Int, _regC :: Int, _ip :: Int, _ops :: Vector Int}
 makeLenses ''Computer
 
 parse_ :: Text -> Computer
 parse_ = either (error "Bad parse") id . parseOnly c
   where
-    c = C <$> rs <*> pure 0 <*> ws <*> pure S.empty
-    rs = R <$> ("Register A: " *> decimal) <*> ("\nRegister B: " *> decimal) <*> ("\nRegister C: " *> decimal)
+    c = C <$> rA <*> rB <*> rC <*> pure 0 <*> ws
+    rA = "Register A: " *> decimal
+    rB = "\nRegister B: " *> decimal
+    rC = "\nRegister C: " *> decimal
     ws = V.fromList <$> ("\n\nProgram: " *> (decimal `sepBy1'` ","))
 
 combo :: Computer -> Int -> Int
 combo c =
     \case
         n | 0 <= n && n <= 3 -> n
-        4 -> c ^. registers . regA
-        5 -> c ^. registers . regB
-        6 -> c ^. registers . regC
+        4 -> c ^. regA
+        5 -> c ^. regB
+        6 -> c ^. regC
         n -> error $ "Bad combo value: " <> show n
 
-next :: Computer -> Computer
-next computer@(C (R a b c) i ws _)
-    | i >= V.length ws - 1 = computer
-    | otherwise = computer' & ip .~ i'
+run :: Computer -> [Int]
+run = go []
   where
-    code = ws V.! i
-    word = ws V.! (i + 1)
-    com = combo computer word
-    i' = if a /= 0 && code == 3 then word else i + 2
-    computer' = case code of
-        0 -> computer & (registers . regA) .~ a `div` 2 ^ com
-        1 -> computer & (registers . regB) .~ xor b word
-        2 -> computer & (registers . regB) .~ com `mod` 8
-        3 -> computer & ip .~ i'
-        4 -> computer & (registers . regB) .~ xor b c
-        5 -> computer & output |>~ com `mod` 8
-        6 -> computer & (registers . regB) .~ a `div` 2 ^ com
-        7 -> computer & (registers . regC) .~ a `div` 2 ^ com
-        _ -> error $ "Bad code: " <> show code
-
-run :: Computer -> Computer
-run c = let c' = next c in if c' == c then c else run c'
+    go output computer@(C a b c i ws)
+        | i >= V.length ws - 1 = reverse output
+        | otherwise =
+            let
+                code = ws V.! i
+                word = ws V.! (i + 1)
+                com = combo computer word
+                i' = if a /= 0 && code == 3 then word else i + 2
+                output' = if code == 5 then (com `mod` 8) : output else output
+                computer' = case code of
+                    0 -> computer & regA .~ a `div` 2 ^ com
+                    1 -> computer & regB .~ xor b word
+                    2 -> computer & regB .~ com `mod` 8
+                    3 -> computer & ip .~ i'
+                    4 -> computer & regB .~ xor b c
+                    5 -> computer
+                    6 -> computer & regB .~ a `div` 2 ^ com
+                    7 -> computer & regC .~ a `div` 2 ^ com
+                    _ -> error $ "Bad code: " <> show code
+             in
+                go output' (computer' & ip .~ i')
 
 part1 :: Computer -> Text
-part1 = T.init . T.tail . T.pack . show . toList . _output . run
-
-replicates :: Computer -> Int -> Bool
-replicates c a = (c' ^. output & toList) == (c ^. ops & toList)
-  where
-    c' = run (c & registers . regA .~ a)
+part1 = T.init . T.tail . T.pack . show . run
 
 part2 :: Computer -> Maybe Int
-part2 c = V.find (replicates c) $ V.enumFromTo 0 100_000_000
+part2 c = V.find quine $ V.enumFromTo 0 maxBound
+  where
+    quine a = run (c & regA .~ a) == (c ^. ops & V.toList)
 
 main :: IO ()
 main = do
