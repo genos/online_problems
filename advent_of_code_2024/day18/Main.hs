5bb5cbead64d8adae7ee252aa4f0c47d16db002b
Graham Enos <graham@strangeworks.com>
2024-12-18 18:40:57 -0500

    haskellllll

--- advent_of_code_2024/day18/Main.hs
+++ advent_of_code_2024/day18/Main.hs
@@ -4,8 +4,10 @@ module Main where
 
 import Algorithm.Search (aStar)
 import Data.Attoparsec.Text hiding (take)
+import Data.Foldable (find)
+import Data.List (tails)
 import Data.Set qualified as S
-import Data.Text qualified as T
+import Data.Text (Text)
 import Data.Text.IO qualified as T
 import Linear.V2
 
@@ -14,7 +16,7 @@ lo, hi :: Int
 lo = 0
 hi = 70
 
-parse_ :: T.Text -> [Coord]
+parse_ :: Text -> [Coord]
 parse_ = either (error "Bad parse") id . parseOnly cs
   where
     cs = c `sepBy1'` "\n"
@@ -25,13 +27,10 @@ dist c d = sum . abs $ c - d
 
 neighbors :: S.Set Coord -> (Coord -> [Coord])
 neighbors bs (V2 x y) =
-    filter
-        (\c -> (minimum c >= lo) && (maximum c <= hi) && not (c `S.member` bs))
-        [ V2 (x - 1) y
-        , V2 (x + 1) y
-        , V2 x (y - 1)
-        , V2 x (y + 1)
-        ]
+    [ c
+    | c <- [V2 (x - 1) y, V2 (x + 1) y, V2 x (y - 1), V2 x (y + 1)]
+    , minimum c >= lo && maximum c <= hi && not (c `S.member` bs)
+    ]
 
 search :: [Coord] -> Maybe (Int, [Coord])
 search cs = aStar (neighbors bs) dist (`dist` end) (== end) start
@@ -40,15 +39,14 @@ search cs = aStar (neighbors bs) dist (`dist` end) (== end) start
     start = pure lo
     end = pure hi
 
-part1 :: [Coord] -> Int
-part1 = maybe 0 fst . search . take 1024
+part1 :: [Coord] -> Maybe Int
+part1 = fmap fst . search . take 1024
 
--- part2 :: [Coord] -> T.Text
--- part2 = T.init . T.tail . T.pack . show . undefined
+part2 :: [Coord] -> Maybe Coord
+part2 = fmap last . find (null . search) . reverse . tails
 
 main :: IO ()
 main = do
     input <- parse_ <$> T.readFile "input.txt"
     print $ part1 input
-
--- T.putStrLn $ part2 input
+    print $ part2 input
