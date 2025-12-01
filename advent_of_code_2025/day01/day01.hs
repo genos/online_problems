5f31bbfb1c44c776fb4fba75028443f4404c1bba
Graham Enos <graham@strangeworks.com>
2025-12-01 13:55:03 -0500

    solved

--- advent_of_code_2025/day01/day01.hs
+++ advent_of_code_2025/day01/day01.hs
@@ -2,6 +2,8 @@ module Main (main) where
 
 import Control.Applicative ((<|>))
 import Data.Attoparsec.Text
+import Data.Bool (bool)
+import Data.Foldable (traverse_)
 import Data.Functor (($>))
 import Data.Text (Text)
 import Data.Text.IO qualified as T
@@ -9,12 +11,31 @@ import Data.Text.IO qualified as T
 parse_ :: Text -> [Int]
 parse_ = either (error "Bad parse") id . parseOnly (line `sepBy1'` char '\n')
   where
-    line = ((char 'L' $> id) <|> (char 'R' $> negate)) <*> decimal
+    line = ((char 'L' $> negate) <|> (char 'R' $> id)) <*> decimal
 
 part1 :: [Int] -> Int
-part1 = length . filter (== 0) . fmap (`mod` 100) . scanl (+) 50
+part1 = fst . foldl' f (0, 50)
+  where
+    f (n, s) i = let r = (s + i) `mod` 100 in (n + bool 0 1 (r == 0), r)
+
+part2 :: [Int] -> Int
+part2 = fst . foldl' f_ (0, 50)
+
+f_ :: (Int, Int) -> Int -> (Int, Int)
+f_ (n, s) = step (n, s)
+  where
+    step (a, b) c
+        | c == 0 = (a, b)
+        | otherwise =
+            let
+                a' = a + bool 0 1 (b' == 0)
+                b' = (b + g) `mod` 100
+                c' = c - g
+                g = signum c
+             in
+                step (a', b') c'
 
 main :: IO ()
 main = do
     input <- parse_ <$> T.readFile "input.txt"
-    print $ part1 input
+    traverse_ (print . ($ input)) [part1, part2]
