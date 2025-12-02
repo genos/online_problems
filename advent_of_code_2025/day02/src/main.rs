17548e07b07af5e6d9c083235aed1dc167f7e1c6
Graham Enos <graham@strangeworks.com>
2025-12-02 11:42:20 -0500

    squeeze

--- advent_of_code_2025/day02/src/main.rs
+++ advent_of_code_2025/day02/src/main.rs
@@ -10,20 +10,18 @@ fn read(s: &str) -> Vec<(u64, u64)> {
         .collect()
 }
 
-fn digits(n: u64) -> Vec<u8> {
+fn rev_digits(mut n: u64) -> Vec<u8> {
     let mut ds = vec![];
-    let mut k = n;
-    while k > 0 {
-        ds.push((k % 10) as u8);
-        k /= 10;
+    while n > 0 {
+        ds.push((n % 10) as u8);
+        n /= 10;
     }
-    ds.reverse();
     ds
 }
 
 fn solve(xs: &[(u64, u64)], pred: impl Fn(&[u8]) -> bool) -> u64 {
     xs.iter()
-        .map(|(lo, hi)| (*lo..=*hi).filter(|n| pred(&digits(*n))).sum::<u64>())
+        .map(|(lo, hi)| (*lo..=*hi).filter(|n| pred(&rev_digits(*n))).sum::<u64>())
         .sum()
 }
 
@@ -33,18 +31,15 @@ fn part_1(xs: &[u8]) -> bool {
 }
 
 fn part_2(xs: &[u8]) -> bool {
-    for i in 1..=xs.len() / 2 {
-        if xs.len() % i == 0
+    (1..=xs.len() / 2).any(|i| {
+        xs.len().is_multiple_of(i)
             && (0..xs.len() / i - 1).all(|j| xs[j * i..(j + 1) * i] == xs[(j + 1) * i..(j + 2) * i])
-        {
-            return true;
-        }
-    }
-    false
+    })
 }
 
 fn main() {
     let input = read(&fs::read_to_string("input.txt").expect("File to read"));
-    println!("{}", solve(&input, part_1));
-    println!("{}", solve(&input, part_2));
+    for p in [part_1, part_2] {
+        println!("{}", solve(&input, p));
+    }
 }
