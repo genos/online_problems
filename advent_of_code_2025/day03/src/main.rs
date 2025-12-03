4aab2c73db743d3f0229618e3a6ad2e8bef13096
Graham Enos <graham@strangeworks.com>
2025-12-03 10:19:09 -0500

    solved

--- advent_of_code_2025/day03/src/main.rs
+++ advent_of_code_2025/day03/src/main.rs
@@ -1,23 +1,51 @@
-fn digits(s: &str) -> Vec<Vec<u32>> {
+fn digits(s: &str) -> Vec<Vec<u64>> {
     s.trim()
         .lines()
-        .map(|l| l.chars().map(|c| c.to_digit(10).expect("digit")).collect())
+        .map(|l| {
+            l.chars()
+                .map(|c| u64::from(c.to_digit(10).expect("digit")))
+                .collect()
+        })
         .collect()
 }
 
-fn part_1(banks: &[Vec<u32>]) -> u32 {
-    banks
-        .iter()
-        .map(|bank| {
-            (0..bank.len())
-                .flat_map(|i| (i + 1..bank.len()).map(move |j| 10 * bank[i] + bank[j]))
-                .max()
-                .expect("nums")
-        })
-        .sum()
+fn argmax(bank: &[u64]) -> (usize, u64) {
+    let mut i = 0;
+    let mut m = bank[i];
+    for (j, b) in bank.iter().enumerate() {
+        if m < *b {
+            m = *b;
+            i = j;
+        }
+    }
+    (i, m)
+}
+
+fn solve_one(n: usize, bank: &[u64]) -> u64 {
+    let mut b = 0;
+    let mut i = 0;
+    let mut len = 0;
+    let mut j = bank.len() - n + 1;
+    while len < n {
+        len += 1;
+        let (k, m) = argmax(&bank[i..j]);
+        b = 10 * b + m;
+        i += k + 1;
+        j += 1;
+    }
+    b
+}
+
+fn part_1(banks: &[Vec<u64>]) -> u64 {
+    banks.iter().map(|b| solve_one(2, b)).sum()
+}
+
+fn part_2(banks: &[Vec<u64>]) -> u64 {
+    banks.iter().map(|b| solve_one(12, b)).sum()
 }
 
 fn main() {
     let input = digits(&std::fs::read_to_string("input.txt").expect("file"));
     println!("{}", part_1(&input));
+    println!("{}", part_2(&input));
 }
