ce50d4ca5d05936ccb3f580689831ff771cce3c9
Graham Enos <graham@strangeworks.com>
2025-12-04 12:29:55 -0500

    part_2

--- advent_of_code_2025/day04/src/main.rs
+++ advent_of_code_2025/day04/src/main.rs
@@ -1,6 +1,6 @@
 use std::{collections::BTreeMap, fs};
 
-#[derive(PartialEq, Eq, Clone, Copy, Debug)]
+#[derive(PartialEq, Clone)]
 enum Tile {
     Open,
     Paper,
@@ -31,25 +31,45 @@ fn parse(s: &str) -> Grid {
         .collect()
 }
 
+fn removable(i: usize, j: usize, g: &Grid) -> bool {
+    let mut paper = 0u8;
+    for x in i.saturating_sub(1)..=i + 1 {
+        for y in j.saturating_sub(1)..=j + 1 {
+            if (x, y) != (i, j) && g.get(&(x, y)) == Some(&Tile::Paper) {
+                paper += 1;
+            }
+        }
+    }
+    paper < 4
+}
+
 fn part_1(g: &Grid) -> usize {
     g.iter()
-        .filter(|&(&(i, j), &t)| {
-            t == Tile::Paper && {
-                let mut paper = 0;
-                for x in i.saturating_sub(1)..=i + 1 {
-                    for y in j.saturating_sub(1)..=j + 1 {
-                        if (x, y) != (i, j) && g.get(&(x, y)) == Some(&Tile::Paper) {
-                            paper += 1;
-                        }
-                    }
-                }
-                paper < 4
-            }
-        })
+        .filter(|&(&(i, j), t)| t == &Tile::Paper && removable(i, j, g))
         .count()
+}
+
+fn part_2(g: &Grid) -> usize {
+    let (mut lookup, mut mutate, mut cleared) = (g.clone(), g.clone(), 0);
+    loop {
+        let mut count = 0;
+        for (&(i, j), t) in &mut mutate {
+            if t == &Tile::Paper && removable(i, j, &lookup) {
+                *t = Tile::Open;
+                count += 1;
+            }
+        }
+        cleared += count;
+        lookup = mutate.clone();
+        if count == 0 {
+            break;
+        }
+    }
+    cleared
 }
 
 fn main() {
     let input = parse(&fs::read_to_string("input.txt").expect("file"));
     println!("{}", part_1(&input));
+    println!("{}", part_2(&input));
 }
