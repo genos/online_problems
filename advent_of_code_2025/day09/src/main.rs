b1e80f5d900ff8ae9ef69c3a25b76d3bb25b7bc8
Graham Enos <graham@strangeworks.com>
2025-12-09 16:36:44 -0500

    part_2

--- advent_of_code_2025/day09/src/main.rs
+++ advent_of_code_2025/day09/src/main.rs
@@ -1,6 +1,11 @@
 use itertools::Itertools;
 
-fn parse(s: &str) -> Vec<(usize, usize)> {
+type Tile = (usize, usize);
+type Pair = (Tile, Tile);
+type Bounds = (usize, usize, usize, usize);
+type Area = usize;
+
+fn parse(s: &str) -> Vec<Tile> {
     s.trim()
         .lines()
         .map(|line| {
@@ -10,15 +15,45 @@ fn parse(s: &str) -> Vec<(usize, usize)> {
         .collect()
 }
 
-fn area((x1, y1): (usize, usize), (x2, y2): (usize, usize)) -> usize {
+fn all_pairs(tiles: &[Tile]) -> impl Iterator<Item = Pair> {
+    tiles.iter().copied().tuple_combinations()
+}
+
+fn area(((x1, y1), (x2, y2)): Pair) -> Area {
     (x1.abs_diff(x2) + 1) * (y1.abs_diff(y2) + 1)
 }
 
-fn part_1(coords: &[(usize, usize)]) -> usize {
-    coords
+fn part_1(tiles: &[Tile]) -> Area {
+    all_pairs(tiles).map(area).max().unwrap_or_default()
+}
+
+fn to_bounds(((x1, y1), (x2, y2)): Pair) -> Bounds {
+    (x1.min(x2), x1.max(x2), y1.min(y2), y1.max(y2))
+}
+
+// with help from https://www.reddit.com/r/adventofcode/comments/1pibab2/comment/nt50hqt
+fn part_2(tiles: &[(usize, usize)]) -> Area {
+    let bounds = tiles
         .iter()
-        .tuple_combinations()
-        .map(|(&a, &b)| area(a, b))
+        .copied()
+        .chain(std::iter::once(tiles[0]))
+        .tuple_windows()
+        .map(to_bounds)
+        .collect::<Vec<_>>();
+    all_pairs(tiles)
+        .filter(|pair| {
+            let (x_lo, x_hi, y_lo, y_hi) = to_bounds(*pair);
+            !bounds.iter().any(|&(h_lo, h_hi, v_lo, v_hi)| {
+                let vertical = (h_lo == h_hi)
+                    && (x_lo + 1..x_hi).contains(&h_lo)
+                    && ((v_lo..v_hi).contains(&y_lo) || (v_lo + 1..v_hi + 1).contains(&y_hi));
+                let horizontal = (v_lo == v_hi)
+                    && (y_lo + 1..y_hi).contains(&v_lo)
+                    && ((h_lo..h_hi).contains(&x_lo) || (h_lo + 1..h_hi + 1).contains(&x_hi));
+                vertical || horizontal
+            })
+        })
+        .map(area)
         .max()
         .unwrap_or_default()
 }
@@ -26,4 +61,5 @@ fn part_1(coords: &[(usize, usize)]) -> usize {
 fn main() {
     let input = parse(&std::fs::read_to_string("input.txt").expect("file"));
     println!("{}", part_1(&input));
+    println!("{}", part_2(&input));
 }
