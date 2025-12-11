cb8dcc8dd88cf697a0bd96fbf46ab6b96c4d7e64
Graham Enos <graham@strangeworks.com>
2025-12-11 14:27:03 -0500

    squeeze

--- advent_of_code_2025/day11/src/main.rs
+++ advent_of_code_2025/day11/src/main.rs
@@ -1,66 +1,5 @@
-/* Petgraph version that is too slow for part 2
-use petgraph::{
-    acyclic::Acyclic,
-    algo::{all_simple_paths, has_path_connecting},
-    prelude::*,
-};
-use std::collections::{HashMap, HashSet, hash_map::RandomState};
-
-type Servers = Acyclic<DiGraph<String, ()>>;
-
-fn parse(s: &str) -> (Servers, HashMap<String, NodeIndex>) {
-    let mut servers = DiGraph::new();
-    let nodes = s
-        .replace(": ", " ")
-        .replace('\n', " ")
-        .trim()
-        .split_ascii_whitespace()
-        .collect::<HashSet<_>>()
-        .iter()
-        .map(|&x| {
-            let n = x.to_string();
-            (n.clone(), servers.add_node(n))
-        })
-        .collect::<HashMap<_, _>>();
-    for line in s.trim().lines() {
-        let (head, rest) = line.split_once(": ").expect("head");
-        let u = nodes[head];
-        for v in rest.split_ascii_whitespace() {
-            servers.add_edge(u, nodes[v], ());
-        }
-    }
-    (Acyclic::try_from_graph(servers).expect("acyclic"), nodes)
-}
-
-fn part_1(servers: &Servers, nodes: &HashMap<String, NodeIndex>) -> usize {
-    all_simple_paths::<Vec<_>, _, RandomState>(servers, nodes["you"], nodes["out"], 0, None).count()
-}
-
-fn part_2(servers: &Servers, nodes: &HashMap<String, NodeIndex>) -> usize {
-    let (svr, dac, fft, out) = (nodes["svr"], nodes["dac"], nodes["fft"], nodes["out"]);
-    if has_path_connecting(servers, dac, fft, None) {
-        println!("dac -> fft");
-        let x = all_simple_paths::<Vec<_>, _, RandomState>(servers, svr, dac, 0, None).count();
-        let y = all_simple_paths::<Vec<_>, _, RandomState>(servers, dac, fft, 0, None).count();
-        let z = all_simple_paths::<Vec<_>, _, RandomState>(servers, fft, out, 0, None).count();
-        x * y * z
-    } else {
-        println!("fft -> dac");
-        let x = all_simple_paths::<Vec<_>, _, RandomState>(servers, svr, fft, 0, None).count();
-        let y = all_simple_paths::<Vec<_>, _, RandomState>(servers, fft, dac, 0, None).count();
-        let z = all_simple_paths::<Vec<_>, _, RandomState>(servers, dac, out, 0, None).count();
-        x * y * z
-    }
-}
-
-fn main() {
-    let (servers, nodes) = parse(&std::fs::read_to_string("input.txt").expect("file"));
-    println!("{}", part_1(&servers, &nodes));
-    println!("{}", part_2(&servers, &nodes));
-}
-*/
-
 // Some help from https://github.com/Fadi88/AoC/blob/master/2025/days/day11/src/lib.rs
+// (Not much help; I had the entire thing right, but used petgraph poorly.)
 // petgraph does _too_ much! We need simplicity.
 
 use std::collections::HashMap;
@@ -75,38 +14,38 @@ fn parse(s: &str) -> HashMap<&str, Vec<&str>> {
         .collect()
 }
 
-fn num_paths<'a>(
-    u: &'a str,
-    v: &'a str,
-    servers: &'a HashMap<&'a str, Vec<&'a str>>,
-    cache: &mut HashMap<(&'a str, &'a str), u64>,
-) -> u64 {
-    if u == v {
-        1
-    } else if let Some(n) = cache.get(&(u, v)) {
-        *n
-    } else {
-        let n = servers
-            .get(u)
-            .map(|ws| ws.iter().map(|w| num_paths(w, v, servers, cache)).sum())
-            .unwrap_or_default();
-        cache.insert((u, v), n);
-        n
+fn paths(source: &str, target: &str, servers: &HashMap<&str, Vec<&str>>) -> u64 {
+    fn go<'a>(
+        u: &'a str,
+        v: &'a str,
+        d: &HashMap<&'a str, Vec<&'a str>>,
+        cache: &mut HashMap<&'a str, u64>,
+    ) -> u64 {
+        if u == v {
+            1
+        } else if let Some(n) = cache.get(u) {
+            *n
+        } else {
+            let n = d
+                .get(u)
+                .map(|ws| ws.iter().map(|w| go(w, v, d, cache)).sum())
+                .unwrap_or_default();
+            cache.insert(u, n);
+            n
+        }
     }
+    go(source, target, servers, &mut HashMap::new())
 }
 
 fn part_1(servers: &HashMap<&str, Vec<&str>>) -> u64 {
-    num_paths("you", "out", servers, &mut HashMap::new())
+    paths("you", "out", servers)
 }
 
 fn part_2(servers: &HashMap<&str, Vec<&str>>) -> u64 {
-    let svr_dac = num_paths("svr", "dac", servers, &mut HashMap::new());
-    let dac_fft = num_paths("dac", "fft", servers, &mut HashMap::new());
-    let fft_out = num_paths("fft", "out", servers, &mut HashMap::new());
-    let svr_fft = num_paths("svr", "fft", servers, &mut HashMap::new());
-    let fft_dac = num_paths("fft", "dac", servers, &mut HashMap::new());
-    let dac_out = num_paths("dac", "out", servers, &mut HashMap::new());
-    svr_dac * dac_fft * fft_out + svr_fft * fft_dac * dac_out
+    [("dac", "fft"), ("fft", "dac")]
+        .iter()
+        .map(|(u, v)| paths("svr", u, servers) * paths(u, v, servers) * paths(v, "out", servers))
+        .sum()
 }
 
 fn main() {
