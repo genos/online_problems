258f814c512bd358f7316178ab7c81b7e1b58200
Graham Enos <graham@strangeworks.com>
2025-12-11 19:39:03 -0500

    crustacean

---
+++ advent_of_code_2025/day01/src/main.rs
@@ -0,0 +1,42 @@
+fn parse(s: &str) -> Vec<i16> {
+    s.trim()
+        .lines()
+        .map(|line| {
+            let (x, y) = line.split_at(1);
+            (if x == "L" { -1 } else { 1 }) * y.parse::<i16>().expect("val")
+        })
+        .collect()
+}
+
+fn part_1(input: &[i16]) -> usize {
+    input
+        .iter()
+        .scan(50, |a, b| {
+            *a += *b;
+            *a %= 100;
+            Some(*a)
+        })
+        .filter(|&n| n == 0)
+        .count()
+}
+
+fn part_2(input: &[i16]) -> usize {
+    input
+        .iter()
+        .fold((0, 50), |(mut n, mut p), i| {
+            let s = i.signum();
+            for _ in 0..(s * i) {
+                p += s;
+                p %= 100;
+                n += usize::from(p == 0);
+            }
+            (n, p)
+        })
+        .0
+}
+
+fn main() {
+    let input = parse(&std::fs::read_to_string("input.txt").expect("file"));
+    println!("{}", part_1(&input));
+    println!("{}", part_2(&input));
+}
